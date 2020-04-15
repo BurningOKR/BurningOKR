fb359c434b6d25ffa4935743a6de0ccae54f0b91
Robin Janke <robin.janke@brockhaus-ag.de>
2020-04-15 16:02:47 +0200

    test(OAuthFrontendDetailsServiceTest.java): Added OAuthFrontendDetailsServiceTest

---
+++ backend/burning-okr/burning-okr-logic/src/test/java/org/burningokr/service/configuration/OAuthFrontendDetailsServiceTest.java
@@ -0,0 +1,100 @@
+package org.burningokr.service.configuration;
+
+import org.burningokr.model.configuration.OAuthClientDetails;
+import org.burningokr.model.configuration.OAuthFrontendDetails;
+import org.burningokr.repositories.configuration.OAuthFrontendDetailsRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.util.AssertionErrors.assertTrue;
+
+@RunWith(MockitoJUnitRunner.class)
+public class OAuthFrontendDetailsServiceTest {
+
+  @Mock private OAuthFrontendDetailsRepository oAuthFrontendDetailsRepository;
+
+  @InjectMocks private OAuthFrontendDetailsService oAuthFrontendDetailsService;
+
+  OAuthFrontendDetails oAuthFrontendDetails;
+
+  @Before
+  public void init() {
+    oAuthFrontendDetails = new OAuthFrontendDetails();
+  }
+
+  @Test
+  public void updateOauthFrontendDetails_updates_details() {
+
+    when(oAuthFrontendDetailsRepository.findAll()).thenReturn(Collections.singletonList(oAuthFrontendDetails));
+
+    OAuthClientDetails oAuthClientDetails = new OAuthClientDetails();
+    oAuthClientDetails.setClientId("test123");
+    oAuthClientDetails.setClientSecret("test456");
+    oAuthClientDetails.setWebServerRedirectUri("www.example.com");
+
+    oAuthFrontendDetailsService.updateOauthFrontendDetails(oAuthClientDetails);
+
+    assertEquals(oAuthClientDetails.getClientId(), oAuthFrontendDetails.getClientId());
+    assertEquals(oAuthClientDetails.getClientSecret(), oAuthFrontendDetails.getDummyClientSecret());
+    assertEquals(oAuthClientDetails.getWebServerRedirectUri(), oAuthClientDetails.getWebServerRedirectUri());
+  }
+
+  @Test
+  public void getOauthFrontendDetails_getsDetails() {
+
+    oAuthFrontendDetails.setClientId("test");
+    oAuthFrontendDetails.setDummyClientSecret("test123");
+
+    when(oAuthFrontendDetailsRepository.findAll()).thenReturn(Collections.singletonList(oAuthFrontendDetails));
+
+    OAuthFrontendDetails oAuthFrontendDetailsFromService = oAuthFrontendDetailsService.getOAuthFrontendDetails();
+
+    assertEquals(oAuthFrontendDetails.getClientId(), oAuthFrontendDetailsFromService.getClientId());
+    assertEquals(oAuthFrontendDetails.getDummyClientSecret(), oAuthFrontendDetailsFromService.getDummyClientSecret());
+
+  }
+
+  @Test
+  public void getOauthFrontendDetails_getsOnlyFirstDetails() {
+
+    oAuthFrontendDetails.setClientId("test");
+    oAuthFrontendDetails.setDummyClientSecret("test123");
+
+    OAuthFrontendDetails oAuthFrontendDetails2 = new OAuthFrontendDetails();
+    oAuthFrontendDetails2.setClientId("test2");
+    oAuthFrontendDetails2.setDummyClientSecret("test456");
+
+    when(oAuthFrontendDetailsRepository.findAll()).thenReturn(Arrays.asList(oAuthFrontendDetails, oAuthFrontendDetails2));
+
+    OAuthFrontendDetails oAuthFrontendDetailsFromService = oAuthFrontendDetailsService.getOAuthFrontendDetails();
+
+    assertEquals(oAuthFrontendDetails.getClientId(), oAuthFrontendDetailsFromService.getClientId());
+    assertEquals(oAuthFrontendDetails.getDummyClientSecret(), oAuthFrontendDetailsFromService.getDummyClientSecret());
+
+  }
+
+  @Test
+  public void getOauthFrontendDetails_throwsExceptionWhenEmpty() {
+
+    when(oAuthFrontendDetailsRepository.findAll()).thenReturn(Collections.emptyList());
+
+    try {
+      OAuthFrontendDetails oAuthFrontendDetailsFromService = oAuthFrontendDetailsService.getOAuthFrontendDetails();
+      fail();
+    } catch (Exception e) {
+      assertTrue("Wrong Exception", e instanceof EntityNotFoundException);
+    }
+  }
+}
