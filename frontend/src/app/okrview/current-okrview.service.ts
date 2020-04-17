102b0d1e66143af7a490781ba4417af6158cf09b
Robin Janke <robin.janke@brockhaus-ag.de>
2020-04-17 10:32:47 +0200

    refactor(CurrentOkrViewService): Finished splitting current okrview servie

--- frontend/src/app/okrview/current-okrview.service.ts
+++ frontend/src/app/okrview/current-okrview.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Subscription } from 'rxjs';
 import { CompanyMapper } from '../shared/services/mapper/company.mapper';
 import { CycleMapper } from '../shared/services/mapper/cycle.mapper';
 import { DepartmentStructureMapper } from '../shared/services/mapper/department-structure.mapper';
@@ -11,17 +10,6 @@ import { CurrentCompanyService } from './current-company.service';
   providedIn: 'root'
 })
 export class CurrentOkrviewService {
-  companySubscription: Subscription;
-  departmentStructureSubscription: Subscription;
-  cycleListSubscription: Subscription;
-
-  // currentCompanyId: number;
-  // currentDepartmentId: number;
-  //
-  // currentCompany: CompanyUnit;
-  // currentDepartmentStructure: DepartmentStructure[];
-  // currentCycle$ = new ReplaySubject<CycleUnit>();
-  // currentCycleList$ = new ReplaySubject<CycleUnit[]>();
 
   constructor(
     private departmentStructureMapperService: DepartmentStructureMapper,
@@ -38,6 +26,7 @@ export class CurrentOkrviewService {
   }
 
   browseDepartment(departmentId: number): void {
+    this.currentNavigationService.clearDepartmentNavigationInformation();
     this.fetchNewValuesForDepartmentId(departmentId);
   }
 
@@ -52,18 +41,13 @@ export class CurrentOkrviewService {
   private fetchNewValuesForCompanyId(companyId: number): void {
     this.currentNavigationService.clearDepartmentNavigationInformation();
     this.currentCompanyService.setCurrentCompanyByCompanyId(companyId);
-
-    // this.currentCompanyId = companyId;
-
     this.currentDepartmentStructureService.setCurrentDepartmentStructureByCompanyId(companyId);
   }
 
   private fetchNewValuesForDepartmentId(departmentId: number): void {
     this.currentCompanyService.setCurrentCompanyByChildDepartmentId(departmentId);
-
     this.currentDepartmentStructureService.setCurrentDepartmentStructureByDepartmentId(departmentId);
     this.currentNavigationService.refreshDepartmentNavigationInformation();
 
-    // this.currentDepartmentId = departmentId;
   }
 }
