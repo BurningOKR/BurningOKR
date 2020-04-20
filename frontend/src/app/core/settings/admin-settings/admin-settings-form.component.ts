ac6f2c6b3d3316acbc3a6e6692b524605495416c
Robin Janke <robin.janke@brockhaus-ag.de>
2020-04-20 14:24:01 +0200

    feat(ConfigurationManagerService): ConfigurationManagerService will now display all configurations in the database
    ConfigurationManagerService will now display all configurations in the
    database, without extra configuration except for translations

--- frontend/src/app/core/settings/admin-settings/admin-settings-form.component.ts
+++ frontend/src/app/core/settings/admin-settings/admin-settings-form.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material';
 import { CurrentUserService } from '../../services/current-user.service';
 import { forkJoin, NEVER, Observable, of } from 'rxjs';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ConfigurationManagerService } from '../configuration-manager.service';
 import { UserSettings } from '../../../shared/model/ui/user-settings';
 import { UserSettingsManagerService } from '../../services/user-settings-manager.service';
@@ -11,21 +11,70 @@ import { DepartmentMapper } from '../../../shared/services/mapper/department.map
 import { DepartmentUnit } from '../../../shared/model/ui/OrganizationalUnit/department-unit';
 import { CompanyUnit } from '../../../shared/model/ui/OrganizationalUnit/company-unit';
 import { Configuration } from '../../../shared/model/ui/configuration';
-import { filter, switchMap, take } from 'rxjs/operators';
+import { filter, map, switchMap, take } from 'rxjs/operators';
 import { OAuthFrontendDetailsService } from '../../auth/services/o-auth-frontend-details.service';
+import { I18n } from '@ngx-translate/i18n-polyfill';
 
 @Component({
   selector: 'app-admin-settings',
   templateUrl: './admin-settings-form.component.html',
   styleUrls: ['./admin-settings-form.component.scss']
 })
-// TODO: rename to AdminSettingsFormComponent or dir
 export class AdminSettingsFormComponent implements OnInit {
 
   adminSettingsForm: FormGroup;
   userSettingsForm: FormGroup;
   companies$: Observable<CompanyUnit[]>;
   departments$: Observable<DepartmentUnit[]>;
+  authType$: Observable<string>;
+
+  private configurationNames: { [key: string]: string } = {
+    'max-key-results': this.i18n({
+      id: '@@settingsFormMaxKeyResultsPerObjective',
+      description: 'Placeholder for maximal amount of keyresults per objective',
+      value: 'Maximale Anzahl von Key Results'
+    }),
+    'objective-progress-green-yellow-threshold': this.i18n({
+      id: '@@settingsFormObjectiveBarTresholdGreenYellow',
+      description: 'Treshold for objective progress bar (Green/Yellow)',
+      value: 'Objective Prognose Schwellenwert (Grün/Gelb)'
+    }),
+    'objective-progress-yellow-red-threshold': this.i18n({
+      id: '@@settingsFormObjectiveBarTresholdYellowRed',
+      description: 'Treshold for objective progress bar (Yellow/Red)',
+      value: 'Objective Prognose Schwellenwert (Gelb/Rot)'
+    }),
+    'general_frontend-base-url': this.i18n({
+      id: '@@settingsFormGeneralFrontendBaseUrl',
+      description: 'the domain of this angular application',
+      value: 'Frontend-Baseurl'
+    }),
+    email_from: this.i18n({
+      id: '@@settingsFormGeneralFrontendBaseUrl',
+      description: 'The address, from which emails are sent',
+      value: 'Email Adresse des OKR Tools'
+    }),
+    'email_subject_new-user': this.i18n({
+      id: '@@settingsFormGeneralFrontendBaseUrl',
+      description: 'the subject of the email, which is sent to new users',
+      value: 'Email Betreff für neue Benutzer'
+    }),
+    'email_subject_forgot-password': this.i18n({
+      id: '@@settingsFormGeneralFrontendBaseUrl',
+      description: 'the subject of the email, which is sent to users who forgot their password',
+      value: 'Email Betreff für die Passwort-Zurücksetzen Email'
+    }),
+    email_subject_feedback: this.i18n({
+      id: '@@settingsFormEmailSubjectFeedback',
+      description: 'the subject of the feedback email',
+      value: 'Email Betreff für Feedback'
+    }),
+    feedback_receivers: this.i18n({
+      id: '@@settingsFormFeedbackReceivers',
+      description: 'the email adresses of the people who receive feedback',
+      value: 'Email Adressen der Feedback Empfänger. (Durch Komma getrennt)'
+    })
+  };
 
   constructor(private dialogRef: MatDialogRef<AdminSettingsFormComponent>,
               private currentUserService: CurrentUserService,
@@ -33,7 +82,8 @@ export class AdminSettingsFormComponent implements OnInit {
               private userSettingsManager: UserSettingsManagerService,
               private companyService: CompanyMapper,
               private departmentService: DepartmentMapper,
-              private oAuthDetails: OAuthFrontendDetailsService) {
+              private oAuthDetails: OAuthFrontendDetailsService,
+              private i18n: I18n) {
   }
 
   private _isCurrentUserAdmin$: Observable<boolean>;
@@ -65,21 +115,8 @@ export class AdminSettingsFormComponent implements OnInit {
   }
 
   saveAdminSettings(updates$: Observable<UserSettings | Configuration>[]): void {
-    const maxKeyResults: number = this.adminSettingsForm.get('maxKeyResults').value;
-    const objectiveProgressGreenYellowThreshold: number = this.adminSettingsForm.get('objectiveProgressGreenYellowThreshold').value;
-    const objectiveProgressYellowRedThreshold: number = this.adminSettingsForm.get('objectiveProgressYellowRedThreshold').value;
-    const generalFrontendBaseUrl: string = this.adminSettingsForm.get('generalFrontendBaseUrl').value;
-    const emailFrom: string = this.adminSettingsForm.get('emailFrom').value;
-    const emailSubjectNewUser: string = this.adminSettingsForm.get('emailSubjectNewUser').value;
-    const emailSubjectForgotPassword: string = this.adminSettingsForm.get('emailSubjectForgotPassword').value;
-
-    updates$.push(this.configurationManagerService.updateMaxKeyResults$(maxKeyResults));
-    updates$.push(this.configurationManagerService.updateObjectiveProgressGreenYellowThreshold(objectiveProgressGreenYellowThreshold));
-    updates$.push(this.configurationManagerService.updateObjectiveProgressYellowRedThreshold(objectiveProgressYellowRedThreshold));
-    updates$.push(this.configurationManagerService.updateGeneralFrontendBaseUrl(generalFrontendBaseUrl));
-    updates$.push(this.configurationManagerService.updateEmailFrom(emailFrom));
-    updates$.push(this.configurationManagerService.updateEmailSubjectNewUser(emailSubjectNewUser));
-    updates$.push(this.configurationManagerService.updateEmailSubjectForgotPassword(emailSubjectForgotPassword));
+    const configurations: Configuration[] = this.adminSettingsForm.getRawValue().settings;
+    updates$.push(this.configurationManagerService.updateConfigurations$(configurations));
   }
 
   saveUserSettings(updates$: Observable<UserSettings | Configuration>[]): void {
@@ -142,33 +179,31 @@ export class AdminSettingsFormComponent implements OnInit {
   }
 
   private initAdminSettingsForm(): void {
-    const maxKeyResults: number = +this.configurationManagerService.maxKeyResults.value;
-    const objectiveProgressGreenYellowThreshold: number =
-      +this.configurationManagerService.objectiveProgressGreenYellowThreshold.value;
-    const objectiveProgressYellowRedThreshold: number =
-      +this.configurationManagerService.objectiveProgressYellowRedThreshold.value;
-    const generalFrontendBaseUrl: string = this.configurationManagerService.generalFrontendBaseUrl.value;
-    const emailFrom: string = this.configurationManagerService.emailFrom.value;
-    const emailSubjectNewUser: string = this.configurationManagerService.emailSubjectNewUser.value;
-    const emailSubjectForgotPassword: string = this.configurationManagerService.emailSubjectForgotPassword.value;
-
-    this.adminSettingsForm = new FormGroup({
-      maxKeyResults: new FormControl(maxKeyResults, [Validators.required]),
-      objectiveProgressGreenYellowThreshold: new FormControl(objectiveProgressGreenYellowThreshold, [Validators.required]),
-      objectiveProgressYellowRedThreshold: new FormControl(objectiveProgressYellowRedThreshold, [Validators.required]),
-      generalFrontendBaseUrl: new FormControl(generalFrontendBaseUrl, [Validators.required]),
-      emailFrom: new FormControl(emailFrom, [Validators.required]),
-      emailSubjectNewUser: new FormControl(emailSubjectNewUser, [Validators.required]),
-      emailSubjectForgotPassword: new FormControl(emailSubjectForgotPassword, [Validators.required])
-    });
-
-    this.oAuthDetails.getAuthType$()
-      .pipe(take(1))
-      .subscribe(authType => {
-        if (authType === 'azure') {
-          this.adminSettingsForm.get('generalFrontendBaseUrl')
-            .disable();
-        }
+    this.configurationManagerService.getAllConfigurations$()
+      .pipe(
+        map((configurations: Configuration[]) => {
+          return configurations
+            .map((configuration: Configuration) => {
+              return new FormGroup({
+                id: new FormControl(configuration.id),
+                name: new FormControl(configuration.name),
+                value: new FormControl(configuration.value, [Validators.required])
+              });
+            })
+            .sort((a: FormGroup, b: FormGroup) => +a.controls.id.value - +b.controls.id.value);
+        })
+      )
+      .subscribe((formGroups: FormGroup[]) => {
+        this.adminSettingsForm = new FormGroup({
+          settings: new FormArray(formGroups)
+        });
       });
+
+    this.authType$ = this.oAuthDetails.getAuthType$()
+      .pipe(take(1));
+  }
+
+  get settings(): FormArray {
+    return this.adminSettingsForm.get('settings') as FormArray;
   }
 }
