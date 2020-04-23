be3e7cd5a6050fc5f21cd3ec564d3c5520cde0fc
Robin Janke <robin.janke@brockhaus-ag.de>
2020-04-23 15:32:37 +0200

    fix(OAuthConfigurationServiceTest): Added OAuthConfigurationServiceTest

--- backend/burning-okr/burning-okr-logic/src/test/java/org/burningokr/service/configuration/OAuthConfigurationServiceTest.java
+++ backend/burning-okr/burning-okr-logic/src/test/java/org/burningokr/service/configuration/OAuthConfigurationServiceTest.java
@@ -1,95 +1,73 @@
 package org.burningokr.service.configuration;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+import java.util.*;
+import org.burningokr.model.configuration.ConfigurationType;
+import org.burningokr.model.configuration.OAuthClientDetails;
+import org.burningokr.model.configuration.OAuthConfiguration;
+import org.burningokr.model.configuration.OAuthConfigurationName;
+import org.burningokr.repositories.configuration.OAuthConfigurationRepository;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class OAuthConfigurationServiceTest {
 
-  //  @Mock private OAuthFrontendDetailsRepository oAuthFrontendDetailsRepository;
-  //
-  //  @InjectMocks private OAuthConfigurationService oAuthConfigurationService;
-  //
-  //  OAuthFrontendDetails oAuthFrontendDetails;
-  //
-  //  @Before
-  //  public void init() {
-  //    oAuthFrontendDetails = new OAuthFrontendDetails();
-  //  }
-  //
-  //  @Test
-  //  public void updateOauthFrontendDetails_updates_details() {
-  //
-  //    when(oAuthFrontendDetailsRepository.findAll())
-  //        .thenReturn(Collections.singletonList(oAuthFrontendDetails));
-  //
-  //    OAuthClientDetails oAuthClientDetails = new OAuthClientDetails();
-  //    oAuthClientDetails.setClientId("test123");
-  //    oAuthClientDetails.setClientSecret("test456");
-  //    oAuthClientDetails.setWebServerRedirectUri("www.example.com");
-  //
-  //    oAuthConfigurationService.updateOAuthConfiguration(oAuthClientDetails);
-  //
-  //    assertEquals(oAuthClientDetails.getClientId(), oAuthFrontendDetails.getClientId());
-  //    assertEquals(oAuthClientDetails.getClientSecret(),
-  // oAuthFrontendDetails.getDummyClientSecret());
-  //    assertEquals(
-  //        oAuthClientDetails.getWebServerRedirectUri(),
-  // oAuthClientDetails.getWebServerRedirectUri());
-  //  }
-  //
-  //  @Test
-  //  public void getOauthFrontendDetails_getsDetails() {
-  //
-  //    oAuthFrontendDetails.setClientId("test");
-  //    oAuthFrontendDetails.setDummyClientSecret("test123");
-  //
-  //    when(oAuthFrontendDetailsRepository.findAll())
-  //        .thenReturn(Collections.singletonList(oAuthFrontendDetails));
-  //
-  //    OAuthFrontendDetails oAuthFrontendDetailsFromService =
-  //        oAuthConfigurationService.getOAuthConfigurations();
-  //
-  //    assertEquals(oAuthFrontendDetails.getClientId(),
-  // oAuthFrontendDetailsFromService.getClientId());
-  //    assertEquals(
-  //        oAuthFrontendDetails.getDummyClientSecret(),
-  //        oAuthFrontendDetailsFromService.getDummyClientSecret());
-  //  }
-  //
-  //  @Test
-  //  public void getOauthFrontendDetails_getsOnlyFirstDetails() {
-  //
-  //    oAuthFrontendDetails.setClientId("test");
-  //    oAuthFrontendDetails.setDummyClientSecret("test123");
-  //
-  //    OAuthFrontendDetails oAuthFrontendDetails2 = new OAuthFrontendDetails();
-  //    oAuthFrontendDetails2.setClientId("test2");
-  //    oAuthFrontendDetails2.setDummyClientSecret("test456");
-  //
-  //    when(oAuthFrontendDetailsRepository.findAll())
-  //        .thenReturn(Arrays.asList(oAuthFrontendDetails, oAuthFrontendDetails2));
-  //
-  //    OAuthFrontendDetails oAuthFrontendDetailsFromService =
-  //        oAuthConfigurationService.getOAuthConfigurations();
-  //
-  //    assertEquals(oAuthFrontendDetails.getClientId(),
-  // oAuthFrontendDetailsFromService.getClientId());
-  //    assertEquals(
-  //        oAuthFrontendDetails.getDummyClientSecret(),
-  //        oAuthFrontendDetailsFromService.getDummyClientSecret());
-  //  }
-  //
-  //  @Test
-  //  public void getOauthFrontendDetails_throwsExceptionWhenEmpty() {
-  //
-  //    when(oAuthFrontendDetailsRepository.findAll()).thenReturn(Collections.emptyList());
-  //
-  //    try {
-  //      oAuthConfigurationService.getOAuthConfigurations();
-  //      fail();
-  //    } catch (Exception e) {
-  //      assertTrue("Wrong Exception", e instanceof EntityNotFoundException);
-  //    }
-  //  }
+  @Mock private OAuthConfigurationRepository oAuthConfigurationRepository;
+
+  @InjectMocks private OAuthConfigurationService oAuthConfigurationService;
+
+  @Test
+  public void updateOauthFrontendDetails_updates_details() {
+
+    when(oAuthConfigurationRepository.findAll()).thenReturn(Collections.emptyList());
+
+    OAuthClientDetails oAuthClientDetails = new OAuthClientDetails();
+    oAuthClientDetails.setClientId("test123");
+    oAuthClientDetails.setClientSecret("test456");
+    oAuthClientDetails.setWebServerRedirectUri("www.example.com");
+
+    oAuthConfigurationService.updateOAuthConfiguration(oAuthClientDetails);
+
+    verify(oAuthConfigurationRepository, times(3)).save(any());
+  }
+
+  @Test
+  public void getOauthFrontendDetails_getsDetails() {
+
+    OAuthConfiguration clientId =
+        new OAuthConfiguration(OAuthConfigurationName.CLIENT_ID, "test123", ConfigurationType.TEXT);
+    OAuthConfiguration clientSecret =
+        new OAuthConfiguration(
+            OAuthConfigurationName.CLIENT_SECRET, "abc123", ConfigurationType.TEXT);
+
+    when(oAuthConfigurationRepository.findAll()).thenReturn(Arrays.asList(clientId, clientSecret));
+
+    Collection<OAuthConfiguration> oAuthConfigurations =
+        oAuthConfigurationService.getOAuthConfigurations();
+
+    assertEquals(
+        "test123",
+        getConfigurationByName(oAuthConfigurations, OAuthConfigurationName.CLIENT_ID).getValue());
+    assertEquals(
+        "abc123",
+        getConfigurationByName(oAuthConfigurations, OAuthConfigurationName.CLIENT_SECRET)
+            .getValue());
+  }
+
+  private OAuthConfiguration getConfigurationByName(
+      Collection<OAuthConfiguration> configurations, OAuthConfigurationName configurationName) {
+    Optional<OAuthConfiguration> foundConfiguration =
+        configurations.stream()
+            .filter(configuration -> configuration.getKey().equals(configurationName.getName()))
+            .findFirst();
+
+    return foundConfiguration.orElse(
+        new OAuthConfiguration(configurationName, "", ConfigurationType.TEXT));
+  }
 }
