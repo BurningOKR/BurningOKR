731605687cf92b8661b499f388a4c75391b741b1
dturnschek <dturnschek@brockhaus-ag.de>
2020-05-07 08:37:15 +0200

    feat(noMailWarning): Add a warning Text and Page if there is no SMTP configured

---
+++ frontend/src/app/information/no-mail-information/no-mail-information.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NoMailInformationComponent } from './no-mail-information.component';
+
+describe('NoMailInformationComponent', () => {
+  let component: NoMailInformationComponent;
+  let fixture: ComponentFixture<NoMailInformationComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NoMailInformationComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NoMailInformationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/information/no-mail-information/no-mail-information.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-no-mail-information',
+  templateUrl: './no-mail-information.component.html',
+  styleUrls: ['./no-mail-information.component.css']
+})
+export class NoMailInformationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
