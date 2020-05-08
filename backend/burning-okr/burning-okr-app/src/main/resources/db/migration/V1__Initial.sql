6720984b7dfc07684a6227af10e230914f1045cd
dturnschek <dturnschek@brockhaus-ag.de>
2020-05-08 10:50:42 +0200

    fix(SQL): Add missing Instert

--- backend/burning-okr/burning-okr-app/src/main/resources/db/migration/V1__Initial.sql
+++ backend/burning-okr/burning-okr-app/src/main/resources/db/migration/V1__Initial.sql
@@ -243,7 +243,28 @@ CREATE TABLE public.user_settings (
       ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-
+INSERT INTO public.oauth_configuration (key, value, type) VALUES ('client-id', 'clientapp', 'text');
+INSERT INTO public.oauth_configuration (key, value, type) VALUES ('client-secret', '', 'text');
+INSERT INTO public.oauth_configuration (key, value, type) VALUES ('scope', 'USER', 'text');
+INSERT INTO public.oauth_configuration (key, value, type) VALUES ('redirect-uri', '', 'text');
+INSERT INTO public.oauth_configuration (key, value, type) VALUES ('silent-refresh-redirect-uri', '', 'text');
+INSERT INTO public.oauth_configuration (key, value, type) VALUES ('auth-type', 'local', 'text');
+INSERT INTO public.oauth_configuration (key, value, type) VALUES ('issuer', '', 'text');
+INSERT INTO public.oauth_configuration (key, value, type) VALUES ('response-type', 'password,refresh_token', 'text');
+INSERT INTO public.oauth_configuration (key, value, type) VALUES ('oidc', null, 'text');
+INSERT INTO public.oauth_configuration (key, value, type) VALUES ('show-debug-information', null, 'text');
+INSERT INTO public.oauth_configuration (key, value, type) VALUES ('require-https', 'false', 'text');
+INSERT INTO public.oauth_configuration (key, value, type) VALUES ('strict-discovery-document-validation', null, 'text');
+INSERT INTO public.oauth_configuration (key, value, type) VALUES ('use-http-basic-auth', 'true', 'text');
+INSERT INTO public.oauth_configuration (key, value, type) VALUES ('access-token-uri', null, 'text');
+INSERT INTO public.oauth_configuration (key, value, type) VALUES ('user-authorization-uri', null, 'text');
+INSERT INTO public.oauth_configuration (key, value, type) VALUES ('client-authentication-scheme', null, 'text');
+INSERT INTO public.oauth_configuration (key, value, type) VALUES ('grant-type', null, 'text');
+INSERT INTO public.oauth_configuration (key, value, type) VALUES ('auto-approve-scopes', null, 'text');
+INSERT INTO public.oauth_configuration (key, value, type) VALUES ('token-name', null, 'text');
+INSERT INTO public.oauth_configuration (key, value, type) VALUES ('user-info-uri', null, 'text');
+INSERT INTO public.oauth_configuration (key, value, type) VALUES ('prefer-token-info', null, 'text');
+INSERT INTO public.oauth_configuration (key, value, type) VALUES ('token-endpoint', '/oauth/token', 'text');
 
 INSERT INTO public.configuration VALUES (63, 'general_frontend-base-url', 'http://localhost:4200', 'text');
 INSERT INTO public.configuration VALUES (65, 'email_from', 'no-reply@burning-okr.com', 'text');
