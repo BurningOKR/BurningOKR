3f9da2764dbfe85cd44ddd8032407d68b46a8b45
SKoschel <skoschel@arcor.de>
2020-05-22 15:16:27 +0200

    feat(CurrentCompanyService): added tests for CurrentCompanyService

--- frontend/src/app/okrview/current-company.service.spec.ts
+++ frontend/src/app/okrview/current-company.service.spec.ts
@@ -2,18 +2,107 @@ import { TestBed } from '@angular/core/testing';
 
 import { CurrentCompanyService } from './current-company.service';
 import { CompanyMapper } from '../shared/services/mapper/company.mapper';
-import { CompanyMapperMock } from '../shared/mocks/company-mapper-mock';
+import { CompanyUnit } from '../shared/model/ui/OrganizationalUnit/company-unit';
+import { of } from 'rxjs';
+
+const companyMapperMock: any = {
+  getCompanyById$: jest.fn(),
+  getParentCompanyOfDepartment$: jest.fn()
+};
+const testCompany: CompanyUnit = {
+  id: 1,
+  corporateObjectiveStructureIds: [],
+  cycleId: 1,
+  departmentIds: [],
+  label: '',
+  name: '',
+  objectives: []
+};
+let service: CurrentCompanyService;
 
 describe('CurrentCompanyService', () => {
   beforeEach(() => TestBed.configureTestingModule({
     providers: [
-      {provide: CompanyMapper, useValue: CompanyMapperMock}
+      {provide: CompanyMapper, useValue: companyMapperMock}       // TODO: wegmachen?
     ]
   }));
 
+  beforeEach(() => {
+    service = TestBed.get(CurrentCompanyService);
+    companyMapperMock.getCompanyById$.mockReturnValue(of(testCompany));
+    companyMapperMock.getParentCompanyOfDepartment$.mockReturnValue(of(testCompany));
+  });
+
   it('should be created', () => {
-    const service: CurrentCompanyService = TestBed.get(CurrentCompanyService);
     expect(service)
       .toBeTruthy();
+  });
+
+  it('should set current company by company id', done => {
+    service.setCurrentCompanyByCompanyId(testCompany.id);
+    service.getCurrentCompany$()
+      .subscribe(value => {
+        expect(value)
+          .toEqual(testCompany);
+        done();
+      });
+  });
+
+  it('setCurrentCompanyByCompanyId gets null as parameter', done => {
+    companyMapperMock.getCompanyById$.mockReset();
+    companyMapperMock.getCompanyById$.mockReturnValue(of(null));
+    service.setCurrentCompanyByCompanyId(testCompany.id);
+    service.getCurrentCompany$()
+      .subscribe(value => {
+        expect(value)
+          .toEqual(null);
+        done();
+      });
+  });
+
+  it('setCurrentCompanyByCompanyId gets undefined as parameter', done => {
+    companyMapperMock.getCompanyById$.mockReset();
+    companyMapperMock.getCompanyById$.mockReturnValue(of(undefined));
+    service.setCurrentCompanyByCompanyId(testCompany.id);
+    service.getCurrentCompany$()
+      .subscribe(value => {
+        expect(value)
+          .toEqual(undefined);
+        done();
+      });
+  });
+
+  it('should set current company by child department id', done => {
+    service.setCurrentCompanyByChildDepartmentId(0);
+    service.getCurrentCompany$()
+      .subscribe(value => {
+        expect(value)
+          .toEqual(testCompany);
+        done();
+      });
+  });
+
+  it('setCurrentCompanyByChildDepartmentId gets null as parameter', done => {
+    companyMapperMock.getParentCompanyOfDepartment$.mockReset();
+    companyMapperMock.getParentCompanyOfDepartment$.mockReturnValue(of(null));
+    service.setCurrentCompanyByChildDepartmentId(0);
+    service.getCurrentCompany$()
+      .subscribe(value => {
+        expect(value)
+          .toEqual(null);
+        done();
+      });
+  });
+
+  it('setCurrentCompanyByChildDepartmentId gets undefined as parameter', done => {
+    companyMapperMock.getParentCompanyOfDepartment$.mockReset();
+    companyMapperMock.getParentCompanyOfDepartment$.mockReturnValue(of(undefined));
+    service.setCurrentCompanyByChildDepartmentId(0);
+    service.getCurrentCompany$()
+      .subscribe(value => {
+        expect(value)
+          .toEqual(undefined);
+        done();
+      });
   });
 });
