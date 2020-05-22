c0d1d41f392a6ac10d8befaf12031ca5ad37678d
dturnschek <dturnschek@brockhaus-ag.de>
2020-05-22 13:06:54 +0200

    refactor(validator): Validator now return typed errors

--- frontend/src/app/shared/validators/date-format-validator-function.ts
+++ frontend/src/app/shared/validators/date-format-validator-function.ts
@@ -3,5 +3,9 @@ import { AbstractControl, ValidatorFn } from '@angular/forms';
 export const dateFormatValidatorFunction: ValidatorFn = (control: AbstractControl): { [key: string]: boolean } => {
   const date: string = control.value;
 
-  if (date === null || date === '') { return { dateFormatError: true }; }
+  if (date === null || date === '') {
+    return {
+      dateFormatError: true
+    };
+  }
 };
