d67dd5a3670e162c6b9c36097d020f7fbc46588e
Robin Janke <robin.janke@brockhaus-ag.de>
2020-05-28 14:13:01 +0200

    feat(StructureController): Added StructureController Signature

--- backend/burning-okr/burning-okr-app/src/main/java/org/burningokr/controller/structure/StructureController.java
+++ backend/burning-okr/burning-okr-app/src/main/java/org/burningokr/controller/structure/StructureController.java
@@ -2,19 +2,19 @@ package org.burningokr.controller.structure;
 
 import lombok.RequiredArgsConstructor;
 import org.burningokr.annotation.RestApiController;
+import org.burningokr.dto.structure.DepartmentDto;
+import org.burningokr.dto.structure.SubStructureDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @RestApiController
 public class StructureController {
 
-  //  private final
-  //
-  //  @GetMapping("/structures/{structureId}")
-  //  public ResponseEntity<DepartmentDto> getStructureByStructureId(
-  //      @PathVariable long structureId) {
-  //    StructureService departmentService =
-  //        departmentServicePicker.getRoleServiceForDepartment(departmentId);
-  //    Department department = departmentService.findById(departmentId);
-  //    return ResponseEntity.ok(departmentMapper.mapEntityToDto(department));
-  //  }
+    @GetMapping("/structures/{structureId}")
+    public ResponseEntity<SubStructureDto> getStructureByStructureId(
+        @PathVariable long structureId) {
+      return ResponseEntity.ok(new DepartmentDto());
+    }
 }
