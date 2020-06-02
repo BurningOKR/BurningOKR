0b2a3edd626f791bd5f1cab36342203f0c04e93c
Robin Janke <robin.janke@brockhaus-ag.de>
2020-06-02 11:31:02 +0200

    test(StructureServiceAdmin): Added test for deleting a Structure which has substructures

--- backend/burning-okr/burning-okr-logic/src/test/java/org/burningokr/service/structure/departmentservices/structureServiceAdminsTest/StructureServiceAdminsTest_CorporateObjectiveStrucutre.java
+++ backend/burning-okr/burning-okr-logic/src/test/java/org/burningokr/service/structure/departmentservices/structureServiceAdminsTest/StructureServiceAdminsTest_CorporateObjectiveStrucutre.java
@@ -1,8 +1,18 @@
 package org.burningokr.service.structure.departmentservices.structureServiceAdminsTest;
 
 import org.burningokr.model.structures.CorporateObjectiveStructure;
+import org.burningokr.model.structures.Department;
+import org.burningokr.model.structures.SubStructure;
+import org.burningokr.service.exceptions.InvalidDeleteRequestException;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class StructureServiceAdminsTest_CorporateObjectiveStrucutre
@@ -16,5 +26,18 @@ public class StructureServiceAdminsTest_CorporateObjectiveStrucutre
   @Override
   protected Class<CorporateObjectiveStructure> getStructureClass() {
     return CorporateObjectiveStructure.class;
+  }
+
+  @Test(expected = InvalidDeleteRequestException.class)
+  public void deleteStructureWithSubStructures_expectsInvalidDeleteRequestException() {
+    CorporateObjectiveStructure corporateObjectiveStructure = new CorporateObjectiveStructure();
+    corporateObjectiveStructure.setId(1337L);
+    List<SubStructure> subStructures = new ArrayList<>();
+    subStructures.add(new Department());
+    corporateObjectiveStructure.setSubStructures(subStructures);
+
+    when(structureRepository.findByIdOrThrow(anyLong())).thenReturn(corporateObjectiveStructure);
+
+    structureServiceAdmins.deleteStructure(1337L, user);
   }
 }
