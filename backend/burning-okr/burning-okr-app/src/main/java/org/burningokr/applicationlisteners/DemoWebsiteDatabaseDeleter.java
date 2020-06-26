2d849fe76bc11efe6f2009e4cc5d97202f457500
Robin Janke <robin.janke@brockhaus-ag.de>
2020-06-26 14:57:25 +0200

    feat(DemoWebsiteDatabaseDeleter): Added example on how to execute sql queries from code.

--- backend/burning-okr/burning-okr-app/src/main/java/org/burningokr/applicationlisteners/DemoWebsiteDatabaseDeleter.java
+++ backend/burning-okr/burning-okr-app/src/main/java/org/burningokr/applicationlisteners/DemoWebsiteDatabaseDeleter.java
@@ -2,16 +2,24 @@ package org.burningokr.applicationlisteners;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
+
 import org.burningokr.repositories.ExtendedRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 
 @Component
 public class DemoWebsiteDatabaseDeleter {
 
   private final WebApplicationContext context;
+  private final EntityManager manager;
+
   private final String[] ignoredRepositories =
       new String[] {
         "adminUserRepository",
@@ -26,9 +34,12 @@ public class DemoWebsiteDatabaseDeleter {
   private LocalDateTime nextDeletionDate;
 
   @Autowired
-  public DemoWebsiteDatabaseDeleter(WebApplicationContext context) {
+  public DemoWebsiteDatabaseDeleter(WebApplicationContext context,
+                                    EntityManagerFactory managerFactory) {
+    this.manager = managerFactory.createEntityManager();
     this.context = context;
     updateNextDeletionDate();
+    // fillDatabase();
   }
 
   @Scheduled(fixedRate = rateInMinutes * 60 * 1000)
@@ -53,5 +64,16 @@ public class DemoWebsiteDatabaseDeleter {
 
   private void updateNextDeletionDate() {
     nextDeletionDate = LocalDateTime.now().plusMinutes(rateInMinutes);
+  }
+
+  // TODO (R.J. 26.06.20) This is just an example on how to execute SQL Scripts in Spring
+  // This should be moved into its own Service and read the SQL string from an SQL File.
+  @Transactional
+  public void fillDatabase() {
+    EntityTransaction transaction = manager.getTransaction();
+    transaction.begin();
+    manager.createNativeQuery("INSERT INTO configuration (id, name, value, type) VALUES (1337, 'Test', 'Test', 'text');")
+        .executeUpdate();
+    transaction.commit();
   }
 }
