c2e46c2865f8c3914a09dfab7d848ff332edcffb
Robin Janke <robin.janke@brockhaus-ag.de>
2020-07-09 09:52:29 +0200

    test(DemoAuthTypeHandler): Fixed test for DemoAuthTypeHandler

--- frontend/src/app/core/auth/services/auth-type-handler/demo-auth-type-handler.service.spec.ts
+++ frontend/src/app/core/auth/services/auth-type-handler/demo-auth-type-handler.service.spec.ts
@@ -1,9 +1,25 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DemoAuthTypeHandlerService } from './demo-auth-type-handler.service';
+import { OAuthService } from 'angular-oauth2-oidc';
+import { Router } from '@angular/router';
+import { FetchingService } from '../../../services/fetching.service';
+
+const oAuthService: any = {};
+
+const router: any = {};
+
+const fetchingService: any = {};
 
 describe('DemoAuthTypeHandlerService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      DemoAuthTypeHandlerService,
+      { provide: OAuthService, useValue: oAuthService },
+      { provide: Router, useValue: router },
+      { provide: FetchingService, useValue: fetchingService }
+    ]
+  }));
 
   it('should be created', () => {
     const service: DemoAuthTypeHandlerService = TestBed.get(DemoAuthTypeHandlerService);
