5ef0ee1ee05420af6c1276b4860b49b118e845d9
Robin Janke <robin.janke@brockhaus-ag.de>
2020-07-15 12:36:44 +0200

    refactor(EnvironmentService): refactored EnvironmentService to be a ConfigurationProperties POJO
    BREAKING CHANGE: You now have to use the auth-mode "azure" instead of
    "aad" in the application.yml
    
    Closes #7

---
+++ backend/burning-okr/burning-okr-logic/src/main/java/org/burningokr/service/environment/AppEnvironment.java
@@ -0,0 +1,22 @@
+package org.burningokr.service.environment;
+
+import javax.validation.constraints.NotEmpty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+@Component
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Validated
+@ConfigurationProperties(prefix = EnvironmentPropertyNames.ENVIRONMENT_PREFIX)
+public class AppEnvironment {
+  private AuthModes authMode;
+}
