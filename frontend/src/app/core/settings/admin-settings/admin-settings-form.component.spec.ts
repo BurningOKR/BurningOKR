199e4b107cf1f56d1b1dcd2e97c608c78102117a
SKoschel <skoschel@arcor.de>
2020-07-17 12:07:03 +0200

    test(AdminSettingsForm): changed test to jest

--- frontend/src/app/core/settings/admin-settings/admin-settings-form.component.spec.ts
+++ frontend/src/app/core/settings/admin-settings/admin-settings-form.component.spec.ts
@@ -7,35 +7,30 @@ import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { DepartmentMapper } from '../../../shared/services/mapper/department.mapper';
 import { I18n } from '@ngx-translate/i18n-polyfill';
 import { i18nMock } from '../../../shared/mocks/i18n-mock';
-import { MatDialogRef } from '@angular/material';
+import { MatDialog, MatDialogRef } from '@angular/material';
 import { OAuthFrontendDetailsService } from '../../auth/services/o-auth-frontend-details.service';
 import { UserSettingsManagerService } from '../../services/user-settings-manager.service';
 import { of } from 'rxjs';
 import { UserSettings } from '../../../shared/model/ui/user-settings';
 import { CompanyUnit } from '../../../shared/model/ui/OrganizationalUnit/company-unit';
 
-const configurationManagerServiceStub: Partial<ConfigurationManagerService> = {
-  getAllConfigurations$: () => of([
-    {
-      id: 0,
-      name: 'config_name',
-      value: 'config_value',
-      type: 'text'
-    }
-  ]),
+const configurationManagerServiceStub: any = {
+  getAllConfigurations$: jest.fn()
 };
-const currentUserServiceStub: Partial<CurrentUserService> = {
-  isCurrentUserAdmin$: () => of(true),
+const currentUserServiceStub: any = {
+  isCurrentUserAdmin$: jest.fn(),
 };
-const oAuthFrontendDetailsServiceStub: Partial<OAuthFrontendDetailsService> = {
-  getAuthType$: () => of('local'),
+const oAuthFrontendDetailsServiceStub: any = {
+  getAuthType$: jest.fn(),
+  isAzureAuthType$: jest.fn()
 };
-const userSettingsManagerServiceStub: Partial<UserSettingsManagerService> = {
-  getUserSettings$: () => of(new UserSettings(0, '', undefined, undefined)),
+const userSettingsManagerServiceStub: any = {
+  getUserSettings$: jest.fn(),
 };
-const companyServiceStub: Partial<CompanyMapper> = {
-  getActiveCompanies$: () => of([new CompanyUnit(0, '', [], [], 0, '')])
+const companyServiceStub: any = {
+  getActiveCompanies$: jest.fn()
 };
+const dialog: any = {};
 
 describe('AdminSettingsForm', () => {
 
@@ -43,6 +38,32 @@ describe('AdminSettingsForm', () => {
   let component: AdminSettingsFormComponent;
 
   beforeEach(() => {
+    configurationManagerServiceStub.getAllConfigurations$.mockReset();
+    currentUserServiceStub.isCurrentUserAdmin$.mockReset();
+    userSettingsManagerServiceStub.getUserSettings$.mockReset();
+    oAuthFrontendDetailsServiceStub.isAzureAuthType$.mockReset();
+    oAuthFrontendDetailsServiceStub.getAuthType$.mockReset();
+    companyServiceStub.getActiveCompanies$.mockReset();
+
+    configurationManagerServiceStub.getAllConfigurations$.mockReturnValue(
+      of([
+        {
+          id: 0,
+          name: 'config_name',
+          value: 'config_value',
+          type: 'text'
+        }
+      ]));
+    currentUserServiceStub.isCurrentUserAdmin$.mockReturnValue(of(true));
+    userSettingsManagerServiceStub.getUserSettings$.mockReturnValue(
+      of(new UserSettings(0, '', undefined, undefined))
+    );
+    oAuthFrontendDetailsServiceStub.isAzureAuthType$.mockReturnValue(of('false'));
+    oAuthFrontendDetailsServiceStub.getAuthType$.mockReturnValue(of('local'));
+    companyServiceStub.getActiveCompanies$.mockReturnValue(
+      of([new CompanyUnit(0, '', [], [], 0, '')])
+    );
+
     TestBed.configureTestingModule(
       {
         declarations: [AdminSettingsFormComponent],
@@ -55,6 +76,7 @@ describe('AdminSettingsForm', () => {
           {provide: MatDialogRef, useValue: {}},
           {provide: OAuthFrontendDetailsService, useValue: oAuthFrontendDetailsServiceStub},
           {provide: UserSettingsManagerService, useValue: userSettingsManagerServiceStub},
+          {provide: MatDialog, useValue: dialog},
         ],
         schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
       })
