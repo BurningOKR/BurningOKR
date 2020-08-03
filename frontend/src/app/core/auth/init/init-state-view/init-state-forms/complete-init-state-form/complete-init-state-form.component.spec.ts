bbdbfbb0e1ec02cc3ebc6922ab09eaa1761f2ddd
Robin Janke <robin.janke@brockhaus-ag.de>
2020-08-03 11:51:43 +0200

    test(CompleteInitStateFormComponent): Fixed Test

--- frontend/src/app/core/auth/init/init-state-view/init-state-forms/complete-init-state-form/complete-init-state-form.component.spec.ts
+++ frontend/src/app/core/auth/init/init-state-view/init-state-forms/complete-init-state-form/complete-init-state-form.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CompleteInitStateFormComponent } from './complete-init-state-form.component';
+import { MaterialTestingModule } from '../../../../../../testing/material-testing.module';
 
 describe('CompleteInitStateFormComponent', () => {
   let component: CompleteInitStateFormComponent;
@@ -8,7 +9,8 @@ describe('CompleteInitStateFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CompleteInitStateFormComponent ]
+      declarations: [ CompleteInitStateFormComponent ],
+      imports: [ MaterialTestingModule ]
     })
     .compileComponents();
   }));
