a17f1586bb162658d17b17d88fd86ccddfe5eca1
Robin Janke <robin.janke@brockhaus-ag.de>
2020-08-05 11:56:10 +0200

    fix(Language): Fixed mixed language bug

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -46,7 +46,7 @@ declare const require: any;
 registerLocaleData(localeEn, 'en', localeEnExtra);
 registerLocaleData(localeEn, 'de', localeEnExtra);
 
-const currentLanguage: string = 'en';
+const currentLanguage: string = 'de';
 
 @NgModule({
   declarations: [
