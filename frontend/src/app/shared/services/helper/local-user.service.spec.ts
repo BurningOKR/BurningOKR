cebd6f95a2b9b59617affbdc7e63b061e35cc11f
Robin Janke <robin.janke@brockhaus-ag.de>
2020-10-01 13:09:02 +0200

    test(LocalUserServiceTest): fixed tests

--- frontend/src/app/shared/services/helper/local-user.service.spec.ts
+++ frontend/src/app/shared/services/helper/local-user.service.spec.ts
@@ -3,15 +3,18 @@ import { TestBed } from '@angular/core/testing';
 import { LocalUserService } from './local-user.service';
 import { LocalUserApiService } from '../api/local-user-api.service';
 import { UserService } from './user.service';
+import { FetchingService } from '../../../core/services/fetching.service';
 
 const localUserApiServiceMock: any = {};
 const userServiceMock: any = {};
+const fetchingServiceMock: any = {};
 
 describe('LocalUserService', () => {
   beforeEach(() => TestBed.configureTestingModule({
     providers: [
       { provide: LocalUserApiService, useValue: localUserApiServiceMock },
-      { provide: UserService, useValue: userServiceMock }
+      { provide: UserService, useValue: userServiceMock },
+      { provide: FetchingService, useValue: fetchingServiceMock }
     ]
   }));
 
