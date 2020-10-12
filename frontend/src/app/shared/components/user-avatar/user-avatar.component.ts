4d53033fd9216b88c2344b425137631a592393e2
Robin Janke <robin.janke@brockhaus-ag.de>
2020-10-12 14:07:09 +0200

    fix(Avatar): The avatar component will now always rerender so that it does not display a wrong profile picture.

--- frontend/src/app/shared/components/user-avatar/user-avatar.component.ts
+++ frontend/src/app/shared/components/user-avatar/user-avatar.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { User } from '../../model/api/user';
 
 @Component({
@@ -6,10 +6,17 @@ import { User } from '../../model/api/user';
   templateUrl: './user-avatar.component.html',
   styleUrls: ['./user-avatar.component.css']
 })
-export class UserAvatarComponent {
+export class UserAvatarComponent implements OnChanges {
   @Input() user: User;
   @Input() size = 64;
   @Input() bgColor = '';
   @Input() clickable = false;
   @Output() clickOnAvatar: EventEmitter<any> = new EventEmitter<any>();
+
+  render: boolean = true;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    setTimeout(() => this.render = false);
+    setTimeout(() => this.render = true);
+  }
 }
