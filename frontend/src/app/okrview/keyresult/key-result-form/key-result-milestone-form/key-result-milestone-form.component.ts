159bf0853751bfa8277bbc55adf0d5d042e0503c
Niklas Lurse <niklas.lurse@icloud.com>
2020-10-19 10:39:26 +0200

    fix(tslint): fixed tslint

--- frontend/src/app/okrview/keyresult/key-result-form/key-result-milestone-form/key-result-milestone-form.component.ts
+++ frontend/src/app/okrview/keyresult/key-result-form/key-result-milestone-form/key-result-milestone-form.component.ts
@@ -1,30 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import {FormArray, FormControl, FormGroup} from "@angular/forms";
+import { Component } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-key-result-milestone-form',
   templateUrl: './key-result-milestone-form.component.html',
   styleUrls: ['./key-result-milestone-form.component.scss']
 })
-export class KeyResultMilestoneFormComponent implements OnInit {
+export class KeyResultMilestoneFormComponent {
 
   isChecked: boolean = false;
   milestoneForm: FormArray = new FormArray([]);
 
-  addMilestone() {
+  addMilestone(): void {
     this.milestoneForm.controls.push(new FormGroup({
       nameControl: new FormControl(),
       targetValueControl: new FormControl(),
     }));
   }
 
-  deleteMilestone(milestoneForm: FormGroup) {
+  deleteMilestone(milestoneForm: FormGroup): void {
     const index: number = this.milestoneForm.controls.findIndex(control => control === milestoneForm);
     this.milestoneForm.removeAt(index);
-  }
-
-  constructor() { }
-
-  ngOnInit() {
   }
 }
