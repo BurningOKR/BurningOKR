6b98d6a32bc5b5629fd8fb52c63f68fac07c749d
Robin Janke <robin.janke@brockhaus-ag.de>
2020-11-05 13:56:43 +0100

    feat(MilestoneSliderWrapper): Added Wrapper for slider to show ticks of milestones

---
+++ frontend/src/app/shared/components/milestone-slider-wrapper/milestone-slider-wrapper.component.ts
@@ -0,0 +1,34 @@
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { ViewKeyResultMilestone } from '../../model/ui/view-key-result-milestone';
+import { ViewKeyResult } from '../../model/ui/view-key-result';
+
+@Component({
+  selector: 'app-milestone-slider-wrapper',
+  templateUrl: './milestone-slider-wrapper.component.html',
+  styleUrls: ['./milestone-slider-wrapper.component.scss']
+})
+export class MilestoneSliderWrapperComponent implements AfterViewInit {
+
+  @Input() keyResult: ViewKeyResult;
+
+  @ViewChild('wrapper', { static: true }) wrapper: ElementRef;
+
+  private readonly SLIDER_BORDER = 8; // The border width of the slider in px
+  private readonly MILESTONE_TICK_OFFSET = 12; // The offset of the ticks so that they fit on the slider
+  private readonly MILESTONE_TICK_WIDTH = 6; // The width of a tick
+
+  private width: number = 0;
+
+  ngAfterViewInit(): void {
+    this.width = this.wrapper.nativeElement.clientWidth - this.SLIDER_BORDER * 2;
+  }
+
+  getMilestoneLeftPxValue(milestone: ViewKeyResultMilestone): number {
+    return Math.ceil(
+      ((milestone.value - this.keyResult.start) / (this.keyResult.end - this.keyResult.start))
+      * this.width
+      + this.MILESTONE_TICK_OFFSET
+      - this.MILESTONE_TICK_WIDTH / 2
+    );
+  }
+}
