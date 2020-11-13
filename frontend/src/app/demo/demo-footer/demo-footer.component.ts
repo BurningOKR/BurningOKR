85f79bf0ed590775b077189c379867d2afbfb9b7
simonlet <skoschel@arcor.de>
2020-11-13 10:08:33 +0100

    refactor(Demo Footer): change style of demo footer

--- frontend/src/app/demo/demo-footer/demo-footer.component.ts
+++ frontend/src/app/demo/demo-footer/demo-footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-demo-footer',
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./demo-footer.component.scss']
 })
 export class DemoFooterComponent {
+  @ViewChild('footer', {static: true}) footer: ElementRef;
 
+  @HostListener('window:scroll')
+  func(): void {
+    this.footer.nativeElement.style.position = 'static';
+    if (this.footer.nativeElement.offsetTop < document.documentElement.scrollTop + document.body.scrollTop
+      + document.documentElement.offsetHeight - this.footer.nativeElement.offsetHeight) {
+          this.footer.nativeElement.style.position = 'fixed';
+    }
+    // var foot = document.getElementById('footer');
+    // foot.style.position = "static";
+    // if( foot.offsetTop < document.documentElement.scrollTop + document.body.scrollTop + document.documentElement.offsetHeight - foot.offsetHeight)
+    //     foot.style.position = "fixed";
+    // })();
+  }
 }
