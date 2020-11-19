802bd93fabbdd5dd8934531d85dda6e9dbd2be8a
Dennis Männel <dennis.maennel001@stud.fh-dortmund.de>
2020-11-19 16:27:47 +0100

    feat(Application): add google analytics code snipped for tracking and update klaro.js config.

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -4,8 +4,8 @@
 // <script src="klaro.js" data-config="myConfigVariableName" />
 // You can also disable auto-loading of the consent notice by adding
 // data-no-auto-load=true to the script tag.
-var beschreibungtext= 'Die BROCKHAUS AG verwendet Cookies, um Ihnen ein möglichst optimales und auf Ihre Bedürfnisse zugeschnittenes Webseiten-​Erlebnis zu bieten. Dazu zählen Cookies, die für den reibungslosen Betrieb der Seite und deren Sicherheit notwendig sind sowie solche, die zu anonymen Statistikzwecken genutzt werden. Sie können selbst entscheiden, welche der Kategorien Sie erlauben möchten.';
-if(window.innerWidth <= 625) {
+var beschreibungtext = 'Die BROCKHAUS AG verwendet Cookies, um Ihnen ein möglichst optimales und auf Ihre Bedürfnisse zugeschnittenes Webseiten-​Erlebnis zu bieten. Dazu zählen Cookies, die für den reibungslosen Betrieb der Seite und deren Sicherheit notwendig sind sowie solche, die zu anonymen Statistikzwecken genutzt werden. Sie können selbst entscheiden, welche der Kategorien Sie erlauben möchten.';
+if (window.innerWidth <= 625) {
     beschreibungtext = '';// 'Die BROCKHAUS AG verwendet Cookies, um Ihnen ein möglichst optimales Webseiten-​Erlebnis zu bieten.';
 }
 
@@ -17,18 +17,32 @@ var klaroConfig = {
     privacyPolicy: '/demo/privacy-policy',
     default: false,
     mustConsent: true,
-    acceptAll: true,
     lang: 'de',
+    acceptAll: true,
     hideDeclineAll: false,
+    hideLearnMore: false,
+    acceptSelected: true,
     translations: {
         de: {
-            acceptAll: 'Einverstanden',
+            acceptAll: 'Allen Cookies zustimmen',
+            acceptSelected:  'Ausgewählten Cookies zustimmen',
+            decline: 'Optionale Cookies ablehnen',
+
             consentModal: {
                 description: beschreibungtext
             },
+            analytics: {
+                title: 'Google Analytics',
+                description: 'Nutzerverhalten analysieren',
+            },
             authentication: {
+                title: 'Authentifizierung',
                 description: 'Authentifizierung für automatische Anmeldung'
             },
+            purposes: {
+                analytics: '',
+                authentication:''
+            }
         }
     },
 
@@ -36,9 +50,19 @@ var klaroConfig = {
         {
             name: 'authentication',
             title: 'Authentifizierung',
-            purposes: ['security'],
+            //purposes: ['security'],
             default: true,
             required: true
-        }
+        },
+        {
+            name: 'analytics',
+            title: 'Google Analytics',
+            //purposes: ['analytics'],
+            cookies: [
+                '/_g(\d|\w|.)*$/'
+            ],
+            required: false,
+            onlyOnce: true,
+        },
     ],
 };
