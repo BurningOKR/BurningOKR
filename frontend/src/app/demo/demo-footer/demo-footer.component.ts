01dcf2c5cdd335644f77af5684ee7a7b9e7cc5ff
jages <16306035+jages@users.noreply.github.com>
2020-12-04 14:07:02 +0100

    footer working as intendet

--- frontend/src/app/demo/demo-footer/demo-footer.component.ts
+++ frontend/src/app/demo/demo-footer/demo-footer.component.ts
@@ -5,20 +5,6 @@ import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@
   templateUrl: './demo-footer.component.html',
   styleUrls: ['./demo-footer.component.scss']
 })
-export class DemoFooterComponent implements AfterViewInit {
+export class DemoFooterComponent {
   @ViewChild('footer', {static: true}) footer: ElementRef;
-
-  // @HostListener('window:scroll')
-  // repositionFooter(): void {
-  //   this.footer.nativeElement.style.position = 'static';
-  //   if (this.footer.nativeElement.offsetTop < document.documentElement.scrollTop + document.body.scrollTop
-  //     + document.documentElement.offsetHeight - this.footer.nativeElement.offsetHeight) {
-  //         this.footer.nativeElement.style.position = 'fixed';
-  //   }
-  // }
-
-  ngAfterViewInit(): void {
-    //this.repositionFooter();
-    console.log('logging');
-  }
 }
