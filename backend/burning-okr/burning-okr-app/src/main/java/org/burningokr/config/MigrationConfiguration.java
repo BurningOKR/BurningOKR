3e196049cde3eb6ce7d1c0b83a1715e2fc8c7595
Robin Janke <robin.janke@brockhaus-ag.de>
2020-12-04 08:49:52 +0100

    fix(Migrations): began fixing migrations, so that updates are less problematic later

--- backend/burning-okr/burning-okr-app/src/main/java/org/burningokr/config/MigrationConfiguration.java
+++ backend/burning-okr/burning-okr-app/src/main/java/org/burningokr/config/MigrationConfiguration.java
@@ -1,6 +1,9 @@
 package org.burningokr.config;
 
 import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.MigrationInfo;
+import org.flywaydb.core.api.MigrationInfoService;
+import org.flywaydb.core.api.MigrationVersion;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +15,8 @@ public class MigrationConfiguration {
   /** Override default flyway initializer to do nothing. */
   @Bean
   FlywayMigrationInitializer flywayInitializer(Flyway flyway) {
+    MigrationInfo version = flyway.info().current();
+    //flyway.setBaselineVersionAsString("52");
     return new FlywayMigrationInitializer(flyway, (f) -> {});
   }
 
