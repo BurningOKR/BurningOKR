e58a028b748322f1fc300cde4153210bd23ceeb3
jages <16306035+jages@users.noreply.github.com>
2020-12-04 11:31:49 +0100

    remove static footer intrusion

--- frontend/src/app/demo/demo-footer/demo-footer.component.ts
+++ frontend/src/app/demo/demo-footer/demo-footer.component.ts
@@ -1,24 +1,24 @@
-import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-demo-footer',
   templateUrl: './demo-footer.component.html',
   styleUrls: ['./demo-footer.component.scss']
 })
-export class DemoFooterComponent {
+export class DemoFooterComponent implements AfterViewInit {
   @ViewChild('footer', {static: true}) footer: ElementRef;
 
-  @HostListener('window:scroll')
-  func(): void {
-    this.footer.nativeElement.style.position = 'static';
-    if (this.footer.nativeElement.offsetTop < document.documentElement.scrollTop + document.body.scrollTop
-      + document.documentElement.offsetHeight - this.footer.nativeElement.offsetHeight) {
-          this.footer.nativeElement.style.position = 'fixed';
-    }
-    // var foot = document.getElementById('footer');
-    // foot.style.position = "static";
-    // if( foot.offsetTop < document.documentElement.scrollTop + document.body.scrollTop + document.documentElement.offsetHeight - foot.offsetHeight)
-    //     foot.style.position = "fixed";
-    // })();
+  // @HostListener('window:scroll')
+  // repositionFooter(): void {
+  //   this.footer.nativeElement.style.position = 'static';
+  //   if (this.footer.nativeElement.offsetTop < document.documentElement.scrollTop + document.body.scrollTop
+  //     + document.documentElement.offsetHeight - this.footer.nativeElement.offsetHeight) {
+  //         this.footer.nativeElement.style.position = 'fixed';
+  //   }
+  // }
+
+  ngAfterViewInit(): void {
+    //this.repositionFooter();
+    console.log('logging');
   }
 }
