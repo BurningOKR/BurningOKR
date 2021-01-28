101b25584aef9a5bd867debd5bc892f89086f390
dmaennel <dmaennel@brockhaus-ag.de>
2021-01-28 11:14:26 +0100

    refactor(taskboard): change design informations, change toggle button for views to switch buttons, change structure for view components, change event concept, load key result data in taskboard component

--- frontend/src/app/okrview/okr-child-unit/department/department-tab-taskboard/department-tab-taskboard.component.ts
+++ frontend/src/app/okrview/okr-child-unit/department/department-tab-taskboard/department-tab-taskboard.component.ts
@@ -1,81 +1,153 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material';
-import { forkJoin, Observable, Subscription } from 'rxjs';
-import { filter, map, switchMap, take } from 'rxjs/operators';
-import { CycleAdminCardComponent } from 'src/app/cycle-admin/cycle-admin-card/cycle-admin-card.component';
-
+import { I18n } from '@ngx-translate/i18n-polyfill';
+import { RxStompService } from '@stomp/ng2-stompjs';
+import { BehaviorSubject, forkJoin, Observable, Subscription } from 'rxjs';
+import { filter, map, take, tap } from 'rxjs/operators';
+import { TaskBoardViewEventService } from 'src/app/okrview/taskboard-services/task-board-view-event.service';
+import { ConfirmationDialogData, ConfirmationDialogComponent } from 'src/app/shared/components/confirmation-dialog/confirmation-dialog.component';
+import { TaskDto } from 'src/app/shared/model/api/task.dto';
+import { ViewTaskBoardEvent } from 'src/app/shared/model/events/view-taskboard-event';
 import { ContextRole } from 'src/app/shared/model/ui/context-role';
 import { CycleUnit } from 'src/app/shared/model/ui/cycle-unit';
 import { OkrChildUnit } from 'src/app/shared/model/ui/OrganizationalUnit/okr-child-unit';
-import { StateTaskMap } from 'src/app/shared/model/ui/state-task-map';
-import { ViewTask } from 'src/app/shared/model/ui/view-task';
-import { ViewTaskState } from 'src/app/shared/model/ui/view-task-state';
-import { TaskService } from 'src/app/shared/services/helper/task.service';
-import { TaskStateMapperService } from 'src/app/shared/services/mapper/task-state.mapper';
+import { ViewTask } from 'src/app/shared/model/ui/taskboard/view-task';
+import { ViewTaskState } from 'src/app/shared/model/ui/taskboard/view-task-state';
+import { TaskBoardGeneralHelper } from 'src/app/shared/services/helper/task-board/task-board-general-helper';
+import { KeyResultMapper } from 'src/app/shared/services/mapper/key-result.mapper';
+import { TaskStateMapper } from 'src/app/shared/services/mapper/task-state.mapper';
 import { TaskMapperService } from 'src/app/shared/services/mapper/task.mapper';
+
 import { TaskFormComponent, TaskFormData } from '../department-tab-task-form/department-tab-task-form.component';
-
-
+import { TaskBoardDragDropEvent } from './taskboard-column/taskboard-column.component';
 
 @Component({
   selector: 'app-department-tab-taskboard',
   templateUrl: './department-tab-taskboard.component.html',
   styleUrls: ['./department-tab-taskboard.component.css']
 })
-export class DepartmentTabTaskboardComponent implements OnInit {
+export class DepartmentTabTaskboardComponent implements OnInit, OnDestroy {
   @Input() childUnit: OkrChildUnit;
   @Input() currentUserRole: ContextRole;
   @Input() cycle: CycleUnit;
 
-  stateTaskMap: StateTaskMap[] = [];
+  showSwimlanes: boolean = false;
+  viewDataEmitter$: BehaviorSubject<ViewTaskBoardEvent> = new BehaviorSubject(null);
 
+  viewData: ViewTaskBoardEvent = new ViewTaskBoardEvent();
   subscriptions: Subscription[];
 
   constructor(
     private taskMapperService: TaskMapperService,
-    private taskStateMapperService: TaskStateMapperService,
-    private taskHelper: TaskService,
-    private matDialog: MatDialog) { }
+    private taskStateMapper: TaskStateMapper,
+    private taskHelper: TaskBoardGeneralHelper,
+    private matDialog: MatDialog,
+    private taskBoardEventService: TaskBoardViewEventService,
+    private keyResultMapper: KeyResultMapper,
+    private stompService: RxStompService,
+    private i18n: I18n) { }
 
   ngOnInit(): void {
     this.subscriptions = [];
     this.subscriptions.push(
       forkJoin({
         tasks$: this.taskMapperService.getTasksForOkrUnit$(this.childUnit.id),
-        states$: this.taskStateMapperService.getTaskStates$(this.childUnit.id)
+        states$: this.taskStateMapper.getTaskStates$(this.childUnit.id),
+        keyResults$: this.keyResultMapper.getKeyResultsForOkrUnit(this.childUnit.id)
       })
-        .subscribe(result => this.initialise(result["states$"], result["tasks$"]))
+        .subscribe(result => {
+          this.viewData.tasks.push(...result["tasks$"]);
+          this.viewData.taskStates.push(...result["states$"]);
+          this.viewData.keyResults.push(...result['keyResults$']);
+
+          this.viewData.tasks = this.taskHelper.orderTaskList(this.viewData.tasks);
+          console.log("after sorting");
+          console.log(this.viewData);
+
+          this.viewDataEmitter$.next(this.viewData);
+        })
     );
+
+    this.subscriptions.push(
+      this.taskBoardEventService.taskAddButtonClick$.subscribe(task => this.onTaskAddButtonClick(task)),
+      this.taskBoardEventService.taskDeleteButtonClick$.subscribe(task => this.onTaskDeleteButtonClick(task)),
+      this.taskBoardEventService.taskUpdateButtonClick$.subscribe(task => this.onTaskUpdateButtonClick(task)),
+      this.taskBoardEventService.taskMovedInView$.subscribe(dropEvent => this.onTaskDragAndDrop(dropEvent)),
+      this.stompService.watch({ destination: `/topic/unit/${this.childUnit.id}/tasks` })
+        .pipe(
+          map(taskDtosMessage => {
+            const newAndUpdatedTasksDtos: TaskDto[] = JSON.parse(taskDtosMessage.body);
+            const newAndUpdatedTasks: ViewTask[] = newAndUpdatedTasksDtos.map(this.taskMapperService.mapToViewTask);
+
+            return newAndUpdatedTasks;
+          })
+        )
+        .subscribe(newAndUpdatedTasks => {
+          console.log("Websocket Topic");
+          console.log(newAndUpdatedTasks);
+          console.log(this.viewData);
+
+          if (newAndUpdatedTasks && newAndUpdatedTasks[0]) {
+            this.taskHelper.mergeTaskListWithNewOrUpdatedElements(this.viewData.tasks, newAndUpdatedTasks);
+            this.viewData.tasks= this.taskHelper.orderTaskList(this.viewData.tasks);
+            console.log('after merge');
+            console.log(this.viewData);
+            this.viewDataEmitter$.next(this.viewData);
+          }
+        }),
+      this.stompService.watch({ destination: `/topic/unit/${this.childUnit.id}/tasks/deleted` })
+        .pipe(
+          map(taskDtosMessage => {
+            const deletedTasksDto: TaskDto = JSON.parse(taskDtosMessage.body);
+            const deletedTask: ViewTask = this.taskMapperService.mapToViewTask(deletedTasksDto);
+
+            return deletedTask;
+          })
+        )
+        .subscribe(task => {
+          console.log("delete Topic");
+          this.viewData.tasks = this.taskHelper.removeTaskAndUpdateTaskList(this.viewData.tasks, task);
+          this.viewDataEmitter$.next(this.viewData);
+        })
+    );
+
+    this.stompService.activate();
+    this.stompService.webSocketErrors$.subscribe(error => console.log(error));
   }
 
-  initialise(states: ViewTaskState[], tasks: ViewTask[]): void {
-    this.stateTaskMap.push(...this.taskHelper.generateStateTaskMapList(states, tasks));
+  ngOnDestroy(): void {
+    console.log("taskboard wird abgebaut");
+    for (const subscription of this.subscriptions) {
+      subscription.unsubscribe();
+    }
+    this.stompService.deactivate();
   }
 
-  onTaskUpdate(task: ViewTask): void {
-    const oldStateId: number = task.stateId;
-    const states: ViewTaskState[] = this.taskHelper.extractStates(this.stateTaskMap);
+  viewToggleChanged(): void {
+    this.showSwimlanes = !this.showSwimlanes;
+  }
+
+  onTaskUpdateButtonClick(task: ViewTask): void {
+    const states: ViewTaskState[] = this.viewData.taskStates;
     const formData: TaskFormData = {
-      unitId: this.childUnit.id, defaultState: states.find(state => state.id === task.stateId), states, task
+      unitId: this.childUnit.id, defaultState: states.find(state => state.id === task.taskStateId), states, task
     };
 
     const updatedTask$: Observable<ViewTask> = this.openDialog$(formData);
     this.subscriptions.push(
       updatedTask$.pipe(
-        switchMap(updatedTask => this.taskMapperService.updateTask$(this.childUnit.id, updatedTask)),
-      )
-        .subscribe(referencedTask => {
-          if (oldStateId !== referencedTask.stateId) {
-            this.removeTaskFromList(referencedTask, oldStateId);
-          }
-          this.addOrUpdateTaskInList(referencedTask);
+        tap(task => {
+          console.log("updated task before send");
+          console.log(task);
+          this.stompService.publish({ destination: `/ws/unit/${this.childUnit.id}/tasks/update`, body: JSON.stringify(task) });
         })
+      ).subscribe()
     );
-
   }
 
-  onTaskAdd(taskState: ViewTaskState): void {
-    const states: ViewTaskState[] = this.taskHelper.extractStates(this.stateTaskMap);
+  onTaskAddButtonClick(taskState: ViewTaskState): void {
+    const states: ViewTaskState[] = this.viewData.taskStates;
     const formData: TaskFormData = {
       unitId: this.childUnit.id, defaultState: taskState, states
     };
@@ -83,13 +155,77 @@ export class DepartmentTabTaskboardComponent implements OnInit {
     const newTask$: Observable<ViewTask> = this.openDialog$(formData);
     this.subscriptions.push(
       newTask$.pipe(
-        switchMap(task => {
-          return this.taskMapperService.createTaskForOkrUnit$(this.childUnit.id, task)
-        }
-        ),
+        tap(task => {
+          // task.previousTaskId = this.taskHelper.getTaskIdForNewTask(this.stateTaskMap, task.taskStateId);
+          this.stompService.publish({ destination: `/ws/unit/${this.childUnit.id}/tasks/add`, body: JSON.stringify(task) });
+        }),
       )
-        .subscribe(task => { this.addOrUpdateTaskInList(task); })
+        .subscribe()
     );
+  }
+
+  onTaskDeleteButtonClick(task: ViewTask): void {
+    console.log("onTaskDelete");
+    const title: string = this.i18n({
+      id: 'deleteTaskDialog_title',
+      value: 'Aufgabe löschen'
+    });
+    const message: string =
+      this.i18n({
+        id: 'deleteTaskDialog_message',
+        value: 'Soll die Aufgabe "{{title}}" wirklich gelöscht werden?'
+      }, { title: task.title });
+    const confirmButtonText: string = this.i18n({
+      id: 'deleteButtonText',
+      description: 'deleteButtonText',
+      value: 'Löschen'
+    });
+
+    const formData: ConfirmationDialogData = {
+      title, message, confirmButtonText
+    };
+
+    this.subscriptions.push(
+      this.openConfirmationDialog$(formData)
+        .pipe(
+          tap(_ => {
+            this.stompService.publish({ destination: `/ws/unit/${this.childUnit.id}/tasks/delete`, body: JSON.stringify(task) });
+          }),
+        )
+        .subscribe()
+    );
+  }
+
+
+  moveTaskInList(currentList: ViewTask[], movedAndUpdatedTask: ViewTask): ViewTask[] {
+    let copiedTasks: ViewTask[] = this.taskHelper.copyTaskList(currentList);
+    copiedTasks = this.taskHelper.removeTaskAndUpdateTaskList(copiedTasks, movedAndUpdatedTask);
+    copiedTasks = this.taskHelper.addTaskAndUpdateTaskList(copiedTasks, movedAndUpdatedTask);
+
+    return copiedTasks;
+  }
+
+  onTaskDragAndDrop(movedAndUpdatedTask: ViewTask): void {
+    console.log("drop Task");
+    console.log(movedAndUpdatedTask);
+
+    this.viewData.tasks = this.moveTaskInList(this.viewData.tasks, movedAndUpdatedTask);
+
+    console.log('update global tasklist after moving task');
+    console.log(this.viewData.tasks);
+
+    this.viewDataEmitter$.next(this.viewData);
+
+
+    const updatedTaskDto = this.taskMapperService.mapToTaskDTO(movedAndUpdatedTask);
+    console.log("Task moved");
+    console.log(updatedTaskDto);
+
+
+    this.stompService.publish({
+      destination: `/ws/unit/${this.childUnit.id}/tasks/update`,
+      body: JSON.stringify(updatedTaskDto)
+    });
   }
 
   private openDialog$(formData: TaskFormData): Observable<ViewTask> {
@@ -102,44 +238,15 @@ export class DepartmentTabTaskboardComponent implements OnInit {
       );
   }
 
-  onTaskDelete(task: ViewTask): void {
-    console.log("removeTaskFromList");
-    this.subscriptions.push(
-      this.taskMapperService.deleteTask$(this.childUnit.id, task.id)
-        .subscribe(_ => this.removeTaskFromList(task, task.stateId))
-    );
-  }
+  private openConfirmationDialog$(formData: ConfirmationDialogData): Observable<boolean> {
+    const dialogReference: MatDialogRef<ConfirmationDialogComponent, boolean> = this.matDialog
+      .open(ConfirmationDialogComponent, { data: formData });
 
-  onTaskMoved($event: ViewTask) {
-    this.subscriptions.push(
-      this.taskMapperService.updateTask$(this.childUnit.id, $event)
-        .subscribe(task => {
-          console.log("updated Task in Database");
-          console.log(task);
-          this.addOrUpdateTaskInList(task);
-        })
-    );
-  }
-
-  removeTaskFromList(task: ViewTask, oldStateId: number): void {
-    console.log("removeTaskFromList");
-    const index: number = this.taskHelper.getListIndex(oldStateId, this.stateTaskMap);
-    if (index >= 0) {
-      const taskIndex: number = this.stateTaskMap[index].tasks.findIndex(listTask => listTask.id === task.id);
-      this.stateTaskMap[index].tasks.splice(taskIndex, 1);
-    }
-  }
-
-  addOrUpdateTaskInList(task: ViewTask): void {
-    console.log("addOrUpdateTaskInList");
-    const mapIndex: number = this.taskHelper.getListIndex(task.stateId, this.stateTaskMap);
-    if (mapIndex >= 0) {
-      if (this.taskHelper.isTaskInList(task, this.stateTaskMap[mapIndex].tasks)) {
-        const taskIndex: number = this.stateTaskMap[mapIndex].tasks.findIndex(listTask => listTask.id === task.id);
-        this.stateTaskMap[mapIndex].tasks.splice(taskIndex, 1, task);
-      } else {
-        this.stateTaskMap[mapIndex].tasks.push(task);
-      }
-    }
+    return dialogReference
+      .afterClosed()
+      .pipe(
+        take(1),
+        filter(value => value)
+      );
   }
 }
