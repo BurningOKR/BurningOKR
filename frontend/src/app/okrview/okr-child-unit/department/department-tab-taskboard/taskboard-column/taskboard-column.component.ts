1dcb2c40d7c59f878e21c1a7a21df7e540972006
dmaennel <dmaennel@brockhaus-ag.de>
2021-01-28 11:17:52 +0100

    refactor(task-board-column): change design, change event concept, add id attribute

--- frontend/src/app/okrview/okr-child-unit/department/department-tab-taskboard/taskboard-column/taskboard-column.component.ts
+++ frontend/src/app/okrview/okr-child-unit/department/department-tab-taskboard/taskboard-column/taskboard-column.component.ts
@@ -1,12 +1,25 @@
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
-import { MatDialog, MatDialogRef } from '@angular/material';
-import { Subscription } from 'rxjs';
-import { filter, switchMap, take } from 'rxjs/operators';
-import { OkrChildUnit } from 'src/app/shared/model/ui/OrganizationalUnit/okr-child-unit';
-import { ViewTask } from 'src/app/shared/model/ui/view-task';
-import { ViewTaskState } from 'src/app/shared/model/ui/view-task-state';
-import { TaskFormComponent } from '../../department-tab-task-form/department-tab-task-form.component';
+import { CdkDragDrop } from "@angular/cdk/drag-drop";
+import { Component, OnInit, OnDestroy, Input, Output, EventEmitter } from "@angular/core";
+
+import { Subscription } from "rxjs";
+import { TaskBoardViewEventService } from "src/app/okrview/taskboard-services/task-board-view-event.service";
+import { TaskStateId } from "src/app/shared/model/id-types";
+import { StateTaskMap } from "src/app/shared/model/ui/taskboard/state-task-map";
+import { ViewTask } from "src/app/shared/model/ui/taskboard/view-task";
+import { ViewTaskState } from "src/app/shared/model/ui/taskboard/view-task-state";
+import { ViewKeyResult } from "src/app/shared/model/ui/view-key-result";
+import { TaskBoardStateColumnViewHelper } from "src/app/shared/services/helper/task-board/task-board-state-column-view-helper";
+
+
+
+export interface MovedTaskUpdater {
+  updateTaskListOnNewPosition();
+}
+
+export interface TaskBoardDragDropEvent {
+  state: ViewTaskState;
+  $event: CdkDragDrop<ViewTask[]>;
+}
 
 @Component({
   selector: 'app-taskboard-column',
@@ -14,22 +27,16 @@ import { TaskFormComponent } from '../../department-tab-task-form/department-tab
   styleUrls: ['./taskboard-column.component.css']
 })
 export class TaskboardColumnComponent implements OnInit, OnDestroy {
-  @Input() state: ViewTaskState;
-  @Input() public taskList: ViewTask[] = [];
-  @Input() childUnit: OkrChildUnit;
-
-  @Output() createTask: EventEmitter<ViewTaskState> = new EventEmitter();
-  @Output() updateTask: EventEmitter<ViewTask> = new EventEmitter();
-  @Output() deleteTask: EventEmitter<ViewTask> = new EventEmitter();
-  @Output() movedTask: EventEmitter<ViewTask> = new EventEmitter();
+  @Input() public map: StateTaskMap;
+  @Input() public id: number;
 
   subscriptions: Subscription[] = [];
 
-  constructor(private matDialog: MatDialog) { }
+  constructor(
+    private taskBoardEventService: TaskBoardViewEventService,
+  ) { }
 
-  ngOnInit(): void {
-    console.log(this.taskList);
-  }
+  ngOnInit(): void { }
 
   ngOnDestroy(): void {
     for (const subscription of this.subscriptions) {
@@ -38,29 +45,24 @@ export class TaskboardColumnComponent implements OnInit, OnDestroy {
   }
 
   createNewTask(): void {
-    this.createTask.emit(this.state);
+    console.log("Column: create Task Button Click");
+    this.taskBoardEventService.taskAddButtonClick$.next(this.map.state);
   }
 
-  openTask(task: ViewTask): void {
-    this.updateTask.emit(task);
+  updateOrViewTask(task: ViewTask): void {
+    this.taskBoardEventService.taskUpdateButtonClick$.next(task);
   }
 
   dropTask($event: CdkDragDrop<ViewTask[]>): void {
-    console.log("drop Task");
-    console.log($event);
-    if ($event.previousContainer === $event.container) {
-      moveItemInArray($event.container.data, $event.previousIndex, $event.currentIndex);
-    } else {
-      transferArrayItem($event.previousContainer.data,
-        $event.container.data,
-        $event.previousIndex,
-        $event.currentIndex);
-    }
-    $event.container.data[$event.currentIndex].stateId = this.state.id;
-    this.movedTask.emit($event.container.data[$event.currentIndex]);
+    const dropEvent: TaskBoardDragDropEvent = {
+      $event,
+      state: this.map.state,
+    };
+    this.taskBoardEventService.taskDragAndDropInView$.next(dropEvent);
   }
 
+
   onDelete(task: ViewTask) {
-    this.deleteTask.emit(task);
+    this.taskBoardEventService.taskDeleteButtonClick$.next(task);
   }
 }
