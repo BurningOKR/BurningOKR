4c87659caa0edf4244c50f8e9535c5a7e03df522
dmaennel <dmaennel@brockhaus-ag.de>
2021-01-28 11:07:11 +0100

    refactor(task card): add design informations, add profile picture to card for assigned users and change event concept.

--- frontend/src/app/okrview/okr-child-unit/department/department-tab-task-card/department-tab-task-card.component.ts
+++ frontend/src/app/okrview/okr-child-unit/department/department-tab-task-card/department-tab-task-card.component.ts
@@ -1,6 +1,12 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TaskBoardViewEventService } from 'src/app/okrview/taskboard-services/task-board-view-event.service';
+import { User } from 'src/app/shared/model/api/user';
+import { ViewTask } from 'src/app/shared/model/ui/taskboard/view-task';
+import { ViewKeyResult } from 'src/app/shared/model/ui/view-key-result';
 
-import { ViewTask } from 'src/app/shared/model/ui/view-task';
+import { UserService } from 'src/app/shared/services/helper/user.service';
+import { KeyResultMapper } from 'src/app/shared/services/mapper/key-result.mapper';
 
 @Component({
   selector: 'app-department-tab-task-card',
@@ -9,22 +15,40 @@ import { ViewTask } from 'src/app/shared/model/ui/view-task';
 })
 export class DepartmentTabTaskCardComponent implements OnInit {
   @Input() task: ViewTask;
-  @Output() deleteEmitter: EventEmitter<ViewTask> = new EventEmitter();
+  users: Observable<User>[];
   isActive: boolean;
+  keyResult$: Observable<ViewKeyResult>;
+
+  constructor(private taskboardEventService: TaskBoardViewEventService,
+    private userService: UserService,
+    private keyResultService: KeyResultMapper,
+    private taskBoardEventService: TaskBoardViewEventService
+  ) { }
 
   ngOnInit(): void {
     this.isActive = false;
+    this.users = [];
+
+    if (this.task && this.task.assignedKeyResultId) {
+      this.keyResult$ = this.keyResultService.getKeyResultById$(this.task.assignedKeyResultId);
+    }
+
+    for (const userid of this.task.assignedUserIds) {
+      this.users.push(this.userService.getUserById$(userid));
+    }
   }
 
   setIsActive(isActive: boolean): void {
     this.isActive = isActive;
   }
 
+  updateOrViewTask(): void {
+    this.taskBoardEventService.taskUpdateButtonClick$.next(this.task);
+  }
+
   onDelete($event: Event, task: ViewTask): void {
     $event.stopPropagation();
-    $event.preventDefault();
-    console.log("onDelete");
 
-    this.deleteEmitter.emit(task);
+    this.taskboardEventService.taskDeleteButtonClick$.next(task);
   }
 }
