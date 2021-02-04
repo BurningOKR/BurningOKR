da96443eb69489700e5aa067b56d4236282b7dda
Patrick Buschmann <pbuschmann@brockhaus-ag.de>
2021-02-04 12:45:30 +0100

    Fixed forgotten injectible in the topicDraftApiService. Removed not needed api functions

--- frontend/src/app/shared/services/api/topic-draft-api.service.ts
+++ frontend/src/app/shared/services/api/topic-draft-api.service.ts
@@ -2,15 +2,17 @@ import { ApiHttpService } from '../../../core/services/api-http.service';
 import { CompanyId, OkrUnitId } from '../../model/id-types';
 import { Observable } from 'rxjs';
 import { OkrTopicDraftDto } from '../../model/api/OkrUnit/okr-topic-draft.dto';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
 
 export class TopicDraftApiService {
   constructor(private api: ApiHttpService) {
   }
 
   // TODO P.B. 28.01.2021 Implement needed methods. Change path when backend is done
-  getTopicDraftById$(id: OkrUnitId): Observable<OkrTopicDraftDto> {
-    return this.api.getData$('TODO');
-  }
 
   postTopicDraftForCompany$(companyId: CompanyId, topicDraftDto: OkrTopicDraftDto): Observable<OkrTopicDraftDto> {
     return this.api.postData$('TODO', topicDraftDto);
@@ -18,9 +20,5 @@ export class TopicDraftApiService {
 
   postTopicDraftForOkrBranch$(unitId: OkrUnitId, topicDraftDto: OkrTopicDraftDto): Observable<OkrTopicDraftDto> {
     return this.api.postData$('TODO', topicDraftDto);
-  }
-
-  deleteTopicDraft$(topicDraftId: OkrUnitId): Observable<boolean> {
-    return this.api.deleteData$('TODO');
   }
 }
