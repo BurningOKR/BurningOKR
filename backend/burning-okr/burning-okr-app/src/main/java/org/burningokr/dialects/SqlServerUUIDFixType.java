d63acdf7e01be8b413ea78ed6339cb744446bf82
Robin Janke <robin.janke@brockhaus-ag.de>
2021-02-08 09:30:37 +0100

    fix(SQLServer): Fixed UUID Type for SQL Server

---
+++ backend/burning-okr/burning-okr-app/src/main/java/org/burningokr/dialects/SqlServerUUIDFixType.java
@@ -0,0 +1,36 @@
+package org.burningokr.dialects;
+
+import org.hibernate.HibernateException;
+import org.hibernate.dialect.Dialect;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.type.AbstractSingleColumnStandardBasicType;
+import org.hibernate.type.LiteralType;
+import org.hibernate.type.StringType;
+import org.hibernate.type.descriptor.java.UUIDTypeDescriptor;
+import org.hibernate.type.descriptor.sql.VarcharTypeDescriptor;
+
+import java.util.UUID;
+
+public class SqlServerUUIDFixType extends AbstractSingleColumnStandardBasicType<UUID> implements LiteralType<UUID> {
+
+  public static final SqlServerUUIDFixType INSTANCE = new SqlServerUUIDFixType();
+
+  public SqlServerUUIDFixType() {
+    super( VarcharTypeDescriptor.INSTANCE, UUIDTypeDescriptor.INSTANCE );
+  }
+
+  @Override
+  protected boolean registerUnderJavaType() {
+    return true;
+  }
+
+  @Override
+  public String getName() {
+    return "fixed-uuid";
+  }
+
+  @Override
+  public String objectToSQLString(UUID uuid, Dialect dialect) throws Exception {
+    return StringType.INSTANCE.objectToSQLString(uuid.toString(), dialect);
+  }
+}
