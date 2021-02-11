e97948e92916b76486428af287c5be4e508ccf67
= <pbuschmann@brockhaus-ag.de>
2021-02-11 12:47:21 +0100

    Updated topicDraftMapper to fit new models

--- frontend/src/app/shared/services/mapper/topic-draft-mapper.ts
+++ frontend/src/app/shared/services/mapper/topic-draft-mapper.ts
@@ -16,20 +16,18 @@ export class TopicDraftMapper {
 
   static mapTopicDraftDto(topicDraft: OkrTopicDraftDto): OkrTopicDraft {
     return new OkrTopicDraft(
-      topicDraft.okrUnitId,
-      topicDraft.name,
-      topicDraft.label,
-      topicDraft.objectiveIds,
       topicDraft.parentUnitId,
-      topicDraft.isActive,
-      topicDraft.isParentUnitABranch,
+      topicDraft.descriptionId,
+      topicDraft.name,
       topicDraft.initiatorId,
       topicDraft.startTeam,
       topicDraft.stakeholders,
       topicDraft.acceptanceCriteria,
       topicDraft.contributesTo,
       topicDraft.delimitation,
-      topicDraft.beginning,
+      topicDraft.beginning ?
+        new Date(topicDraft.beginning[0], topicDraft.beginning[1] - 1, topicDraft.beginning[2]) :
+        null,
       topicDraft.dependencies,
       topicDraft.resources,
       topicDraft.handoverPlan
@@ -39,21 +37,19 @@ export class TopicDraftMapper {
   static mapTopicDraft(topicDraft: OkrTopicDraft): OkrTopicDraftDto {
     const topicDraftDto: OkrTopicDraftDto = new OkrTopicDraftDto();
 
-    topicDraftDto.__okrUnitType = UnitType.TOPIC_DRAFT;
-    topicDraftDto.okrUnitId = topicDraft.id;
-    topicDraftDto.name = topicDraft.name;
-    topicDraftDto.label = topicDraft.label;
-    topicDraftDto.objectiveIds = topicDraft.objectives;
     topicDraftDto.parentUnitId = topicDraft.parentUnitId;
-    topicDraftDto.isActive = topicDraft.isActive;
-    topicDraftDto.isParentUnitABranch = topicDraft.isParentUnitABranch;
+    topicDraftDto.name = topicDraft.name;
     topicDraftDto.initiatorId = topicDraft.initiatorId;
     topicDraftDto.startTeam = topicDraft.startTeam;
     topicDraftDto.stakeholders = topicDraft.stakeholders;
     topicDraftDto.acceptanceCriteria = topicDraft.acceptanceCriteria;
     topicDraftDto.contributesTo = topicDraft.contributesTo;
     topicDraftDto.delimitation = topicDraft.delimitation;
-    topicDraftDto.beginning = topicDraft.beginning;
+    topicDraftDto.beginning = topicDraft.beginning ? [
+      Number(topicDraft.beginning.getFullYear()),
+      Number(topicDraft.beginning.getMonth()) + 1,
+      Number(topicDraft.beginning.getDate())
+    ] : null;
     topicDraftDto.dependencies = topicDraft.dependencies;
     topicDraftDto.resources = topicDraft.resources;
     topicDraftDto.handoverPlan = topicDraft.handoverPlan;
