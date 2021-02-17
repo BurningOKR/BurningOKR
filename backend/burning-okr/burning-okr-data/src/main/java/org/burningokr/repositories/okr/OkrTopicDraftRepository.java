35b17b7c035cce116c56be94bbe634f2926e38ce
= <pbuschmann@brockhaus-ag.de>
2021-02-17 09:28:09 +0100

    Added topicDraft repo

---
+++ backend/burning-okr/burning-okr-data/src/main/java/org/burningokr/repositories/okr/OkrTopicDraftRepository.java
@@ -0,0 +1,10 @@
+package org.burningokr.repositories.okr;
+
+import org.burningokr.model.okr.OkrTopicDraft;
+import org.burningokr.repositories.ExtendedRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OkrTopicDraftRepository
+        extends ExtendedRepository<OkrTopicDraft, Long> {
+}
