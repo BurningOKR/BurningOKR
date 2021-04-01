34aedaa094e8455ca583f64c7298d9ef17f60a5c
dmaennel <dmaennel@brockhaus-ag.de>
2021-04-01 12:36:24 +0200

    refactor(db and DefaultTaskBoardState): rename DefaultTaskBoardState and the sql script for task management tables are a sub version

--- backend/burning-okr/burning-okr-model/src/main/java/org/burningokr/model/okr/DefaultTaskBoardState.java
+++ backend/burning-okr/burning-okr-model/src/main/java/org/burningokr/model/okr/DefaultTaskBoardState.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 
 @Entity
 @Data
-public class DefaultTaskState implements Trackable<Long> {
+public class DefaultTaskBoardState implements Trackable<Long> {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
