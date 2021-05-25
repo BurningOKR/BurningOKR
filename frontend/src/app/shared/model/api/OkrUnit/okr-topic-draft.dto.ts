f09463962199dcafbb6522ba0608e054e2025da2
EcCo18 <Clemens.kurzeya@icloud.com>
2021-05-25 14:31:32 +0200

    feat(submitted-topic-draft-card): finished mobile view.

--- frontend/src/app/shared/model/api/OkrUnit/okr-topic-draft.dto.ts
+++ frontend/src/app/shared/model/api/OkrUnit/okr-topic-draft.dto.ts
@@ -1,6 +1,5 @@
 import { OkrUnitId } from '../../id-types';
 import { OkrTopicDescriptionDto } from './okr-topic-description.dto';
-import { status } from '../../ui/OrganizationalUnit/okr-topic-draft/okr-topic-draft-status-enum';
 import { User } from '../user';
 
 export class OkrTopicDraftDto extends OkrTopicDescriptionDto {
--- frontend/src/app/submitted-topic-drafts/submitted-topic-draft-card/submitted-topic-draft-card.component.ts
+++ frontend/src/app/submitted-topic-drafts/submitted-topic-draft-card/submitted-topic-draft-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { OkrTopicDraft } from '../../shared/model/ui/OrganizationalUnit/okr-topic-draft/okr-topic-draft';
 import { status } from '../../shared/model/ui/OrganizationalUnit/okr-topic-draft/okr-topic-draft-status-enum';
 
@@ -7,20 +7,9 @@ import { status } from '../../shared/model/ui/OrganizationalUnit/okr-topic-draft
   templateUrl: './submitted-topic-draft-card.component.html',
   styleUrls: ['./submitted-topic-draft-card.component.css']
 })
-export class SubmittedTopicDraftCardComponent implements OnInit {
+export class SubmittedTopicDraftCardComponent {
   @Input()
   topicDraft: OkrTopicDraft;
 
   enumStatus = status;
-  currentStatusString: string;
-
-  ngOnInit(): void {
-    switch (this.topicDraft.currentStatus) {
-      case status.rejected: this.currentStatusString = 'status-rejected'; break;
-      case status.approved: this.currentStatusString = 'status-approved'; break;
-      case status.submitted: this.currentStatusString = 'status-submitted'; break;
-      case status.draft: this.currentStatusString = 'status-draft'; break;
-      default: this.currentStatusString = 'status-draft';
-    }
-  }
 }
