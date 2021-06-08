41094365ab3c934242afa602b6b3d0fd0855f3ba
Rouven Tjalf Rosploch <rrosploch@brockhaus-ag.de>
2021-06-08 15:40:18 +0200

    fix: Websocket API connection (prod)

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import { Environment } from '../app/shared/model/environment/environment';
 
 export const environment: Environment = {
   production: true,
-  brokerURLSuffix: 'wsregistry',
+  brokerURLSuffix: 'api/wsregistry',
 };
