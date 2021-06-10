9839090fa1a553797f0d896b25f26c60984be2fc
jzumbrink <jzumbrink@brockhaus-ag.de>
2021-06-10 11:59:08 +0200

    Added multiline Textbox and updated the style of submitted-topic-draft-details.component

--- frontend/src/app/submitted-topic-drafts/submitted-topic-draft-details/submitted-topic-draft-details.component.ts
+++ frontend/src/app/submitted-topic-drafts/submitted-topic-draft-details/submitted-topic-draft-details.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-submitted-topic-draft-details',
   templateUrl: './submitted-topic-draft-details.component.html',
-  styleUrls: ['./submitted-topic-draft-details.component.css', '../submitted-topic-draft-card/submitted-topic-draft-card.component.css']
+  styleUrls: ['./submitted-topic-draft-details.component.css']
 })
 export class SubmittedTopicDraftDetailsComponent implements OnInit {
 
