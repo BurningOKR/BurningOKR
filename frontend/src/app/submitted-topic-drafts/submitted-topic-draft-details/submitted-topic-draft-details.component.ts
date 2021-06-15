70c4add5b05462d0bf55047b2ab8108671beb222
NicoleLeinich <leinichn@yahoo.de>
2021-06-15 10:37:19 +0200

    Translations.

--- frontend/src/app/submitted-topic-drafts/submitted-topic-draft-details/submitted-topic-draft-details.component.ts
+++ frontend/src/app/submitted-topic-drafts/submitted-topic-draft-details/submitted-topic-draft-details.component.ts
@@ -10,27 +10,26 @@ import { User } from '../../shared/model/api/user';
 })
 export class SubmittedTopicDraftDetailsComponent {
 
-  // @Input()
-  topicDraft: OkrTopicDraft;
+  @Input() topicDraft: OkrTopicDraft;
   enumStatus = status;
 
   constructor() {
     this.topicDraft = new OkrTopicDraft(
       1,
       status.approved,
-      new User("746eabe8-e36c-4b46-b72d-f24a249b3860", "Markus","Mueller","mmueller@web.de","IT-Consultant","IT","photo.jpg",true),
+      new User('575eb66a-75bf-4c5a-88ab-b56ebe49fd42', 'Spongebob', 'Schwammkopf', 'bikiniBottom@mail.de', 'Burgerbratter', '', '', true),
       2,
       'Automatische Generierung von Essen',
       'sddasdasadsdafs',
-      ["746eabe8-e36c-4b46-b72d-f24a249b3860", "7c8db234-a6f2-4bff-9b95-dd26ef82246b", "b16b928b-6259-4f12-87f4-00e2f8fb6385", 'af444d94-0c40-430f-bce7-95d79bc6a607'],
-      ["d223f694-8a45-4ece-8a17-d265149ae260", "746eabe8-e36c-4b46-b72d-f24a249b3860", 'af444d94-0c40-430f-bce7-95d79bc6a607'],
+      ['575eb66a-75bf-4c5a-88ab-b56ebe49fd42', 'ccc5c00f-04e6-4a23-b157-781163841bb5'],
+      ['575eb66a-75bf-4c5a-88ab-b56ebe49fd42', 'ccc5c00f-04e6-4a23-b157-781163841bb5'],
       'Irgendwelche Kriterien',
       'alles was läuft und fährt',
       'Irgendwas wird doch immer reguliert',
-      new Date(2018, 11, 24, 10, 33, 30, 0),
+      new Date(2018, 11, 24, 10, 33, 30, 10),
       'Ganz ganz viele Dependencies',
-      'Irgendwas mit Resourcen',
+      'Irgendwas mit Ressourcen',
       'Ein ganz toller Plan zum Übergeben von Sachen und Essen'
-    )
+    );
   }
 }
