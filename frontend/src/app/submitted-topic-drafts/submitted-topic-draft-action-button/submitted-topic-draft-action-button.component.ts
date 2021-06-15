937a274fb010c5f868ae4673348b621d59918063
pbuschmann <pbuschmann@brockhaus-ag.de>
2021-06-15 11:54:05 +0200

    feat(SubmittedTopicDraftDelete): Added dialog for deletion of topicDrafts. Changed t.d. card to inject topicDraft

--- frontend/src/app/submitted-topic-drafts/submitted-topic-draft-action-button/submitted-topic-draft-action-button.component.ts
+++ frontend/src/app/submitted-topic-drafts/submitted-topic-draft-action-button/submitted-topic-draft-action-button.component.ts
@@ -1,24 +1,33 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
 import { OkrTopicDraft } from '../../shared/model/ui/OrganizationalUnit/okr-topic-draft/okr-topic-draft';
+import {
+  ConfirmationDialogComponent,
+  ConfirmationDialogData
+} from '../../shared/components/confirmation-dialog/confirmation-dialog.component';
+import { take } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+import { I18n } from '@ngx-translate/i18n-polyfill';
+import { MatDialog, MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-submitted-topic-draft-action-button',
   templateUrl: './submitted-topic-draft-action-button.component.html',
   styleUrls: ['./submitted-topic-draft-action-button.component.css']
 })
-export class SubmittedTopicDraftActionButtonComponent {
+export class SubmittedTopicDraftActionButtonComponent implements OnDestroy {
 
   @Input() topicDraft: OkrTopicDraft;
 
-  @Output() clickedEditAction: EventEmitter<void> = new EventEmitter<void>();
-  @Output() clickedDeleteAction: EventEmitter<void> = new EventEmitter<void>();
-  @Output() clickedCommentsAction: EventEmitter<void> = new EventEmitter<void>();
-  @Output() clickedSubmitAction: EventEmitter<void> = new EventEmitter<void>();
-  @Output() clickedWithdrawAction: EventEmitter<void> = new EventEmitter<void>();
-  @Output() clickedApprove: EventEmitter<void> = new EventEmitter<void>();
-  @Output() clickedDiscardApprovalAction: EventEmitter<void> = new EventEmitter<void>();
-  @Output() clickedRefuse: EventEmitter<void>  = new EventEmitter<void>();
-  @Output() clickedDiscardRefusalAction: EventEmitter<void> = new EventEmitter<void>();
+  subscriptions: Subscription[] = [];
+
+  constructor(private matDialog: MatDialog,
+              private i18n: I18n) {
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+    this.subscriptions = [];
+  }
 
   printNotImplemented(): string {
     // TODO: methode Entfernen
@@ -26,5 +35,51 @@ export class SubmittedTopicDraftActionButtonComponent {
     console.log('Not Implemented');
 
     return 'Not Implemented';
+  }
+
+  clickedDeleteTopicDraft(): void {
+    const title: string =
+        this.i18n({
+          id: 'deleteTopicDraftTitle',
+          description: 'Title of the delete topicdraft dialog',
+          value: 'Themenentwurf löschen'
+        });
+
+    const message: string =
+        this.i18n({
+          id: 'deleteTopicDraftMessage',
+          description: 'Do you want to delete topic draft x',
+          value: 'Themenentwurf "{{name}}" löschen?',
+        }, {name: this.topicDraft.name});
+
+    const confirmButtonText: string = this.i18n({
+      id: 'deleteButtonText',
+      description: 'deleteButtonText',
+      value: 'Löschen'
+    });
+
+    const dialogData: ConfirmationDialogData = {
+      title,
+      message,
+      confirmButtonText
+    };
+
+    const dialogReference: MatDialogRef<ConfirmationDialogComponent, object>
+        = this.matDialog.open(ConfirmationDialogComponent, {width: '600px', data: dialogData});
+
+    this.subscriptions.push(
+        dialogReference
+            .afterClosed()
+            .pipe(take(1))
+            .subscribe(isConfirmed => {
+              if (isConfirmed) {
+                this.deleteTopicDraft();
+              }
+            })
+    );
+  }
+
+  deleteTopicDraft(): void {
+    // TODO
   }
 }
