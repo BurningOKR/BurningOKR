28e729467231250580954b14746d1f9e3645c216
Rouven Tjalf Rosploch <rrosploch@brockhaus-ag.de>
2021-06-18 13:29:01 +0200

    fix(SubmittedTopicDraftCardComponent): Frontend test

--- frontend/src/app/submitted-topic-drafts/submitted-topic-draft-card/submitted-topic-draft-card.component.spec.ts
+++ frontend/src/app/submitted-topic-drafts/submitted-topic-draft-card/submitted-topic-draft-card.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SubmittedTopicDraftCardComponent } from './submitted-topic-draft-card.component';
+import { Component, Input } from '@angular/core';
+import { OkrTopicDraft } from '../../shared/model/ui/OrganizationalUnit/okr-topic-draft/okr-topic-draft';
+import { MaterialTestingModule } from '../../testing/material-testing.module';
+import { status } from '../../shared/model/ui/OrganizationalUnit/okr-topic-draft/okr-topic-draft-status-enum';
+import { User } from '../../shared/model/api/user';
 
 describe('SubmittedTopicDraftCardComponent', () => {
   let component: SubmittedTopicDraftCardComponent;
@@ -8,7 +13,8 @@ describe('SubmittedTopicDraftCardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SubmittedTopicDraftCardComponent ]
+      declarations: [ SubmittedTopicDraftCardComponent, SubmittedTopicDraftActionButtonMock ],
+      imports: [ MaterialTestingModule ]
     })
     .compileComponents();
   }));
@@ -16,8 +22,19 @@ describe('SubmittedTopicDraftCardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SubmittedTopicDraftCardComponent);
     component = fixture.componentInstance;
+    component.topicDraft = new OkrTopicDraft
+    (1, status.submitted, new User('1', 'T', 'T'), 0, '', '', null, null, '', '', '', new Date(), '', '', '');
     fixture.detectChanges();
   });
+
+  @Component({
+    // tslint:disable-next-line:component-selector
+    selector: 'app-submitted-topic-draft-action-button',
+    template: ''
+  })
+  class SubmittedTopicDraftActionButtonMock {
+    @Input() topicDraft: OkrTopicDraft;
+  }
 
   it('should create', () => {
     expect(component)
