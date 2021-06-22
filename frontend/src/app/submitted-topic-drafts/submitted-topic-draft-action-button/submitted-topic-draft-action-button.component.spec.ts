5355cf43e1f53394272a195201fa825d1d468cf7
pbuschmann <pbuschmann@brockhaus-ag.de>
2021-06-22 13:40:41 +0200

    test(SubmittedTopicDraftDelete): Fixed Frontend Test

--- frontend/src/app/submitted-topic-drafts/submitted-topic-draft-action-button/submitted-topic-draft-action-button.component.spec.ts
+++ frontend/src/app/submitted-topic-drafts/submitted-topic-draft-action-button/submitted-topic-draft-action-button.component.spec.ts
@@ -2,15 +2,32 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SubmittedTopicDraftActionButtonComponent } from './submitted-topic-draft-action-button.component';
 import { MaterialTestingModule } from '../../testing/material-testing.module';
+import { MatDialog } from '@angular/material';
+import { HttpClient } from '@angular/common/http';
+import { AuthenticationService } from '../../core/auth/services/authentication.service';
+import { I18n } from '@ngx-translate/i18n-polyfill';
+import { NGXLogger } from 'ngx-logger';
+import { Router } from '@angular/router';
 
 describe('SubmittedTopicDraftActionButtonComponent', () => {
   let component: SubmittedTopicDraftActionButtonComponent;
   let fixture: ComponentFixture<SubmittedTopicDraftActionButtonComponent>;
 
+  const i18nMock: any = jest.fn();
+  const routerMock: any = jest.fn();
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ SubmittedTopicDraftActionButtonComponent ],
-      imports: [ MaterialTestingModule ]
+      imports: [ MaterialTestingModule ],
+      providers: [
+        { provide: MatDialog, useValue: {} },
+        { provide: HttpClient, useValue: {} },
+        { provide: AuthenticationService, useValue: {} },
+        { provide: I18n, useValue: i18nMock },
+        { provide: NGXLogger, useValue: {} },
+        { provide: Router, useValue: routerMock }
+      ]
     })
     .compileComponents();
   }));
