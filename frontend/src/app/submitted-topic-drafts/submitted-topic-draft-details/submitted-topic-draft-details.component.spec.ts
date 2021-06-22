539cb6f25e5e1a4f173f430ec473659816bf38c2
jzumbrink <jzumbrink@brockhaus-ag.de>
2021-06-22 13:46:54 +0200

    fixed one test

--- frontend/src/app/submitted-topic-drafts/submitted-topic-draft-details/submitted-topic-draft-details.component.spec.ts
+++ frontend/src/app/submitted-topic-drafts/submitted-topic-draft-details/submitted-topic-draft-details.component.spec.ts
@@ -1,6 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SubmittedTopicDraftDetailsComponent } from './submitted-topic-draft-details.component';
+import { StatusDotComponent } from '../../shared/components/status-dot/status-dot.component';
+import { UserMinibuttonComponent } from '../../shared/components/user-minibutton/user-minibutton.component';
+import { UserAvatarComponent } from '../../shared/components/user-avatar/user-avatar.component';
+import { AvatarComponent } from 'ngx-avatar';
+import { MaterialTestingModule } from '../../testing/material-testing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { OAuthService } from 'angular-oauth2-oidc';
+import {HttpClient} from "@angular/common/http";
+import {AuthenticationService} from "../../core/auth/services/authentication.service";
+import {I18n} from "@ngx-translate/i18n-polyfill";
+import {NGXLogger} from "ngx-logger";
+import {Router} from "@angular/router";
 
 describe('SubmittedTopicDraftDetailsComponent', () => {
   let component: SubmittedTopicDraftDetailsComponent;
@@ -8,7 +21,24 @@ describe('SubmittedTopicDraftDetailsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SubmittedTopicDraftDetailsComponent ]
+      declarations: [ SubmittedTopicDraftDetailsComponent,
+                      StatusDotComponent,
+                      UserMinibuttonComponent,
+                      AvatarComponent,
+                      //MatDialogRef,
+                      UserAvatarComponent],
+      imports: [ MaterialTestingModule,
+                 FormsModule,
+                 ReactiveFormsModule],
+      providers: [
+        {provide: MatDialogRef, useValue: {}},
+        {provide: OAuthService, useValue: {}},
+        {provide: HttpClient, useValue: {}},
+        {provide: AuthenticationService, useValue: {}},
+        {provide: I18n, useValue: {}},
+        {provide: NGXLogger, useValue: {}},
+        {provide: Router, useValue: {}}
+      ]
     })
     .compileComponents();
   }));
