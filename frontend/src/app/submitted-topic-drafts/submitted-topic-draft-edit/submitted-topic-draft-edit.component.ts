4bb03c80bebb957a653c4f64ffb4e78f1f1824a6
jzumbrink <j.zumbrink@gmx.de>
2021-06-25 10:50:30 +0200

    added minBegin as an Input field to choose a different Begin when editing a topic draft

--- frontend/src/app/submitted-topic-drafts/submitted-topic-draft-edit/submitted-topic-draft-edit.component.ts
+++ frontend/src/app/submitted-topic-drafts/submitted-topic-draft-edit/submitted-topic-draft-edit.component.ts
@@ -14,6 +14,7 @@ export class SubmittedTopicDraftEditComponent implements OnInit {
   topicDraft: OkrTopicDraft;
   topicDraftForm: FormGroup;
   title: string;
+  minBegin: Date;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) private formData: (SubmittedTopicDraftDetailsFormData | any)
@@ -21,6 +22,20 @@ export class SubmittedTopicDraftEditComponent implements OnInit {
 
   ngOnInit(): void {
     this.topicDraft = this.formData.topicDraft;
+    this.minBegin = this.topicDraft.beginning;
+    /*this.topicDraftForm = new FormGroup({
+      name: new FormControl(this.topicDraft.name, [Validators.maxLength(255), Validators.required]),
+      acceptanceCriteria: new FormControl(this.topicDraft.acceptanceCriteria, Validators.maxLength(1024)),
+      contributesTo: new FormControl(this.topicDraft.contributesTo, Validators.maxLength(1024)),
+      delimitation: new FormControl(this.topicDraft.delimitation, Validators.maxLength(1024)),
+      beginning: new FormControl(this.topicDraft.beginning, [Validators.required]),
+      dependencies: new FormControl(this.topicDraft.dependencies, Validators.maxLength(1024)),
+      resources: new FormControl(this.topicDraft.resources, Validators.maxLength(1024)),
+      handoverPlan: new FormControl(this.topicDraft.handoverPlan, Validators.maxLength(1024)),
+      initiatorId: new FormControl(this.topicDraft.initiatorId, [Validators.required]),
+      startTeam: new FormControl(this.topicDraft.startTeam),
+      stakeholders: new FormControl(this.topicDraft.stakeholders)
+    });*/
     this.topicDraftForm = new FormGroup({
       name: new FormControl(this.topicDraft.name, [Validators.maxLength(255), Validators.required]),
       acceptanceCriteria: new FormControl(this.topicDraft.acceptanceCriteria, Validators.maxLength(1024)),
@@ -34,6 +49,11 @@ export class SubmittedTopicDraftEditComponent implements OnInit {
       startTeam: new FormControl(this.topicDraft.startTeam),
       stakeholders: new FormControl(this.topicDraft.stakeholders)
     });
+
+    if (this.formData.topicDraft) {
+      this.topicDraftForm.patchValue(this.formData.topicDraft);
+    }
+
     this.title = 'Themenentwurf bearbeiten';
   }
 
