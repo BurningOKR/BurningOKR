f9ca91b7deeb0a5641c6dd7b247b9ab7e94f0d8d
pbuschmann <pbuschmann@brockhaus-ag.de>
2021-07-01 12:48:31 +0200

    test(SubmittedTopicDraftActionButtonComponent): Fixed Test

--- frontend/src/app/submitted-topic-drafts/submitted-topic-draft-action-button/submitted-topic-draft-action-button.component.spec.ts
+++ frontend/src/app/submitted-topic-drafts/submitted-topic-draft-action-button/submitted-topic-draft-action-button.component.spec.ts
@@ -8,13 +8,26 @@ import { AuthenticationService } from '../../core/auth/services/authentication.s
 import { I18n } from '@ngx-translate/i18n-polyfill';
 import { NGXLogger } from 'ngx-logger';
 import { Router } from '@angular/router';
+import { OAuthService } from 'angular-oauth2-oidc';
+import { User } from '../../shared/model/api/user';
+import { OkrTopicDraft } from '../../shared/model/ui/OrganizationalUnit/okr-topic-draft/okr-topic-draft';
+import { status } from '../../shared/model/ui/OrganizationalUnit/okr-topic-draft/okr-topic-draft-status-enum';
+import { CurrentUserService } from '../../core/services/current-user.service';
+import { of } from 'rxjs';
 
 describe('SubmittedTopicDraftActionButtonComponent', () => {
   let component: SubmittedTopicDraftActionButtonComponent;
   let fixture: ComponentFixture<SubmittedTopicDraftActionButtonComponent>;
 
+  let currentUser: User;
+  let topicDraft: OkrTopicDraft;
+
   const i18nMock: any = jest.fn();
   const routerMock: any = jest.fn();
+  const currentUserServiceMock: any = {
+    getCurrentUser$: jest.fn(),
+    isCurrentUserAdmin$: jest.fn()
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -26,15 +39,26 @@ describe('SubmittedTopicDraftActionButtonComponent', () => {
         { provide: AuthenticationService, useValue: {} },
         { provide: I18n, useValue: i18nMock },
         { provide: NGXLogger, useValue: {} },
-        { provide: Router, useValue: routerMock }
+        { provide: Router, useValue: routerMock },
+        { provide: OAuthService, useValue: {} },
+        { provide: CurrentUserService, useValue: currentUserServiceMock }
       ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    currentUser = new User('1', '', '', '', '', '', '', true);
+    topicDraft = new OkrTopicDraft(-1, status.submitted, currentUser, 1, '', '1', [], [], '', '', '', new Date(), '', '', '');
+
+    currentUserServiceMock.getCurrentUser$.mockReset();
+    currentUserServiceMock.getCurrentUser$.mockReturnValue(of(currentUser));
+    currentUserServiceMock.isCurrentUserAdmin$.mockReset();
+    currentUserServiceMock.isCurrentUserAdmin$.mockReturnValue(of(false));
+
     fixture = TestBed.createComponent(SubmittedTopicDraftActionButtonComponent);
     component = fixture.componentInstance;
+    component.topicDraft = topicDraft;
     fixture.detectChanges();
   });
 
