3e8b6d1d1c0c726cd36bbbf269e750e980a06f38
jzumbrink <j.zumbrink@gmx.de>
2021-07-02 11:06:30 +0200

    fixed error from merging

--- backend/burning-okr/burning-okr-logic/src/main/java/org/burningokr/service/security/UserRoleFromContextService.java
+++ backend/burning-okr/burning-okr-logic/src/main/java/org/burningokr/service/security/UserRoleFromContextService.java
@@ -118,23 +118,6 @@ public class UserRoleFromContextService {
   }
 
   /**
-   *
-   * @param topicDraftId a long value
-   * @return an {@link UserContextRole} object
-   */
-  public UserContextRole getUserRoleTopicDraft(Long topicDraftId){
-    OkrTopicDraft topicDraft = topicDraftRepository.findByIdOrThrow(topicDraftId);
-    UUID currentUserId = userService.getCurrentUser().getId();
-
-    if(currentUserId.equals(topicDraft.getInitiatorId())){
-      return UserContextRole.ENTITYOWNER;
-    }
-    else{
-      return UserContextRole.USER;
-    }
-  }
-
-  /**
    * Gets the UserContextRole of a okrUnit.
    *
    * @param okrUnit an {@link OkrChildUnit} object
