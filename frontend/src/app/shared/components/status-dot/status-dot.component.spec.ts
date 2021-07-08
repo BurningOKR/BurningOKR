4261464615c13b1512b43c62c2e2366cab8a45f6
pbuschmann <pbuschmann@brockhaus-ag.de>
2021-07-08 10:31:39 +0200

    fix(TopicDraftStatusDot): Fixed tooltip to fit the statuscolor

--- frontend/src/app/shared/components/status-dot/status-dot.component.spec.ts
+++ frontend/src/app/shared/components/status-dot/status-dot.component.spec.ts
@@ -1,14 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StatusDotComponent } from './status-dot.component';
+import { I18n } from '@ngx-translate/i18n-polyfill';
 
 describe('StatusDotComponent', () => {
   let component: StatusDotComponent;
   let fixture: ComponentFixture<StatusDotComponent>;
 
+  const i18nMock: any = jest.fn();
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [StatusDotComponent]
+      declarations: [StatusDotComponent],
+      providers: [
+        {provide: I18n, useValue: i18nMock}
+      ]
     })
       .compileComponents();
   }));
