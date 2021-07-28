f8b16ea55cacb1126892516c0b22eb50add05b10
EcCo18 <Clemens.kurzeya@icloud.com>
2021-07-28 14:58:06 +0200

    feat(objective-comment): reworked migration script.

--- backend/burning-okr/burning-okr-app/src/main/resources/db/migration/postgresql/V64__ObjectiveNoteTable.sql
+++ backend/burning-okr/burning-okr-app/src/main/resources/db/migration/postgresql/V64__ObjectiveNoteTable.sql
@@ -1,9 +1,9 @@
 CREATE TABLE public.note_objective
 (
     id               bigint NOT NULL,
-    parent_objective bigint NOT NULL,
+    parent_objective_id bigint NOT NULL,
     CONSTRAINT note_objective_id_pk PRIMARY KEY (id),
-    CONSTRAINT objective_fk FOREIGN KEY (parent_objective)
+    CONSTRAINT objective_fk FOREIGN KEY (parent_objective_id)
         REFERENCES public.objective (id) MATCH SIMPLE
         ON UPDATE NO ACTION ON DELETE NO ACTION
 ) WITH (
