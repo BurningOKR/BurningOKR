b37076366b77298e8d28c06dd8e2c8c0f2ecfa62
EcCo18 <Clemens.kurzeya@icloud.com>
2021-07-30 14:31:21 +0200

    feat(notes): added tests.

--- backend/burning-okr/burning-okr-app/src/test/java/org/burningokr/mapper/okr/NoteAbstractMapperTest.java
+++ backend/burning-okr/burning-okr-app/src/test/java/org/burningokr/mapper/okr/NoteAbstractMapperTest.java
@@ -2,6 +2,13 @@ package org.burningokr.mapper.okr;
 
 import org.burningokr.dto.okr.NoteDto;
 import org.burningokr.model.okr.Note;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.UUID;
 
 import static org.junit.Assert.*;
 
@@ -9,7 +16,48 @@ public class NoteAbstractMapperTest {
 
     private class NoteMapper extends NoteAbstractMapper{}
 
-    private Note note = new Note();
-    private NoteDto noteDto = new NoteDto();
-    // private NoteMa
+    private Note note;
+    private NoteDto noteDto;
+    private NoteMapper noteMapper;
+
+    @Before
+    public void reset() {
+        noteDto = new NoteDto();
+        note = new Note();
+        noteMapper = new NoteMapper();
+    }
+
+    // Dto to Entity
+
+    @Test
+    public void mapNoteDtoToEntity_expects_NoteBodyIsMapped() {
+        String expected = "test";
+        noteDto.setNoteBody(expected);
+        Assert.assertEquals(expected, noteMapper.mapNoteDtoToEntity(noteDto).getText());
+    }
+
+    @Test
+    public void mapNoteDtoToEntity_expects_NoteIdIsMapped() {
+        Long expected = 1L;
+        noteDto.setNoteId(expected);
+        Assert.assertEquals(expected, noteMapper.mapNoteDtoToEntity(noteDto).getId());
+    }
+
+    @Test
+    public void mapNoteDtoToEntity_expects_UserIdIsMapped() {
+        UUID expected = new UUID(1L, 1L);
+        noteDto.setUserId(expected);
+        Assert.assertEquals(expected, noteMapper.mapNoteDtoToEntity(noteDto).getUserId());
+    }
+
+    @Test
+    public void mapNoteDtoToEntity_expects_DateIsMapped() {
+        LocalDateTime expected = LocalDateTime.now();
+        noteDto.setDate(expected);
+        Assert.assertEquals(expected, noteMapper.mapNoteDtoToEntity(noteDto).getDate());
+    }
+
+    // Entity to Entity
+
+
 }
