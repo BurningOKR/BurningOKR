244fdcdaa047f62019dc3a92e1b42fe9d223d7e7
Moritz Tietze <moritz.tietze@icloud.com>
2021-08-04 10:57:48 +0200

    Replaced absolute logging path in slf4j config xml with dynamically generated path (depending on os)

--- backend/burning-okr/burning-okr-app/src/main/java/org/burningokr/BurningOkrApp.java
+++ backend/burning-okr/burning-okr-app/src/main/java/org/burningokr/BurningOkrApp.java
@@ -8,6 +8,8 @@ import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+import org.slf4j.MDC;
+
 @SpringBootApplication
 @EnableResourceServer
 public class BurningOkrApp extends SpringBootServletInitializer {
@@ -23,6 +25,9 @@ public class BurningOkrApp extends SpringBootServletInitializer {
    * @param args an array of Strings
    */
   public static void main(String[] args) {
+    // change root directory for slf4j logging via MDC
+    String osName = System.getProperty("os.name").toLowerCase();
+    MDC.put("tempPath", (osName.contains("win") ? "C:\\temp\\" : "/tmp/"));
     SpringApplication app = new SpringApplication(BurningOkrApp.class);
     app.setWebApplicationType(WebApplicationType.SERVLET);
     app.addListeners(new ApplicationPidFileWriter());
