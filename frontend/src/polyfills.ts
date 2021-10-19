ac178cd4b7e9f3a4422917ad2fb9a2987013ea38
EcCo18 <clemens.kurzeya@icloud.com>
2021-10-19 13:53:16 +0200

    @angular/cli migration - update-zonejs
    Update 'zone.js' to version 0.11.x. Read more about this here: https://github.com/angular/angular/blob/master/packages/zone.js/CHANGELOG.md#breaking-changes-since-zonejs-v0111

--- frontend/src/polyfills.ts
+++ frontend/src/polyfills.ts
@@ -27,7 +27,7 @@ import 'core-js/es/reflect';
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone'; // Included with Angular CLI.
+import 'zone.js'; // Included with Angular CLI.
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
