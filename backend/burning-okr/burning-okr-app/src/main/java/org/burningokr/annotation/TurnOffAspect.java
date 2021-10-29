0add8d5b7189874603226e60542668bc0f4572b5
Jonas <jonas.behrens@brockhaus-ag.de>
2021-10-29 15:06:08 +0200

    feature(TurnOffAspect.java): Backend Playground behaviour can be dynamically disabled in application.yaml --> spring.environment.demo is needed (true == Playground | false == Prod)

--- backend/burning-okr/burning-okr-app/src/main/java/org/burningokr/annotation/TurnOffAspect.java
+++ backend/burning-okr/burning-okr-app/src/main/java/org/burningokr/annotation/TurnOffAspect.java
@@ -5,11 +5,15 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.burningokr.service.exceptions.ForbiddenException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Component;
 
+@ConditionalOnExpression("${spring.environment.demo}")
 @Aspect
 @Component
 public class TurnOffAspect {
+
   @Pointcut("@annotation(org.burningokr.annotation.TurnOff)")
   private void turnOffAnnotation() {}
 
@@ -17,4 +21,5 @@ public class TurnOffAspect {
   public Object turnOff(ProceedingJoinPoint pjp) throws ForbiddenException {
     throw new ForbiddenException("Method disabled");
   }
+
 }
