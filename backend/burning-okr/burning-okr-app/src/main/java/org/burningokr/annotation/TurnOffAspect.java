141d51b260b8b0e852d77f8e958b7c39f356fd1f
jklein <justin.klein@brockhaus-ag.de>
2021-11-08 14:45:02 +0100

    refactor(TurnOffAspect): Fixed Styling issues

--- backend/burning-okr/burning-okr-app/src/main/java/org/burningokr/annotation/TurnOffAspect.java
+++ backend/burning-okr/burning-okr-app/src/main/java/org/burningokr/annotation/TurnOffAspect.java
@@ -5,7 +5,6 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.burningokr.service.exceptions.ForbiddenException;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Component;
 
@@ -21,5 +20,4 @@ public class TurnOffAspect {
   public Object turnOff(ProceedingJoinPoint pjp) throws ForbiddenException {
     throw new ForbiddenException("Method disabled");
   }
-
 }
