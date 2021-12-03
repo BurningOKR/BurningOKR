747c51eab8bc324c42b8152d5cf89cc49148c760
Jonas <jonas.behrens@brockhaus-ag.de>
2021-12-03 16:54:16 +0100

    more translations

--- frontend/src/app/okr-units/okr-unit-form/okr-unit-form.component.ts
+++ frontend/src/app/okr-units/okr-unit-form/okr-unit-form.component.ts
@@ -7,6 +7,7 @@ import { CompanyUnit } from '../../shared/model/ui/OrganizationalUnit/company-un
 import { DialogComponent } from '../../shared/components/dialog-component/dialog.component';
 import { I18n } from '@ngx-translate/i18n-polyfill';
 import { ValidationErrorService } from '../../shared/services/helper/validation-error.service';
+import {TranslateService} from "@ngx-translate/core";
 
 interface CompanyFormData {
   company?: CompanyUnit;
@@ -21,12 +22,16 @@ interface CompanyFormData {
 export class OkrUnitFormComponent {
   companyForm: FormGroup;
   title: string;
+  saveTranslation: string;
+  createTranslation: string;
+  structureTranslation: string;
 
   constructor(private dialogRef: MatDialogRef<DialogComponent<CompanyFormData>>,
               private companyMapper: CompanyMapper,
               private i18n: I18n,
               @Inject(MAT_DIALOG_DATA) private formData: CompanyFormData,
-              private x: ValidationErrorService) {
+              private x: ValidationErrorService,
+              private translate: TranslateService) {
     this.companyForm = new FormGroup({
       name: new FormControl('', [Validators.required, Validators.maxLength(255)]),
       label: new FormControl(this.getDefaultLabel(), [Validators.required, Validators.minLength(1), Validators.maxLength(255)])
@@ -36,7 +41,19 @@ export class OkrUnitFormComponent {
       this.companyForm.patchValue(this.formData.company);
     }
 
-    const saveText: string = this.i18n({
+    translate.get('okr-unit-form.save', {value: this.getDefaultLabel()}).subscribe((text: string) => {
+      this.saveTranslation = text;
+    });
+    translate.get('okr-unit-form.create', {value: this.getDefaultLabel()}).subscribe((text: string) => {
+      this.createTranslation = text;
+    });
+    translate.get('okr-unit-form.structure').subscribe((text: string) => {
+      this.structureTranslation = text;
+    });
+
+
+
+    /*const saveText: string = this.i18n({
         id: 'component_companyForm_saveText',
         value: ' {{label}} speichern.'
       }, {label: this.getDefaultLabel()}
@@ -44,9 +61,9 @@ export class OkrUnitFormComponent {
     const createText: string = this.i18n({
         id: 'component_companyForm_createText',
         value: '{{label}} erstellen.'
-      }, {label: this.getDefaultLabel()});
+      }, {label: this.getDefaultLabel()});*/
 
-    this.title = this.formData.company ? saveText : createText;
+    this.title = this.formData.company ? this.saveTranslation : this.createTranslation;
   }
 
   saveCompany(): void {
@@ -67,7 +84,7 @@ export class OkrUnitFormComponent {
     if (this.formData.company) {
       return this.formData.company.label;
     } else {
-      return this.i18n({id: 'schema', value: 'Struktur'});
+      return this.structureTranslation;
     }
   }
 }
