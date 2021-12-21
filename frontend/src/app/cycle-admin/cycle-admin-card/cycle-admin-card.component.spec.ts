612e3976ceaa292a01528b2fc3e86d5b96cd2500
Jan Pietsch <jpietsch@brockhaus-ag.de>
2021-12-21 14:00:14 +0100

    feat(cycle-admin-card): translated cycle-admin-card.component.ts using ngx translate and fixed tests and cycle-creation-form.component.html

--- frontend/src/app/cycle-admin/cycle-admin-card/cycle-admin-card.component.spec.ts
+++ frontend/src/app/cycle-admin/cycle-admin-card/cycle-admin-card.component.spec.ts
@@ -1,10 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { CycleAdminCardComponent } from './cycle-admin-card.component';
-import { I18n } from '@ngx-translate/i18n-polyfill';
-import { i18nMock } from '../../shared/mocks/i18n-mock';
 import { CycleUnit } from '../../shared/model/ui/cycle-unit';
 import { DeleteDialogData } from '../../shared/model/ui/delete-dialog-data';
 import { MatDialog } from '@angular/material/dialog';
@@ -12,10 +9,12 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { CycleMapper } from '../../shared/services/mapper/cycle.mapper';
 import { MaterialTestingModule } from '../../testing/material-testing.module';
+import { TranslateService } from '@ngx-translate/core';
 
 describe('CycleAdminCardComponent', () => {
   let fixture: any;
   let component: any;
+  let translate: TranslateService
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -29,7 +28,6 @@ describe('CycleAdminCardComponent', () => {
       declarations: [CycleAdminCardComponent],
       schemas: [ CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA ],
       providers: [
-        { provide: I18n, useValue: i18nMock},
         { provide: MatDialog, useValue: {}},
         { provide: CycleMapper, useValue: {}}
       ]
@@ -39,6 +37,7 @@ describe('CycleAdminCardComponent', () => {
     fixture = TestBed.createComponent(CycleAdminCardComponent);
     component = fixture.debugElement.componentInstance;
     component.cycle = new CycleUnit(1111, '', [], new Date(111111), new Date(111111), undefined, false);
+    translate = TestBed.inject(TranslateService);
   });
 
   afterEach(() => {
@@ -51,6 +50,13 @@ describe('CycleAdminCardComponent', () => {
   });
 
   it('should return fitting data object #getDataForCycleDeletionDialog without error warning', () => {
+    spyOn(translate, 'instant').and.callFake(function(arg) {
+      if (arg === 'cycle-admin-card.deletion-dialog.content') {
+        return 'correctCycleDeletionContent';
+      } else if (arg === 'cycle-admin-card.deletion-dialog.title') {
+        return 'correctCycleDeletionTitle';
+      }
+    });
     const data: {data: DeleteDialogData} = component.getDataForCycleDeletionDialog();
 
     expect(data)
@@ -66,6 +72,7 @@ describe('CycleAdminCardComponent', () => {
   });
   it('should return correct string with getCycleTranslation', () => {
     const expected: string = 'correctCycleDeletionTitle';
+    spyOn(translate, 'instant').and.returnValue('correctCycleDeletionTitle')
 
     const actual: string = component.getCycleTranslation();
 
@@ -75,6 +82,7 @@ describe('CycleAdminCardComponent', () => {
 
   it('should return correct string with getGeneralDeleteDialogContentTranslation', () => {
     const expected: string = 'correctCycleDeletionContent';
+    spyOn(translate, 'instant').and.returnValue('correctCycleDeletionContent')
 
     const actual: string = component.getGeneralDeleteDialogContentTranslation();
 
