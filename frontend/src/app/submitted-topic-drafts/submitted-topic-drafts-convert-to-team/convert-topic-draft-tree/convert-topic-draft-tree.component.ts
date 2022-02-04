bd676a2a3dbd6fb13e182b3f94bd04bec80a52ed
pbuschmann <pbuschmann@brockhaus-ag.de>
2022-02-04 10:18:28 +0100

    feat(Convert-Topic-Draft-Tree-Dialog): More css stuff, substructure seperator line, added todo comment explaining current state and what needs to be fixed.

--- frontend/src/app/submitted-topic-drafts/submitted-topic-drafts-convert-to-team/convert-topic-draft-tree/convert-topic-draft-tree.component.ts
+++ frontend/src/app/submitted-topic-drafts/submitted-topic-drafts-convert-to-team/convert-topic-draft-tree/convert-topic-draft-tree.component.ts
@@ -18,8 +18,19 @@ export class ConvertTopicDraftTreeComponent {
     this.isOpen = !this.isOpen;
   }
 
-  test(): void {
+  selectStructure(): void {
     console.log('clicked');
     console.log(this.substructure);
+    /*
+    TODO: P.B. 04.02.2022
+        - One way to implement is that this needs to be emitted up to the convert-submitted-topic-draft-to-team component as the selected structure where it will be created.
+        - Another would be to split the create logic into its own service, remove the convert button and open a small dialog when clicking on a substructure.
+          The dialog then asks the user if that is where he wants the team to be created.
+        - I am also not sure if a tree-structure is what we want here. It becomes overcrowded when there are too many substructures (or structures have long names).
+          Ok for BAG, probably not OK for bigger companies.
+        !IMPORTANT: Mobile View doesnt work right now. The dialog is out of screen on iphone SE for example.
+
+
+    */
   }
 }
