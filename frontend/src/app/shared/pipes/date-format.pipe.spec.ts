895ee45b07cddfef8b1d715edaa955a953124275
jklein <justin.klein@brockhaus-ag.de>
2022-02-07 14:53:45 +0100

    test(date-format-pipe)

--- frontend/src/app/shared/pipes/date-format.pipe.spec.ts
+++ frontend/src/app/shared/pipes/date-format.pipe.spec.ts
@@ -1,15 +1,37 @@
 import { DateFormatPipe } from './date-format.pipe';
-import {OkrTranslationHelperService} from "../services/helper/okr-translation-helper.service";
+import {OkrTranslationHelperService} from '../services/helper/okr-translation-helper.service';
+import {of} from 'rxjs';
 
 describe('DateFormatPipe', () => {
 
   let dateFormatPipe: DateFormatPipe;
+  const testDate: Date = new Date('11.22.2021');
 
-  beforeAll ( () => {
+  beforeAll (() => {
     dateFormatPipe = new DateFormatPipe(new OkrTranslationHelperService(undefined, undefined, undefined, undefined));
   });
 
-  it('create an instance', () => {
+  it('should create an instance', () => {
     expect(dateFormatPipe).toBeTruthy();
+  });
+
+  it('should return formatted testDate', async () => {
+    dateFormatPipe.currentLanguage$ = of('en');
+    let formattedDate: string = await dateFormatPipe.transform(testDate).toPromise();
+    expect(formattedDate).toBe('11/22/2021');
+
+    dateFormatPipe.currentLanguage$ = of('de');
+    formattedDate = await dateFormatPipe.transform(testDate).toPromise();
+    expect(formattedDate).toBe('22.11.2021');
+  });
+
+  it('should return formatted testDate with Month as String', async () => {
+    dateFormatPipe.currentLanguage$ = of('en');
+    let formattedDate: string = await dateFormatPipe.transform(testDate, true).toPromise();
+    expect(formattedDate).toBe('Nov 22, 2021');
+
+    dateFormatPipe.currentLanguage$ = of('de');
+    formattedDate = await dateFormatPipe.transform(testDate, true).toPromise();
+    expect(formattedDate).toBe('22. Nov. 2021');
   });
 });
