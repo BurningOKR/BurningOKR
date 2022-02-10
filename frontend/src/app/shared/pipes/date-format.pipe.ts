e1c342bd0e7caa158b356f14b5cd62175fb6fb3b
jklein <justin.klein@brockhaus-ag.de>
2022-02-10 16:21:05 +0100

    refactor(date-format-pipe): removed console log

--- frontend/src/app/shared/pipes/date-format.pipe.ts
+++ frontend/src/app/shared/pipes/date-format.pipe.ts
@@ -18,17 +18,11 @@ export class DateFormatPipe implements PipeTransform {
   }
 
   private getDateWithMonthString$(date: Date): Observable<string> {
-    console.log('Monat String');
-
     return this.getLocalDateString$(date, {month: 'short', day: 'numeric', year: 'numeric'});
   }
 
   private getLocalDateString$(date: Date, options: any = {}) {
-    console.log('Date Function with: ', options);
-
     return this.currentLanguage$.pipe(map(language => {
-      console.log(language);
-
       return date.toLocaleDateString(language, options);
     }));
   }
