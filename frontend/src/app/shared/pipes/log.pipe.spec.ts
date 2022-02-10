5296b3273b48951329c5c23b1316f7a7e7c5b2cc
jklein <justin.klein@brockhaus-ag.de>
2022-02-10 16:20:04 +0100

    refactor(log-pipe): removed unused test

--- frontend/src/app/shared/pipes/log.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LogPipe } from './log.pipe';
-
-describe('LogPipe', () => {
-  it('create an instance', () => {
-    const pipe: LogPipe = new LogPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
