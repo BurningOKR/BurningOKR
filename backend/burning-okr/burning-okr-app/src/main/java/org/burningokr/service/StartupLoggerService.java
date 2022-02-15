f5a0419731c21e30da1dbc25676ef5cc2505df29
Timo Krischik <Timo.Krischik@brockhaus-ag.de>
2022-02-15 16:27:45 +0100

    feat(startup-logging): Added Service for startup logging

---
+++ backend/burning-okr/burning-okr-app/src/main/java/org/burningokr/service/StartupLoggerService.java
@@ -0,0 +1,32 @@
+package org.burningokr.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StartupLoggerService {
+  @Autowired private BuildProperties buildProperties;
+  private final Logger logger = LoggerFactory.getLogger("Startup");
+
+  @EventListener(ApplicationStartedEvent.class)
+  public void logReadyUp() {
+    String burning_okr_message =
+        "\n"
+            + "\n"
+            + "  ____                   _                ____  _  _______  \n"
+            + " |  _ \\                 (_)              / __ \\| |/ /  __ \\ \n"
+            + " | |_) |_   _ _ __ _ __  _ _ __   __ _  | |  | | ' /| |__) |\n"
+            + " |  _ <| | | | '__| '_ \\| | '_ \\ / _` | | |  | |  < |  _  / \n"
+            + " | |_) | |_| | |  | | | | | | | | (_| | | |__| | . \\| | \\ \\ \n"
+            + " |____/ \\__,_|_|  |_| |_|_|_| |_|\\__, |  \\____/|_|\\_\\_|  \\_\\\n"
+            + "                                  __/ |                     \n"
+            + "                                 |___/                      \n"
+            + "Version: v{}";
+    logger.info(burning_okr_message, buildProperties.getVersion());
+  }
+}
