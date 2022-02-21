f68eb7fecf8df5866a2da34380269689aeac9709
jklein <justin.klein@brockhaus-ag.de>
2022-02-21 16:37:57 +0100

    test(topic-draft-auth-service)

--- frontend/src/app/topic-drafts/topic-draft-auth.service.spec.ts
+++ frontend/src/app/topic-drafts/topic-draft-auth.service.spec.ts
@@ -4,19 +4,33 @@ import { CurrentUserService } from '../core/services/current-user.service';
 import { of } from 'rxjs';
 
 describe('TopicDraftAuthServiceService', () => {
+
   let topicDraftAuthService: TopicDraftAuthService;
   const currentUserServiceMock: CurrentUserService = new CurrentUserService(undefined, undefined);
-  const mockUser: User = new User();
+  const mockUser: User = new User('mockId');
 
-  currentUserServiceMock.isCurrentUserAdmin$ = jest.fn(() => of(true));
-  currentUserServiceMock.isCurrentUserAuditor$ = jest.fn(() => of(true));
-  currentUserServiceMock.getCurrentUser$ = jest.fn(() => of(mockUser));
+  currentUserServiceMock.isCurrentUserAdmin$ = jest.fn().mockReturnValue(of(true));
+  currentUserServiceMock.isCurrentUserAuditor$ = jest.fn().mockReturnValue(of(true));
+  currentUserServiceMock.getCurrentUser$ = jest.fn().mockReturnValue(of(mockUser));
 
   beforeEach(() => {
     topicDraftAuthService = new TopicDraftAuthService(currentUserServiceMock);
   });
 
+  it('should get the correct Creator Status', () => {
+    expect((topicDraftAuthService as any).isCurrentUserCreator('mockId')).toBeTruthy();
+    expect((topicDraftAuthService as any).isCurrentUserCreator('notMockId')).toBeFalsy();
+  });
+
   it('should be created', () => {
     expect(topicDraftAuthService).toBeTruthy();
+    expect((topicDraftAuthService as any).isAdmin).toBeTruthy();
+    expect((topicDraftAuthService as any).isAuditor).toBeTruthy();
+    expect((topicDraftAuthService as any).currentUser).toEqual(mockUser);
+
+    expect(topicDraftAuthService).toBeTruthy();
+    expect((topicDraftAuthService as any).isAdmin).toBeTruthy();
+    expect((topicDraftAuthService as any).isAuditor).toBeTruthy();
+    expect((topicDraftAuthService as any).currentUser).toEqual(mockUser);
   });
 });
