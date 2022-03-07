6a89207208ca15f4ebb65d3fb7869c7c99e7e4a8
jklein <justin.klein@brockhaus-ag.de>
2022-03-07 10:38:33 +0100

    test(TopicDraftStatusService): added tests for functions evaluating the status

--- frontend/src/app/topic-drafts/topic-draft-status.service.spec.ts
+++ frontend/src/app/topic-drafts/topic-draft-status.service.spec.ts
@@ -10,11 +10,14 @@ describe('TopicDraftStatusService', () => {
   topicDraftMapperMock.updateTopicDraftStatus$ = jest.fn().mockReturnValue(of(undefined));
   let testTopicDraft: OkrTopicDraft;
 
+  const draftTopicDraft: OkrTopicDraft = getMockTopicDraft(status.draft);
+  const submittedTopicDraft: OkrTopicDraft = getMockTopicDraft(status.submitted);
+  const approvedTopicDraft: OkrTopicDraft = getMockTopicDraft(status.approved);
+  const rejectedTopicDraft: OkrTopicDraft = getMockTopicDraft(status.rejected);
+
   beforeEach(() => {
     topicDraftStatusService = new TopicDraftStatusService(topicDraftMapperMock);
-    testTopicDraft = new OkrTopicDraft(0,status.draft, undefined, 0, '', undefined,
-      undefined, undefined, undefined, undefined, undefined, undefined,
-      undefined, undefined, undefined);
+    testTopicDraft = getMockTopicDraft(status.draft);
   });
 
   it('should be created', () => {
@@ -38,4 +41,38 @@ describe('TopicDraftStatusService', () => {
     topicDraftStatusService.changeTopicDraftStatusTo(testTopicDraft, null);
     expect(testTopicDraft.currentStatus).toBe(status.draft);
   });
+
+  it('should show if the topicDraft can be converted to a team',  () => {
+    expect(TopicDraftStatusService.isTopicDraftConvertableToTeam(draftTopicDraft)).toBe(false);
+    expect(TopicDraftStatusService.isTopicDraftConvertableToTeam(submittedTopicDraft)).toBe(false);
+    expect(TopicDraftStatusService.isTopicDraftConvertableToTeam(approvedTopicDraft)).toBe(true);
+    expect(TopicDraftStatusService.isTopicDraftConvertableToTeam(rejectedTopicDraft)).toBe(false);
+  });
+
+  it('should show if the topicDraft can be approved or rejected',  () => {
+    expect(TopicDraftStatusService.isTopicDraftInApprovingStage(draftTopicDraft)).toBe(false);
+    expect(TopicDraftStatusService.isTopicDraftInApprovingStage(submittedTopicDraft)).toBe(true);
+    expect(TopicDraftStatusService.isTopicDraftInApprovingStage(approvedTopicDraft)).toBe(true);
+    expect(TopicDraftStatusService.isTopicDraftInApprovingStage(rejectedTopicDraft)).toBe(true);
+  });
+
+  it('should show if the topicDraft can be edited',  () => {
+    expect(TopicDraftStatusService.isTopicDraftInSubmissionStage(draftTopicDraft)).toBe(true);
+    expect(TopicDraftStatusService.isTopicDraftInSubmissionStage(submittedTopicDraft)).toBe(true);
+    expect(TopicDraftStatusService.isTopicDraftInSubmissionStage(approvedTopicDraft)).toBe(false);
+    expect(TopicDraftStatusService.isTopicDraftInSubmissionStage(rejectedTopicDraft)).toBe(false);
+  });
+
+  it('should show if the topicDraft is approved or rejected',  () => {
+    expect(TopicDraftStatusService.isTopicDraftApprovedOrRejected(draftTopicDraft)).toBe(false);
+    expect(TopicDraftStatusService.isTopicDraftApprovedOrRejected(submittedTopicDraft)).toBe(false);
+    expect(TopicDraftStatusService.isTopicDraftApprovedOrRejected(approvedTopicDraft)).toBe(true);
+    expect(TopicDraftStatusService.isTopicDraftApprovedOrRejected(rejectedTopicDraft)).toBe(true);
+  });
 });
+
+function getMockTopicDraft(topicDraftStatus: status): OkrTopicDraft {
+  return new OkrTopicDraft(0, topicDraftStatus, undefined, 0, '', undefined,
+    undefined, undefined, undefined, undefined, undefined, undefined,
+    undefined, undefined, undefined);
+}
