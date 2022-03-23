a55580264a0f2a6b9ef9b571ea208ac545c0e0c7
pbuschmann <pbuschmann@brockhaus-ag.de>
2022-03-23 08:59:02 +0100

    feat(Dashboard): Added dashboard creation and overview components; Added routing to those components; Added menu-entry to reach overview
    Signed-off-by: jklein <justin.klein@brockhaus-ag.de>

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -7,7 +7,9 @@ import { LandingPageNavigationComponent } from './core/landing-page-router/landi
 import { AdminRoleGuard } from './admin/admin-role-guard';
 import { CycleAdminContainerComponent } from './cycle-admin/cycle-admin-container/cycle-admin-container.component';
 import { AuthGuard } from './core/auth/guards/auth.guard';
-import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
+import { CreateDashboardComponent } from './dashboard/sites/create-dashboard/create-dashboard.component';
+import { DashboardOverviewComponent } from './dashboard/sites/dashboard-overview/dashboard-overview.component';
+import { DashboardComponent } from './dashboard/sites/dashboard/dashboard.component';
 import { OkrUnitDashboardComponent } from './okr-units/okr-unit-dashboard/okr-unit-dashboard.component';
 import { ErrorComponent } from './core/error/error.component';
 import { NoMailInformationComponent } from './information/no-mail-information/no-mail-information.component';
@@ -42,7 +44,9 @@ const routes: Routes = [
   },
   { path: 'error', component: ErrorComponent },
   { path: 'noMailInformation', component: NoMailInformationComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'companies/:companyId/dashboard-overview', component: DashboardOverviewComponent },
+  { path: 'companies/:companyId/dashboard', component: DashboardComponent },
+  { path: 'companies/:companyId/create-dashboard', component: CreateDashboardComponent },
   { path: '', redirectTo: environment.playground ? 'demo' : 'landingpage', pathMatch: 'full' },
   { path: '**', redirectTo: environment.playground ? 'landingpage' : '' },
 ];
---
+++ frontend/src/app/dashboard/sites/create-dashboard/create-dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-dashboard',
+  templateUrl: './create-dashboard.component.html',
+  styleUrls: ['./create-dashboard.component.scss']
+})
+export class CreateDashboardComponent {
+
+}
