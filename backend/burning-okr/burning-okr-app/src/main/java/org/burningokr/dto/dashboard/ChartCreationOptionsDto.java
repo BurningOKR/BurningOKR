aa5d741e82f735b9d12d12f4d21d9d4d14c25f7a
pbuschmann <pbuschmann@brockhaus-ag.de>
2022-03-29 11:51:39 +0200

    feat(DTOs): Adding backend dtos and changes frontend dtos/enums

---
+++ backend/burning-okr/burning-okr-app/src/main/java/org/burningokr/dto/dashboard/ChartCreationOptionsDto.java
@@ -0,0 +1,12 @@
+package org.burningokr.dto.dashboard;
+
+import lombok.Data;
+import org.burningokr.dto.okrUnit.OkrDepartmentDto;
+
+@Data
+public class ChartCreationOptionsDto {
+  private Long chartCreationOptionsId:;
+  private String title;
+  private ChartType chartType;
+  private OkrDepartmentDto[] teams;
+}
