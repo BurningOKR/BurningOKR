d973c936e4faacb24d83cee14cfdb51df5498ed7
jklein <justin.klein@brockhaus-ag.de>
2022-03-30 09:13:16 +0200

    feat(TopicDraft): Added functionality to convert a TD to a Department

--- backend/burning-okr/burning-okr-logic/src/main/java/org/burningokr/service/topicDraft/ConvertTopicDraftToTeamService.java
+++ backend/burning-okr/burning-okr-logic/src/main/java/org/burningokr/service/topicDraft/ConvertTopicDraftToTeamService.java
@@ -17,5 +17,41 @@ import javax.transaction.Transactional;
 public class ConvertTopicDraftToTeamService {
 
   private final OkrTopicDraftService okrTopicDraftService;
+  private final OkrUnitServiceFactory<OkrChildUnit> okrUnitServiceFactory;
 
+  @Transactional
+  public OkrDepartment convertTopicDraftToTeam (int topicDraftId, int parentOkrUnitId) {
+    OkrTopicDraft topicDraft = this.okrTopicDraftService.findById(topicDraftId);
+    OkrUnitService<OkrChildUnit> okrUnitService = this.okrUnitServiceFactory.getUserOkrUnitService();
+    OkrDepartment okrDepartment = new OkrDepartment();
+    okrDepartment.setId(null);
+    copyDetailsFromTopicDraftToDepartment(topicDraft, okrDepartment);
+    return okrDepartment;
+  }
+
+  public void copyDetailsFromTopicDraftToDepartment (OkrTopicDraft topicDraft, OkrDepartment okrDepartment) {
+    okrDepartment.setActive(true);
+    okrDepartment.setLabel("Abteilung");
+    okrDepartment.setName(topicDraft.getName());
+    okrDepartment.setOkrTopicDescription(convertTopicDraftToOkrTopicDescription(topicDraft));
+  }
+
+  public OkrTopicDescription convertTopicDraftToOkrTopicDescription (OkrTopicDraft topicDraft) {
+    // TODO Copy-Constructor?
+    OkrTopicDescription topicDescription = new OkrTopicDescription();
+    topicDescription.setDescription(topicDraft.getDescription());
+    topicDescription.setBeginning(topicDraft.getBeginning());
+    topicDescription.setDelimitation(topicDraft.getDelimitation());
+    topicDescription.setDependencies(topicDraft.getDependencies());
+    topicDescription.setContributesTo(topicDraft.getContributesTo());
+    topicDescription.setName(topicDraft.getName());
+    topicDescription.setStartTeam(topicDraft.getStartTeam());
+    topicDescription.setHandoverPlan(topicDraft.getHandoverPlan());
+    topicDescription.setInitiatorId(topicDraft.getInitiatorId());
+    topicDescription.setId(topicDraft.getId());
+    topicDescription.setResources(topicDraft.getResources());
+    topicDescription.setStakeholders(topicDraft.getStakeholders());
+
+    return topicDescription;
+  }
 }
