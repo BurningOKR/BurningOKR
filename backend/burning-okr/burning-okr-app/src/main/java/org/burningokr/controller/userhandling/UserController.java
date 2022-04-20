86679bb358d333740b6be000b20d882d1896c04d
7Grabi7 <robin.froelke@gmx.de>
2022-04-20 13:05:19 +0200

    Added activeUsers RequestParam for getAllUsers

--- backend/burning-okr/burning-okr-app/src/main/java/org/burningokr/controller/userhandling/UserController.java
+++ backend/burning-okr/burning-okr-app/src/main/java/org/burningokr/controller/userhandling/UserController.java
@@ -5,11 +5,14 @@ import java.util.UUID;
 import org.burningokr.annotation.RestApiController;
 import org.burningokr.dto.users.UserDto;
 import org.burningokr.mapper.users.UserMapper;
+import org.burningokr.model.users.User;
 import org.burningokr.service.userhandling.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestApiController
 public class UserController {
@@ -29,8 +32,17 @@ public class UserController {
   }
 
   @GetMapping("/users")
-  public ResponseEntity<Collection<UserDto>> getAllUsers() {
-    return ResponseEntity.ok(userMapper.mapEntitiesToDtos(userService.findAll()));
+  public ResponseEntity<Collection<UserDto>> getAllUsers(@RequestParam(value = "activeUsers", required = false) Boolean activeUsers) {
+    Collection<User> userList;
+    if(activeUsers == null){
+      userList = userService.findAll();
+    } else if (activeUsers){
+      userList = null;
+    } else {
+      userList = null;
+    }
+
+    return ResponseEntity.ok(userMapper.mapEntitiesToDtos(userList));
   }
 
   @GetMapping("/users/{userId}")
