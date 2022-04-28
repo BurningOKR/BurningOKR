415e23a0900eea5d6c328978cfd33210790d6fd4
jklein <justin.klein@brockhaus-ag.de>
2022-04-28 13:58:41 +0200

    Added Beseline for writing tests for convert-topic-draft-to-team.service.ts

--- frontend/src/app/topic-drafts/convert-topic-draft-to-team/convert-topic-draft-to-team.service.spec.ts
+++ frontend/src/app/topic-drafts/convert-topic-draft-to-team/convert-topic-draft-to-team.service.spec.ts
@@ -1,16 +1,25 @@
-import { TestBed } from '@angular/core/testing';
-
 import { ConvertTopicDraftToTeamService } from './convert-topic-draft-to-team.service';
+import { OkrTopicDraft } from '../../shared/model/ui/OrganizationalUnit/okr-topic-draft/okr-topic-draft';
+import { Observable, of } from 'rxjs';
+import { OkrDepartment } from '../../shared/model/ui/OrganizationalUnit/okr-department';
 
 describe('ConvertTopicDraftToTeamService', () => {
-  let service: ConvertTopicDraftToTeamService;
+  let convertTopicDraftToTeamService: ConvertTopicDraftToTeamService;
+  const topicDraftApiServiceMock: any = {
+    convertTopicDraftToTeam$: jest.fn()
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ConvertTopicDraftToTeamService);
+    topicDraftApiServiceMock.convertTopicDraftToTeam$.mockReset();
+    topicDraftApiServiceMock.convertTopicDraftToTeam$.mockReturnValue(convertTopicDraftToDepartmentMock$);
+    convertTopicDraftToTeamService = new ConvertTopicDraftToTeamService(topicDraftApiServiceMock);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(convertTopicDraftToTeamService).toBeTruthy();
   });
+
+  function convertTopicDraftToDepartmentMock$(topicDraft: OkrTopicDraft): Observable<OkrDepartment> {
+    return of(new OkrDepartment(1, `Department of ${topicDraft.name}`, [], -1, 'Team', topicDraft.initiatorId, '', [], true,  true));
+  }
 });
