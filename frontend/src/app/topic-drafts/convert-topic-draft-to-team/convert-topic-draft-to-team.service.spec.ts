4ed0742a0fbf296d44bf0281e59451e757d5dc74
jklein <justin.klein@brockhaus-ag.de>
2022-05-02 12:02:51 +0200

    test(convert-topic-draft-to-team-service): setting of the selectedStructure

--- frontend/src/app/topic-drafts/convert-topic-draft-to-team/convert-topic-draft-to-team.service.spec.ts
+++ frontend/src/app/topic-drafts/convert-topic-draft-to-team/convert-topic-draft-to-team.service.spec.ts
@@ -1,25 +1,37 @@
 import { ConvertTopicDraftToTeamService } from './convert-topic-draft-to-team.service';
 import { OkrTopicDraft } from '../../shared/model/ui/OrganizationalUnit/okr-topic-draft/okr-topic-draft';
-import { Observable, of } from 'rxjs';
+import { Observable, of, ReplaySubject } from 'rxjs';
 import { OkrDepartment } from '../../shared/model/ui/OrganizationalUnit/okr-department';
+import { Structure } from '../../shared/model/ui/OrganizationalUnit/structure';
 
 describe('ConvertTopicDraftToTeamService', () => {
   let convertTopicDraftToTeamService: ConvertTopicDraftToTeamService;
+  let selectedUnit$: ReplaySubject<Structure>;
+
   const topicDraftApiServiceMock: any = {
     convertTopicDraftToTeam$: jest.fn()
   };
+
+  const mockStructure1: Structure = getMockStructure(1, 'MockStructure1', 'MockLabel1');
 
   beforeEach(() => {
     topicDraftApiServiceMock.convertTopicDraftToTeam$.mockReset();
     topicDraftApiServiceMock.convertTopicDraftToTeam$.mockReturnValue(convertTopicDraftToDepartmentMock$);
     convertTopicDraftToTeamService = new ConvertTopicDraftToTeamService(topicDraftApiServiceMock);
+    selectedUnit$ = (convertTopicDraftToTeamService as any).selectedUnit$;
   });
 
-  it('should be created', () => {
-    expect(convertTopicDraftToTeamService).toBeTruthy();
+  it('should set the Structure correctly', () => {
+    selectedUnit$.asObservable().subscribe(structure => expect(structure).toBe(undefined));
+    convertTopicDraftToTeamService.setSelectedUnit(mockStructure1);
+    selectedUnit$.asObservable().subscribe(structure => expect(structure).toBe(mockStructure1));
   });
 
   function convertTopicDraftToDepartmentMock$(topicDraft: OkrTopicDraft): Observable<OkrDepartment> {
     return of(new OkrDepartment(1, `Department of ${topicDraft.name}`, [], -1, 'Team', topicDraft.initiatorId, '', [], true,  true));
+  }
+
+  function getMockStructure(id: number, name: string, label: string): Structure {
+    return new Structure(id, name, label, [], []);
   }
 });
