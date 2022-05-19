4540c7db1eec33335b1f92b6e73c01c06b440a16
pbuschmann <pbuschmann@brockhaus-ag.de>
2022-05-19 13:08:36 +0200

    feat(Dashboard Translations): Added translations for dashboard sites.
    Signed-off-by: jklein <justin.klein@brockhaus-ag.de>

--- backend/burning-okr/burning-okr-app/src/main/resources/db/migration/postgresql/V67__KeyResultHistory.sql
+++ backend/burning-okr/burning-okr-app/src/main/resources/db/migration/postgresql/V67__KeyResultHistory.sql
@@ -1,16 +1,16 @@
 CREATE TABLE key_result_history
 (
-  id BIGINT NOT NULL,
-  start_value BIGINT,
-  current_value BIGINT,
-  target_value BIGINT,
-  date_changed DATE,
-  key_result_id BIGINT,
-  CONSTRAINT pk_keyresulthistory PRIMARY KEY (id)
+    id BIGINT NOT NULL,
+    start_value BIGINT,
+    current_value BIGINT,
+    target_value BIGINT,
+    date_changed DATE,
+    key_result_id BIGINT,
+    CONSTRAINT pk_keyresulthistory PRIMARY KEY (id)
 );
 
 ALTER TABLE key_result_history
-  ADD CONSTRAINT fk_keyresulthistory_on_keyresult FOREIGN KEY (key_result_id) REFERENCES key_result (id);
+    ADD CONSTRAINT fk_keyresulthistory_on_keyresult FOREIGN KEY (key_result_id) REFERENCES key_result (id);
 
 
 INSERT INTO key_result_history (id, start_value, current_value, target_value, date_changed, key_result_id)
