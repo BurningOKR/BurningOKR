2a163deb830766f77cfd952b3005852b8b216f22
pbuschmann <pbuschmann@brockhaus-ag.de>
2022-05-23 11:53:22 +0200

    test(ChartOptionsCard): Fix for test

--- frontend/src/app/dashboard/sites/create-dashboard/chart-options-card/chart-options-card.component.spec.ts
+++ frontend/src/app/dashboard/sites/create-dashboard/chart-options-card/chart-options-card.component.spec.ts
@@ -1,21 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MaterialTestingModule } from '../../../../testing/material-testing.module';
+import { ChartCreationOptionsDto } from '../../../model/dto/chart-creation-options.dto';
 
 import { ChartOptionsCardComponent } from './chart-options-card.component';
 
 describe('ChartOptionsCardComponent', () => {
   let component: ChartOptionsCardComponent;
   let fixture: ComponentFixture<ChartOptionsCardComponent>;
+  const chart: ChartCreationOptionsDto = {} as ChartCreationOptionsDto;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ChartOptionsCardComponent ]
-    })
-    .compileComponents();
+        imports: [MaterialTestingModule],
+        declarations: [ChartOptionsCardComponent],
+      })
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ChartOptionsCardComponent);
     component = fixture.componentInstance;
+    component.chart = chart;
     fixture.detectChanges();
   });
 
