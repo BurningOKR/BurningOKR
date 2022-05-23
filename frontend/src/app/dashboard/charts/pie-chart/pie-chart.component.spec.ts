4179f060cf9b2d6deb6fbfd6ee10949fe90a5cb0
pbuschmann <pbuschmann@brockhaus-ag.de>
2022-05-23 11:01:31 +0200

    test(Chart components): Fix for chart tests

--- frontend/src/app/dashboard/charts/pie-chart/pie-chart.component.spec.ts
+++ frontend/src/app/dashboard/charts/pie-chart/pie-chart.component.spec.ts
@@ -1,21 +1,26 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PieChartOptions } from '../../model/ui/pie-chart-options';
 
 import { PieChartComponent } from './pie-chart.component';
 
 describe('PieChartComponent', () => {
   let component: PieChartComponent;
   let fixture: ComponentFixture<PieChartComponent>;
+  const pieChartOptions: PieChartOptions = {} as PieChartOptions;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PieChartComponent ]
-    })
-    .compileComponents();
+        declarations: [PieChartComponent],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      })
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PieChartComponent);
     component = fixture.componentInstance;
+    component.chartOptions = pieChartOptions;
     fixture.detectChanges();
   });
 
