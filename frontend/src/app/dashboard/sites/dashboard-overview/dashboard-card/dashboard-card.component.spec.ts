709570dee403f28437bbae897b1d950762655ccb
pbuschmann <pbuschmann@brockhaus-ag.de>
2022-05-23 11:12:06 +0200

    test(Dashboard-card): Tests now run

--- frontend/src/app/dashboard/sites/dashboard-overview/dashboard-card/dashboard-card.component.spec.ts
+++ frontend/src/app/dashboard/sites/dashboard-overview/dashboard-card/dashboard-card.component.spec.ts
@@ -1,21 +1,31 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { User } from '../../../../shared/model/api/user';
+import { MaterialTestingModule } from '../../../../testing/material-testing.module';
+import { Dashboard } from '../../../model/ui/dashboard';
 
 import { DashboardCardComponent } from './dashboard-card.component';
 
 describe('DashboardCardComponent', () => {
   let component: DashboardCardComponent;
   let fixture: ComponentFixture<DashboardCardComponent>;
+  const dashboard: Dashboard = {} as Dashboard;
+  const creator: User = {} as User;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DashboardCardComponent ]
-    })
-    .compileComponents();
+        imports: [MaterialTestingModule],
+        declarations: [DashboardCardComponent],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      })
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DashboardCardComponent);
     component = fixture.componentInstance;
+    dashboard.creator = creator;
+    component.dashboard = dashboard;
     fixture.detectChanges();
   });
 
