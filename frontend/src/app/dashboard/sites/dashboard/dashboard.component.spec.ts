58b4aba7146f2f9f14ee046eef6e9c3fbfab400c
pbuschmann <pbuschmann@brockhaus-ag.de>
2022-05-23 11:48:15 +0200

    test(Dashboard): Fix for test

--- frontend/src/app/dashboard/sites/dashboard/dashboard.component.spec.ts
+++ frontend/src/app/dashboard/sites/dashboard/dashboard.component.spec.ts
@@ -1,4 +1,10 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NGXLogger } from 'ngx-logger';
+import { AuthenticationService } from '../../../core/auth/services/authentication.service';
+import { MaterialTestingModule } from '../../../testing/material-testing.module';
 
 import { DashboardComponent } from './dashboard.component';
 
@@ -8,9 +14,15 @@ describe('DashboardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
-    })
-    .compileComponents();
+        imports: [RouterTestingModule, MaterialTestingModule, HttpClientTestingModule],
+        declarations: [DashboardComponent],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA],
+        providers: [
+          { provide: AuthenticationService, useValue: {} },
+          { provide: NGXLogger, useValue: {} },
+        ],
+      })
+      .compileComponents();
   });
 
   beforeEach(() => {
