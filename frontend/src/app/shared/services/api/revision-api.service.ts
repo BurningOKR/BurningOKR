cde2443aa365a2fab33fb7ecf623722ad50d2e8f
Maxim Wilman <mwilman@brockhaus-ag.de>
2023-02-21 14:18:29 +0100

    fixed lint

--- frontend/src/app/shared/services/api/revision-api.service.ts
+++ frontend/src/app/shared/services/api/revision-api.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
-import {ApiHttpService} from "../../../core/services/api-http.service";
-import {TaskId} from "../../model/id-types";
-import {Observable} from "rxjs";
-import {RevisionDto} from "../../model/api/revision-dto";
+import { ApiHttpService } from '../../../core/services/api-http.service';
+import { TaskId } from '../../model/id-types';
+import { Observable } from 'rxjs';
+import { RevisionDto } from '../../model/api/revision-dto';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RevisionApiService {
 
-  constructor(private api: ApiHttpService) { }
+  constructor(private api: ApiHttpService) {
+  }
 
   getRevisionsForTask$(taskId: TaskId): Observable<RevisionDto[]> {
     return this.api.getData$(`tasks/revisions/${taskId}`);
