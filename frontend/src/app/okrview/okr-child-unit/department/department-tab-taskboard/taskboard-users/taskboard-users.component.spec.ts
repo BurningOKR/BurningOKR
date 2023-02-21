602dde368b80f966fa4a9ec69cef6242c2a866c4
Maxim Wilman <mwilman@brockhaus-ag.de>
2023-02-21 15:53:55 +0100

    fixed tests

--- frontend/src/app/okrview/okr-child-unit/department/department-tab-taskboard/taskboard-users/taskboard-users.component.spec.ts
+++ frontend/src/app/okrview/okr-child-unit/department/department-tab-taskboard/taskboard-users/taskboard-users.component.spec.ts
@@ -1,26 +1,48 @@
-//TODO - fix test
-/*
- describe('TaskboardUsersComponent', () => {
- let component: TaskboardUsersComponent;
- let fixture: ComponentFixture<TaskboardUsersComponent>;
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
- beforeEach(async () => {
- await TestBed.configureTestingModule({
- declarations: [TaskboardUsersComponent, AvatarComponent],
- imports: [OAuthModule.forRoot(), HttpClientTestingModule],
- })
- .compileComponents();
- });
+import { TaskboardUsersComponent } from './taskboard-users.component';
+import { AvatarComponent } from 'ngx-avatars';
+import { OAuthModule } from 'angular-oauth2-oidc';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CurrentUserService } from '../../../../../core/services/current-user.service';
+import { UserService } from '../../../../../shared/services/helper/user.service';
+import { of } from 'rxjs';
 
- beforeEach(() => {
- fixture = TestBed.createComponent(TaskboardUsersComponent);
- component = fixture.componentInstance;
- fixture.detectChanges();
- });
+describe('TaskboardUsersComponent', () => {
+  let component: TaskboardUsersComponent;
+  let fixture: ComponentFixture<TaskboardUsersComponent>;
 
- it('should create', () => {
- expect(component).toBeTruthy();
- });
+  const currentUserServiceMock: any = {
+    getCurrentUserId$: jest.fn(),
+    isCurrentUserAdmin$: jest.fn(),
+  };
 
- });
- */
+  const localUserServiceMock: any = {
+    getUsers$: jest.fn(),
+    getAdminIds$: jest.fn(),
+  };
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TaskboardUsersComponent, AvatarComponent],
+      imports: [OAuthModule.forRoot(), HttpClientTestingModule],
+      providers: [
+        { provide: CurrentUserService, useValue: currentUserServiceMock },
+        { provide: UserService, useValue: localUserServiceMock },
+      ],
+    })
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    currentUserServiceMock.getCurrentUserId$.mockReturnValue(of('1'));
+
+    fixture = TestBed.createComponent(TaskboardUsersComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
