143ac3df42fb58951fed4ce919dbf9a6728bcdd8
Daniel Jonas <djonas@brockhaus-ag.de>
2023-03-01 16:54:18 +0100

    feat(dashboard): Userstory 16909 - Dashboard Title can be edited now

--- frontend/src/app/dashboard/sites/edit-dashboard/edit-dashboard.component.ts
+++ frontend/src/app/dashboard/sites/edit-dashboard/edit-dashboard.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { Dashboard } from '../../model/ui/dashboard';
+import { map, switchMap } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
+import { DashboardService } from '../../services/dashboard.service';
 
 @Component({
   selector: 'app-edit-dashboard',
@@ -6,10 +11,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-dashboard.component.scss'],
 })
 export class EditDashboardComponent implements OnInit {
-  constructor() {
+  dashboard$: Observable<Dashboard>;
+
+  constructor(private readonly activatedRoute: ActivatedRoute, private readonly dashboardService: DashboardService) {
   }
 
   ngOnInit(): void {
+    this.dashboard$ = this.activatedRoute.paramMap.pipe(
+      map(params => +params.get('dashboardId')),
+      switchMap((dashboardId: number) => this.dashboardService.getDashboardById$(dashboardId)),
+    );
   }
 
 }
