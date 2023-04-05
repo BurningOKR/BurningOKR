59bc4a75c3acab73678da2a396a91cd2d1f0d64e
Daniel Jonas <djonas@brockhaus-ag.de>
2023-04-05 15:13:15 +0200

    feat(dashboard): Userstory 16909 - Test File Compiles now and isTruthy test succeeds.

--- frontend/src/app/dashboard/sites/edit-dashboard/dashboard-modification/dashboard-modification.component.spec.ts
+++ frontend/src/app/dashboard/sites/edit-dashboard/dashboard-modification/dashboard-modification.component.spec.ts
@@ -1,164 +1,165 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DashboardModificationComponent } from './dashboard-modification.component';
 import { DepartmentMapper } from '../../../../shared/services/mapper/department.mapper';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MatDialog } from '@angular/material/dialog';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
-import { AuthenticationService } from '../../../../core/auth/services/authentication.service';
-import { OAuthService } from 'angular-oauth2-oidc';
+import { of } from 'rxjs';
+import { OkrDepartment } from '../../../../shared/model/ui/OrganizationalUnit/okr-department';
+import { Dashboard } from '../../../model/ui/dashboard';
+import { UnitType } from '../../../../shared/model/api/OkrUnit/unit-type.enum';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// const departmentMapperMock: any = {
+//   getDepartmentById$: jest.fn(getDepartmentByIdMock$),
+//   getAllDepartmentsForCompanyFlatted$: jest.fn(getAllDepartmentsForCompanyFlattedMock$)
+// };
+//
+// function getDepartmentByIdMock$(departmentId: OkrUnitId): Observable<OkrDepartment> {
+//   console.log('Hello World!');
+//
+//   return null; //of(testCompanyList.filter(c => c.id === companyId).pop());
+// }
+//
+// function getAllDepartmentsForCompanyFlattedMock$(companyId: CompanyId): Observable<OkrDepartment[]> {
+//   console.log('Hello World!');
+//
+//   return [okrTestDepartment1, okrTestDepartment2]; //of(testCompanyList.filter(c => c.id === companyId).pop());
+// }
+//
+
+const okrTestDepartment1: OkrDepartment = {
+  id: 1,
+  isActive: true,
+  isParentUnitABranch: false,
+  label: 'department',
+  name: 'test1',
+  objectives: [],
+  okrMasterId: '',
+  okrMemberIds: [],
+  okrTopicSponsorId: '',
+  parentUnitId: 1,
+  type: UnitType.DEPARTMENT,
+};
+
+const okrTestDepartment2: OkrDepartment = {
+  id: 2,
+  isActive: true,
+  isParentUnitABranch: false,
+  label: 'department',
+  name: 'test2',
+  objectives: [],
+  okrMasterId: '',
+  okrMemberIds: [],
+  okrTopicSponsorId: '',
+  parentUnitId: 1,
+  type: UnitType.DEPARTMENT,
+};
+
+const testDashboard: Dashboard = {
+  id: 1000,
+  title: 'DashboardMock',
+  companyId: 1,
+  creatorId: '1',
+  charts: [],
+  creationDate: new Date(),
+};
+
+const departmentService: any = {
+  getAllDepartmentsForCompanyFlatted$: jest.fn(),
+};
 
 describe('DashboardModificationComponent', () => {
   let component: DashboardModificationComponent;
   let fixture: ComponentFixture<DashboardModificationComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
       imports: [
         HttpClientTestingModule,
-        TranslateModule,
+        TranslateModule.forRoot(),
+        ReactiveFormsModule,
       ],
       declarations: [DashboardModificationComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
       providers: [
-        { provide: DepartmentMapper }, //, useClass: DepartmentMapper},
+        { provide: DepartmentMapper, useValue: departmentService },
         { provide: MatDialog, useValue: {} },
-        { provide: TranslateService, useValue: {} },
-        { provide: AuthenticationService },
-        { provide: OAuthService },
-        // { provide: DepartmentService}
       ],
     })
-      .overrideComponent(DashboardModificationComponent, {})
       .compileComponents();
+
+    departmentService.getAllDepartmentsForCompanyFlatted$.mockReset();
+    departmentService.getAllDepartmentsForCompanyFlatted$.mockReturnValue(of([okrTestDepartment1, okrTestDepartment2]));
 
     fixture = TestBed.createComponent(DashboardModificationComponent);
     component = fixture.componentInstance;
-    component.dashboard = {
-      id: 1000,
-      title: 'DashboardMock',
-      companyId: 1,
-      creatorId: '1',
-      charts: [],
-      creationDate: null,
-    };
+    component.dashboard = testDashboard;
     fixture.detectChanges();
-  });
+  }));
 
-  // afterEach(() => {
-  //   fixture.destroy();
+  // beforeEach(() => {
+  //   // fixture = TestBed.createComponent(DashboardModificationComponent);
+  //   // component = fixture.componentInstance;
+  //   // component = TestBed.inject(DashboardModificationComponent);
+  //   // departmentMapperMock.getDepartmentById$.mockReset();
+  //   // departmentMapperMock.getDepartmentById$.mockImplementation(getDepartmentByIdMock$);
+  //   // departmentMapperMock.getAllDepartmentsForCompanyFlatted$.mockReset();
+  //   // departmentMapperMock.getAllDepartmentsForCompanyFlatted$.mockImplementation(getAllDepartmentsForCompanyFlattedMock$);
+  //
+  //   // departmentService.getAllDepartmentsForCompanyFlatted$.mockReset();
+  //   // departmentService.getAllDepartmentsForCompanyFlatted$.mockReturnValue(of([okrTestDepartment1, okrTestDepartment2]));
+  //   //
+  //   // fixture = TestBed.createComponent(DashboardModificationComponent);
+  //   // component = fixture.componentInstance;
+  //   // component.dashboard = testDashboard;
+  //   // fixture.detectChanges();
   // });
 
-  it('should create', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
   });
-});
 
-// import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-//
-// import { DashboardModificationComponent } from './dashboard-modification.component';
-// import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
-// import { HttpClientTestingModule } from '@angular/common/http/testing';
-// import { MatDialog } from '@angular/material/dialog';
-// import { DepartmentMapper } from '../../../../shared/services/mapper/department.mapper';
-// import { TranslateModule, TranslateService } from '@ngx-translate/core';
-//
-// describe('DashboardModificationComponent', () => {
-//   let fixture: ComponentFixture<DashboardModificationComponent>;
-//   let component: DashboardModificationComponent;
-//   // let department: OkrDepartment;
-//
-//   // const departmentService: any = {
-//   //   getAllDepartmentsForCompanyFlatted$: jest.fn(),
-//   // };
-//
-//   // beforeEach(waitForAsync(() => {
-//   //   TestBed.configureTestingModule({
-//   //     imports: [
-//   //       HttpClientTestingModule,
-//   //       TranslateModule,
-//   //     ],
-//   //     declarations: [DashboardModificationComponent],
-//   //     schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
-//   //     providers: [
-//   //       { provide: DepartmentMapper, useValue: {} },
-//   //       { provide: MatDialog, useValue: {} },
-//   //       { provide: TranslateService, useValue: {} },
-//   //     ],
-//   //   })
-//   //     .compileComponents();
-//   // }));
-//
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       imports: [
-//         HttpClientTestingModule,
-//         TranslateModule,
-//       ],
-//       declarations: [DashboardModificationComponent],
-//       schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
-//       providers: [
-//         { provide: DepartmentMapper, useValue: {} },
-//         { provide: MatDialog, useValue: {} },
-//         { provide: TranslateService, useValue: {} },
-//       ],
-//     })
-//       .compileComponents();
-//
-//     fixture = TestBed.createComponent(DashboardModificationComponent);
-//     component = fixture.componentInstance;
-//
-//     // component.dashboard = {
-//     //   id: 1000,
-//     //   title: 'DashboardMock',
-//     //   companyId: 1,
-//     //   creatorId: '1',
-//     //   charts: [],
-//     //   creationDate: null,
-//     // };
-//
-//     // department = {
-//     //   id: 1,
-//     //   name: 'testDepartment',
-//     //   objectives: [],
-//     //   parentUnitId: 0,
-//     //   label: 'department',
-//     //   okrMasterId: 'master',
-//     //   okrTopicSponsorId: 'topicSponsor',
-//     //   okrMemberIds: ['member'],
-//     //   isActive: true,
-//     //   isParentUnitABranch: false,
-//     //   type: UnitType.DEPARTMENT,
-//     // };
-//
-//     // departmentService.getAllDepartmentsForCompanyFlatted$.mockReset();
-//     // departmentService.getAllDepartmentsForCompanyFlatted$.mockReturnValue(of(department));
-//
-//     fixture.detectChanges();
-//   });
-//
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-//
-//   // it('should call updateDashboard', () => {
-//   //   spyOn(component, 'updateDashboard');
-//   //   const button = fixture.debugElement.nativeElement.querySelector('button[id="save-changes-button"]');
-//   //   button.click();
-//   //   tick();
-//   //   expect(component.updateDashboard).toHaveBeenCalled();
-//   //
-//   //   // const onClickMock = spyOn(component, 'updateDashboard');
-//   //   // // const ne = fixture.nativeElement as HTMLElement;
-//   //   // // ne.querySelector('button[id="save-changes-button"]');
-//   //   // de.query(By.css('button')).triggerEventHandler('click', null);
-//   //   // expect(onClickMock).toHaveBeenCalled();
-//   //
-//   //   // spyOn(component, 'updateDashboard');
-//   //   // const button = de.nativeElement.querySelector('button');
-//   //   // button.click();
-//   //   // tick();
-//   //   // expect(component.updateDashboard).toHaveBeenCalled();
-//   // });
-// });
+  // beforeEach(async () => {
+  //   await TestBed.configureTestingModule({
+  //     imports: [
+  //       HttpClientTestingModule,
+  //       TranslateModule,
+  //     ],
+  //     declarations: [DashboardModificationComponent],
+  //     schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+  //     providers: [
+  //       { provide: DepartmentMapper }, //, useClass: DepartmentMapper},
+  //       { provide: MatDialog, useValue: {} },
+  //       { provide: TranslateService, useValue: {} },
+  //       { provide: AuthenticationService },
+  //       { provide: OAuthService },
+  //       { provide: DepartmentService}
+  //     ],
+  //   })
+  //     .overrideComponent(DashboardModificationComponent, {})
+  //     .compileComponents();
+  //
+  //   fixture = TestBed.createComponent(DashboardModificationComponent);
+  //   component = fixture.componentInstance;
+  //   component.dashboard = {
+  //     id: 1000,
+  //     title: 'DashboardMock',
+  //     companyId: 1,
+  //     creatorId: '1',
+  //     charts: [],
+  //     creationDate: null,
+  //   };
+  //   fixture.detectChanges();
+  // });
+
+  afterEach(() => {
+    fixture.destroy();
+  });
+
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
+});
