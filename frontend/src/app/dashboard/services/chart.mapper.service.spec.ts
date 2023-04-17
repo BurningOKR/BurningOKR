7e290c999b1c9626052970213b4956836660943e
Daniel Jonas <djonas@brockhaus-ag.de>
2023-04-17 13:11:59 +0200

    feat(dashboard): Userstory 16909 - Finished mapping tests in frontend

--- frontend/src/app/dashboard/services/chart.mapper.service.spec.ts
+++ frontend/src/app/dashboard/services/chart.mapper.service.spec.ts
@@ -1,6 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ChartMapperService } from './chart.mapper.service';
+import { BaseChartOptions } from '../model/ui/base-chart-options';
+import { BaseChartOptionsDto } from '../model/dto/chart-options/base-chart-options.dto';
+import { ChartInformationTypeEnum } from '../model/dto/chart-creation-options.dto';
 
 describe('ChartMapperService', () => {
   let service: ChartMapperService;
@@ -12,5 +15,51 @@ describe('ChartMapperService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should map BaseChartOptions (Line) to BaseChartOptionsDto', () => {
+    const baseChartOptions: BaseChartOptions = {
+      id: 0,
+      title: { text: 'expectedTitle', align: 'left' },
+      chart: undefined,
+      series: [],
+      chartType: ChartInformationTypeEnum.LINE_PROGRESS,
+      selectedTeamIds: [],
+      colors: [],
+
+    };
+
+    const baseChartOptionsDto: BaseChartOptionsDto = {
+      id: 0,
+      title: 'expectedTitle',
+      chartType: ChartInformationTypeEnum.LINE_PROGRESS,
+      selectedTeamIds: [],
+
+    } as any;
+
+    expect(service.mapEntityToDto(baseChartOptions)).toEqual(baseChartOptionsDto);
+  });
+
+  it('should map BaseChartOptions (Pie) to BaseChartOptionsDto', () => {
+    const baseChartOptions: BaseChartOptions = {
+      id: 0,
+      title: { text: 'expectedTitle', align: 'left' },
+      chart: undefined,
+      series: [],
+      chartType: ChartInformationTypeEnum.PIE_TOPICDRAFTOVERVIEW,
+      selectedTeamIds: [],
+      colors: [],
+
+    };
+
+    const baseChartOptionsDto: BaseChartOptionsDto = {
+      id: 0,
+      title: 'expectedTitle',
+      chartType: ChartInformationTypeEnum.PIE_TOPICDRAFTOVERVIEW,
+      selectedTeamIds: [],
+
+    } as any;
+
+    expect(service.mapEntityToDto(baseChartOptions)).toEqual(baseChartOptionsDto);
   });
 });
