e323c0c89b42cd3857e14bc7031f295f5bc0b512
Daniel Jonas <djonas@brockhaus-ag.de>
2023-05-11 14:19:39 +0200

    bug(dashboard): trying to fix dev-deploy fail

--- frontend/src/app/dashboard/charts/basic-line-chart/line-chart.component.ts
+++ frontend/src/app/dashboard/charts/basic-line-chart/line-chart.component.ts
@@ -1,16 +1,13 @@
-import { Component, Injectable } from '@angular/core';
+import { Component } from '@angular/core';
 import { LineChartOptions } from '../../model/ui/line-chart-options';
 import { CustomChartComponent } from '../../decorator/chart-component.decorator';
 import { BaseChartComponent } from '../base-chart/base-chart.component';
 
+@CustomChartComponent(LineChartOptions)
 @Component({
   selector: 'app-basic-line-chart',
   templateUrl: './line-chart.component.html',
   styleUrls: ['./line-chart.component.scss'],
-})
-@CustomChartComponent(LineChartOptions)
-@Injectable({
-  providedIn: 'root',
 })
 export class LineChartComponent extends BaseChartComponent<LineChartOptions> {
 }
