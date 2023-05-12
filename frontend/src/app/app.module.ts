4fc5256eef06194ddf1361dfa38d57cc5f527510
Daniel <104769716+DanielPJonas@users.noreply.github.com>
2023-05-12 13:57:43 +0200

    Feature/dashboard staging fix (#249)
    * fix(dashboard): possible fix for chart mapping error on staging. Replaced constructor in mapping with static type name.
    
    * fix(dashboard): fixed broken test
    
    * fix(dashboard): merged with dev
    
    * fix(dashboard): test fix for failing not failing test
    
    * fix(dashboard): änderung an error meldung um prüfen zu können, ob diese live sind auf staging
    
    * fix(dashboard): anpassung der error meldung um fehler finden zu können
    
    * fix(dashboard): test schlägt wieder fehl
    
    * fix(dashboard): more debugging help through console outputs (to be removed after fix)
    
    * fix(dashboard): fix for bugged test pipeline
    
    * fix(dashboard): more logs
    
    * fix(dashboard): @Injectable attempt to fix TreeShaking issue
    
    * bug(dashboard): Removed Logs
    
    * fix(dashboard): various tests to prevent the custom decorator from being shaken
    
    * bug(dashboard): trying to fix dev-deploy fail
    
    * bug(dashboard): trying to fix dev-deploy fail

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -67,6 +67,7 @@ export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
+// @ts-ignore
 @NgModule({
   declarations: [
     AdminViewComponent,
