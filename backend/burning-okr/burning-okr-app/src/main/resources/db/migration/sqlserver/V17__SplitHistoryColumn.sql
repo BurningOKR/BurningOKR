b550abd243aaa26a0528ca760deb6f89607d6b8f
Daniel Jonas <djonas@brockhaus-ag.de>
2023-06-09 10:14:50 +0200

    bug(taskboard): copied migration script for mssql database, not yet tested

---
+++ backend/burning-okr/burning-okr-app/src/main/resources/db/migration/sqlserver/V17__SplitHistoryColumn.sql
@@ -0,0 +1,18 @@
+-- ALTER TABLE okr_unit
+--     ADD COLUMN company_history_id BIGINT,
+--     ADD CONSTRAINT fk_company_history FOREIGN KEY (company_history_id) REFERENCES okr_unit_history (id) ON DELETE CASCADE;
+--
+-- ALTER TABLE okr_unit
+--     ADD COLUMN branch_history_id BIGINT,
+--     ADD CONSTRAINT fk_branch_history FOREIGN KEY (branch_history_id) REFERENCES okr_unit_history (id) ON DELETE CASCADE;
+--
+-- ALTER TABLE okr_unit
+--     ADD COLUMN department_history_id BIGINT,
+--     ADD CONSTRAINT fk_department_history FOREIGN KEY (department_history_id) REFERENCES okr_unit_history (id) ON DELETE CASCADE;
+--
+-- UPDATE okr_unit SET company_history_id = history_id WHERE okr_unit_type = 'OKR_COMPANY';
+-- UPDATE okr_unit SET department_history_id = history_id WHERE okr_unit_type = 'OKR_DEPARTMENT';
+-- UPDATE okr_unit SET branch_history_id = history_id WHERE okr_unit_type = 'OKR_BRANCH';
+--
+-- ALTER TABLE okr_unit
+--     DROP COLUMN history_id;
