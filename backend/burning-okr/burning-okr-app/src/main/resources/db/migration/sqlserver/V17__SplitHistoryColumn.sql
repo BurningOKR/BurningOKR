e92ef26d71ce910dc0a8c246d9fef166ad352d8f
Daniel Jonas <djonas@brockhaus-ag.de>
2023-06-12 11:45:58 +0200

    bug(duplicate key): Migration für msSQL erstellt

--- backend/burning-okr/burning-okr-app/src/main/resources/db/migration/sqlserver/V17__SplitHistoryColumn.sql
+++ backend/burning-okr/burning-okr-app/src/main/resources/db/migration/sqlserver/V17__SplitHistoryColumn.sql
@@ -1,18 +1,28 @@
--- ALTER TABLE okr_unit
---     ADD COLUMN company_history_id BIGINT,
---     ADD CONSTRAINT fk_company_history FOREIGN KEY (company_history_id) REFERENCES okr_unit_history (id) ON DELETE CASCADE;
---
--- ALTER TABLE okr_unit
---     ADD COLUMN branch_history_id BIGINT,
---     ADD CONSTRAINT fk_branch_history FOREIGN KEY (branch_history_id) REFERENCES okr_unit_history (id) ON DELETE CASCADE;
---
--- ALTER TABLE okr_unit
---     ADD COLUMN department_history_id BIGINT,
---     ADD CONSTRAINT fk_department_history FOREIGN KEY (department_history_id) REFERENCES okr_unit_history (id) ON DELETE CASCADE;
---
--- UPDATE okr_unit SET company_history_id = history_id WHERE okr_unit_type = 'OKR_COMPANY';
--- UPDATE okr_unit SET department_history_id = history_id WHERE okr_unit_type = 'OKR_DEPARTMENT';
--- UPDATE okr_unit SET branch_history_id = history_id WHERE okr_unit_type = 'OKR_BRANCH';
---
--- ALTER TABLE okr_unit
---     DROP COLUMN history_id;
+ALTER TABLE okr_unit
+    ADD company_history_id BIGINT;
+
+ALTER TABLE okr_unit
+    ADD branch_history_id BIGINT;
+
+ALTER TABLE okr_unit
+    ADD department_history_id BIGINT;
+Go
+
+ALTER TABLE okr_unit
+    ADD CONSTRAINT fk_company_history FOREIGN KEY (company_history_id) REFERENCES okr_unit_history (id);
+
+ALTER TABLE okr_unit
+    ADD CONSTRAINT fk_branch_history FOREIGN KEY (branch_history_id) REFERENCES okr_unit_history (id);
+
+ALTER TABLE okr_unit
+    ADD CONSTRAINT fk_department_history FOREIGN KEY (department_history_id) REFERENCES okr_unit_history (id);
+
+UPDATE okr_unit SET company_history_id = history_id WHERE okr_unit_type = 'OKR_COMPANY';
+UPDATE okr_unit SET department_history_id = history_id WHERE okr_unit_type = 'OKR_DEPARTMENT';
+UPDATE okr_unit SET branch_history_id = history_id WHERE okr_unit_type = 'OKR_BRANCH';
+
+ALTER TABLE okr_unit
+    DROP fk_okrunit_on_history
+
+ALTER TABLE okr_unit
+    DROP COLUMN history_id;
