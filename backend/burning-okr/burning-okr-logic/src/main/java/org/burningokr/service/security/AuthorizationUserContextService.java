78b36861175267fa0f3abc18595fe59a6d2f515b
EcCo18 <clemens.kurzeya@icloud.com>
2023-06-12 14:04:23 +0200

    feat(azure-ad): added azure ad as provider for oidc

--- backend/burning-okr/burning-okr-logic/src/main/java/org/burningokr/service/security/AuthorizationUserContextService.java
+++ backend/burning-okr/burning-okr-logic/src/main/java/org/burningokr/service/security/AuthorizationUserContextService.java
@@ -4,6 +4,7 @@ import com.nimbusds.jose.shaded.gson.internal.LinkedTreeMap;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.burningokr.model.configuration.SystemProperties;
 import org.burningokr.model.users.User;
 import org.burningokr.service.userhandling.UserService;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,6 +22,7 @@ import java.util.UUID;
 @Slf4j
 public class AuthorizationUserContextService {
   private final UserService userService;
+  private final SystemProperties systemProperties;
   private final HashMap<UUID, User> userHashMap = new HashMap<>();
 
   private void checkIfStringIsEmpty(String attributeName, String validatedString) throws InvalidTokenException {
@@ -31,69 +33,140 @@ public class AuthorizationUserContextService {
 
   public User getAuthenticatedUser() throws EntityNotFoundException {
     var userToken = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    var userId = UUID.fromString(userToken.getSubject());
 
-    var userOptional = userService.findById(userId);
+    var userId = switch (systemProperties.getProvider()) {
+      case "azureAD":
+        yield getUserIdFromAzureAdToken(userToken);
+      case "keycloak":
+        yield UUID.fromString(userToken.getSubject());
+      default:
+        throw new RuntimeException("Unexpected value: " + systemProperties.getProvider());
+    };
 
-    return userOptional.orElseThrow(() -> {
-      log.error("user with uuid: %s is not present in the database".formatted(userId));
-      return new EntityNotFoundException();
-    });
-  }
-
-  public void updateUserFromToken(Jwt userToken) throws InvalidTokenException {
-    var userId = UUID.fromString(userToken.getSubject());
-    var userRoles = getRolesFromJwt(userToken);
-
-    User user = userHashMap.get(userId);
-
+    var user = userHashMap.get(userId);
     if (user == null) {
-      var userOptional = userService.findById(userId);
-      user = userOptional.orElseGet(() -> createNewUser(userId));
+      throw new RuntimeException("user could not be found in hashmap");
     }
 
-    if (!user.equals(userHashMap.get(userId))) {
-      updateUserEntityByTokenValues(userToken, userRoles, user);
-    }
-  }
-
-  public void updateUserEntityByTokenValues(Jwt userToken, ArrayList<String> userRoles, User user) {
-    log.info("updateUserEntityByTokenValues called");
-    user.setAdmin(userRoles.contains("burning-okr-admin")); // TODO Auth: extract role name in env variable
-    user.setGivenName(getAttributeFromJwt(userToken, "given_name"));
-    user.setSurname(getAttributeFromJwt(userToken, "family_name"));
-    user.setMail(getAttributeFromJwt(userToken, "email"));
-
-    userHashMap.put(user.getId(), user);
-    userService.updateUser(user);
-  }
-
-  private User createNewUser(UUID userId) {
-    log.info("user with id: %s not present, registering new user".formatted(userId));
-    User user = new User();
-    user.setId(userId);
-    user.setActive(true);
-    user.setCreatedAt(LocalDateTime.now());
     return user;
   }
 
-  private ArrayList<String> getRolesFromJwt(Jwt userToken) throws InvalidTokenException {
+  public void updateUserFromToken(Jwt userToken) throws InvalidTokenException {
+    User tokenUser = switch (systemProperties.getProvider()) {
+      case "azureAD":
+        yield getUserFromAzureAdToken(userToken);
+      case "keycloak":
+        yield getUserFromKeycloakToken(userToken);
+      default:
+        throw new RuntimeException("provider specified in application yaml is not supported");
+    };
+
+    User cachedUser = userHashMap.get(tokenUser.getId());
+
+    if (cachedUser == null || !isCachedUserEqualToTokenUser(cachedUser, tokenUser)) {
+      updateCachedAndDatabaseUser(tokenUser);
+    }
+  }
+
+  private User getUserFromAzureAdToken(Jwt userToken) {
+    var userId = getUserIdFromAzureAdToken(userToken);
+    var userRoles = getRolesFromAzureToken(userToken);
+    var names = getGivenNameAndSurNameFromAzureAdToken(userToken);
+    var surname = names[0];
+    var givenName = names[1];
+    var mail = getAttributeFromJwt(userToken, "preferred_username");
+
+    return User.builder()
+            .id(userId)
+            .givenName(givenName)
+            .surname(surname)
+            .mail(mail)
+            .admin(userRoles.contains(systemProperties.getOidcAdminGroupName()))
+            .active(true)
+            .createdAt(LocalDateTime.now())
+            .build();
+  }
+
+  private String[] getGivenNameAndSurNameFromAzureAdToken(Jwt userToken) {
+    String concatName = getAttributeFromJwt(userToken, "name");
+    return concatName.replace(" ", "").split(","); // first string is family and second given name
+  }
+
+  private User getUserFromKeycloakToken(Jwt userToken) {
+    var userId = UUID.fromString(userToken.getSubject());
+    var userRoles = getRolesFromKeycloakToken(userToken);
+
+    return User.builder()
+            .id(userId)
+            .admin(userRoles.contains(systemProperties.getOidcAdminGroupName()))
+            .active(true)
+            .givenName(getAttributeFromJwt(userToken, "given_name"))
+            .surname(getAttributeFromJwt(userToken, "family_name"))
+            .mail(getAttributeFromJwt(userToken, "email"))
+            .createdAt(LocalDateTime.now())
+            .build();
+  }
+
+  private boolean isCachedUserEqualToTokenUser(User cachedUser, User tokenUser) {
+    return cachedUser.getId().equals(tokenUser.getId()) &&
+            cachedUser.getMail().equals(tokenUser.getMail()) &&
+            cachedUser.getGivenName().equals(tokenUser.getGivenName()) &&
+            cachedUser.getSurname().equals(tokenUser.getSurname()) &&
+            cachedUser.isAdmin() == tokenUser.isAdmin();
+  }
+
+  private UUID getUserIdFromAzureAdToken(Jwt jwt) {
+    var realmsMap = jwt.getClaims();
+    return UUID.fromString((String) realmsMap.get("oid"));
+  }
+
+  public void updateCachedAndDatabaseUser(User user) {
+    log.info("updating user in cache and database");
+    userHashMap.put(user.getId(), userService.updateUser(user));
+  }
+
+  private ArrayList<String> getRolesFromAzureToken(Jwt userToken) throws InvalidTokenException {
+    var realmsMap = (LinkedTreeMap<?, ?>) userToken.getClaims().get("realm_access"); // TODO (C.K.): search for correct field
+    var userRoles = new ArrayList<String>();
+
+    if (realmsMap != null) {
+      if (!checkIfKeysAreString(realmsMap.keySet())) {
+        throw new InvalidTokenException("Not all realm-access keys are of type String");
+      }
+
+      if (!(realmsMap.get("roles") instanceof ArrayList<?> roles)) {
+        throw new InvalidTokenException("roles-list is not of type ArrayList");
+      }
+
+      if (!checkIfListContainsStrings(roles)) {
+        throw new InvalidTokenException("Not all roles are of type String");
+      }
+
+      roles.forEach(role -> userRoles.add((String) role));
+    }
+
+    return userRoles;
+  }
+
+  private ArrayList<String> getRolesFromKeycloakToken(Jwt userToken) throws InvalidTokenException {
     var realmsMap = (LinkedTreeMap<?, ?>) userToken.getClaims().get("realm_access");
     var userRoles = new ArrayList<String>();
 
-    if (!checkIfKeysAreString(realmsMap.keySet())) {
-      throw new InvalidTokenException("Not all realm-access keys are of type String");
-    }
+    if (realmsMap != null) {
+      if (!checkIfKeysAreString(realmsMap.keySet())) {
+        throw new InvalidTokenException("Not all realm-access keys are of type String");
+      }
 
-    if (!(realmsMap.get("roles") instanceof ArrayList<?> roles)) {
-      throw new InvalidTokenException("roles-list is not of type ArrayList");
-    }
+      if (!(realmsMap.get("roles") instanceof ArrayList<?> roles)) {
+        throw new InvalidTokenException("roles-list is not of type ArrayList");
+      }
 
-    if (!checkIfListContainsStrings(roles)) {
-      throw new InvalidTokenException("Not all roles are of type String");
-    }
+      if (!checkIfListContainsStrings(roles)) {
+        throw new InvalidTokenException("Not all roles are of type String");
+      }
 
-    roles.forEach(role -> userRoles.add((String) role));
+      roles.forEach(role -> userRoles.add((String) role));
+    }
 
     return userRoles;
   }
