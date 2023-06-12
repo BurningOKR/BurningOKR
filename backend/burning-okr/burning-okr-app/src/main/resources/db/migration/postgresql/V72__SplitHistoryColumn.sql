80715f57184d80e848bbea0dd4719afc0bc5cf29
Christos Chouklis <131682014+chouklis@users.noreply.github.com>
2023-06-12 13:22:09 +0200

    Feature/migration mssql database (#255)
    * fix(postgres): migration script updated
    
    * fix(mssql): migration script updated
    
    ---------
    
    Co-authored-by: cchoulkis <cchouklis@brockhaus-ag.de>

--- backend/burning-okr/burning-okr-app/src/main/resources/db/migration/postgresql/V72__SplitHistoryColumn.sql
+++ backend/burning-okr/burning-okr-app/src/main/resources/db/migration/postgresql/V72__SplitHistoryColumn.sql
@@ -1,14 +1,14 @@
 ALTER TABLE okr_unit
     ADD COLUMN company_history_id BIGINT,
-    ADD CONSTRAINT fk_company_history FOREIGN KEY (company_history_id) REFERENCES okr_unit_history (id) ON DELETE CASCADE;
+    ADD CONSTRAINT fk_company_history FOREIGN KEY (company_history_id) REFERENCES okr_unit_history (id);
 
 ALTER TABLE okr_unit
     ADD COLUMN branch_history_id BIGINT,
-    ADD CONSTRAINT fk_branch_history FOREIGN KEY (branch_history_id) REFERENCES okr_unit_history (id) ON DELETE CASCADE;
+    ADD CONSTRAINT fk_branch_history FOREIGN KEY (branch_history_id) REFERENCES okr_unit_history (id);
 
 ALTER TABLE okr_unit
     ADD COLUMN department_history_id BIGINT,
-    ADD CONSTRAINT fk_department_history FOREIGN KEY (department_history_id) REFERENCES okr_unit_history (id) ON DELETE CASCADE;
+    ADD CONSTRAINT fk_department_history FOREIGN KEY (department_history_id) REFERENCES okr_unit_history (id);
 
 UPDATE okr_unit SET company_history_id = history_id WHERE okr_unit_type = 'OKR_COMPANY';
 UPDATE okr_unit SET department_history_id = history_id WHERE okr_unit_type = 'OKR_DEPARTMENT';
