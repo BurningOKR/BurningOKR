fbae27235ea26a5e5eb08c1c2787a3782c7b1ac6
Fabio <fabioleidheuser@gmail.com>
2023-06-23 16:27:51 +0200

    test(UserSettingsMapperTest): rearranged tests

--- backend/burning-okr/burning-okr-app/src/test/java/org/burningokr/mapper/settings/UserSettingsMapperTest.java
+++ backend/burning-okr/burning-okr-app/src/test/java/org/burningokr/mapper/settings/UserSettingsMapperTest.java
@@ -27,94 +27,131 @@ public class UserSettingsMapperTest {
   }
 
   @Test
-  public void test_mapDtoToEntity_ExpectIdIsMapped() {
-    Long id = 42L;
-    this.dto.setId(id);
-
-    this.entity = this.mapper.mapDtoToEntity(dto);
-
-    assertEquals(id, this.entity.getId());
-  }
-
-  @Test
-  public void test_mapDtoToEntity_ExpectCompanyIsNullIfThereIsNoCompanyId() {
-    this.entity = this.mapper.mapDtoToEntity(dto);
-
-    assertNull(this.entity.getDefaultOkrCompany());
-  }
-
-  @Test
-  public void test_mapDtoToEntity_ExpectDefaultCompanyIsMapped() {
-    Long defaultCompanyId = 50L;
-    this.dto.setDefaultCompanyId(defaultCompanyId);
-
-    this.entity = this.mapper.mapDtoToEntity(dto);
-
-    assertEquals(defaultCompanyId, this.entity.getDefaultOkrCompany().getId());
-  }
-
-  @Test
-  public void test_mapDtoToEntity_ExpectTeamIsNullIfThereIsNoTeamId() {
-    this.entity = this.mapper.mapDtoToEntity(dto);
-
-    assertNull(this.entity.getDefaultTeam());
-  }
-
-  @Test
-  public void test_mapDtoToEntity_ExpectDefaultTeamIsMapped() {
-    Long defaultTeamId = 40L;
-    this.dto.setDefaultTeamId(defaultTeamId);
-
-    this.entity = this.mapper.mapDtoToEntity(dto);
-
-    assertEquals(defaultTeamId, this.entity.getDefaultTeam().getId());
-  }
-
-  @Test
-  public void test_mapEntityToDto_ExpectIdIsMapped() {
+  public void test_mapEntityToDto_expectIdIsMapped() {
+    //Arrange
     Long id = 42L;
     this.entity.setId(id);
 
+    //Act
     this.dto = this.mapper.mapEntityToDto(entity);
 
+    //Assert
     assertEquals(id, this.dto.getId());
   }
 
   @Test
-  public void test_mapEntityToDto_ExpectDefaultCompanyIsMapped() {
+  public void test_mapEntityToDto_expectDefaultCompanyIsMapped() {
+    //Arrange
     Long companyId = 40L;
     OkrCompany okrCompany = new OkrCompany();
     okrCompany.setId(companyId);
     this.entity.setDefaultOkrCompany(okrCompany);
 
+    //Act
     this.dto = this.mapper.mapEntityToDto(entity);
 
+    //Assert
     assertEquals(companyId, this.dto.getDefaultCompanyId());
   }
 
   @Test
-  public void test_mapEntityToDto_ExpectDefaultTeamIsMapped() {
+  public void test_mapEntityToDto_expectDefaultTeamIsMapped() {
+    //Arrange
     Long defaultTeamId = 100L;
     OkrDepartment defaultTeam = new OkrDepartment();
     defaultTeam.setId(defaultTeamId);
     this.entity.setDefaultTeam(defaultTeam);
 
+    //Act
     this.dto = this.mapper.mapEntityToDto(entity);
 
+    //Assert
     assertEquals(defaultTeamId, this.dto.getDefaultTeamId());
   }
 
   @Test
-  public void test_mapEntitiesToDtos_ExpectEmptyList() {
+  public void test_mapEntitiesToDtos_expectEmptyList() {
+    //Arrange
     Collection<UserSettings> entities = new ArrayList<>();
+
+    //Act
     Collection<UserSettingsDto> dtos = this.mapper.mapEntitiesToDtos(entities);
+
+    //Assert
     assertTrue(dtos.isEmpty());
   }
 
   @Test
-  public void test_mapEntitiesToDtos_ExpectListIsMappedWithTwoElements() {
+  public void test_mapEntitiesToDtos_expectListIsMappedWithTwoElements() {
+    //Arrange
     Collection<UserSettings> entities = Arrays.asList(new UserSettings(), new UserSettings());
+
+    //Act
     Collection<UserSettingsDto> dtos = mapper.mapEntitiesToDtos(entities);
+
+    //Assert
     assertEquals(2, dtos.size());
   }
+
+  @Test
+  public void test_mapDtoToEntity_expectIdIsMapped() {
+    //Arrange
+    Long id = 42L;
+    this.dto.setId(id);
+
+    //Act
+    this.entity = this.mapper.mapDtoToEntity(dto);
+
+    //Assert
+    assertEquals(id, this.entity.getId());
+  }
+
+  @Test
+  public void test_mapDtoToEntity_expectCompanyIsNullIfThereIsNoCompanyId() {
+    //Arrange
+
+    //Act
+    this.entity = this.mapper.mapDtoToEntity(dto);
+
+    //Assert
+    assertNull(this.entity.getDefaultOkrCompany());
+  }
+
+  @Test
+  public void test_mapDtoToEntity_expectDefaultCompanyIsMapped() {
+    //Arrange
+    Long defaultCompanyId = 50L;
+    this.dto.setDefaultCompanyId(defaultCompanyId);
+
+    //Act
+    this.entity = this.mapper.mapDtoToEntity(dto);
+
+    //Assert
+    assertEquals(defaultCompanyId, this.entity.getDefaultOkrCompany().getId());
+  }
+
+  @Test
+  public void test_mapDtoToEntity_expectTeamIsNullIfThereIsNoTeamId() {
+    //Arrange
+
+    //Act
+    this.entity = this.mapper.mapDtoToEntity(dto);
+
+    //Assert
+    assertNull(this.entity.getDefaultTeam());
+  }
+
+  @Test
+  public void test_mapDtoToEntity_expectDefaultTeamIsMapped() {
+    //Arrange
+    Long defaultTeamId = 40L;
+    this.dto.setDefaultTeamId(defaultTeamId);
+
+    //Act
+    this.entity = this.mapper.mapDtoToEntity(dto);
+
+    //Assert
+    assertEquals(defaultTeamId, this.entity.getDefaultTeam().getId());
+  }
+
 }
