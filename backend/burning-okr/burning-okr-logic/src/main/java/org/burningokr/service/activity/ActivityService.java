201c5c8c16c40e0e218cd8ab5f7d22b8a1f423f2
Björn Merschmeier <bjoern@bmerschmeier.de>
2023-07-03 15:14:14 +0200

    fix(Tests): Removed system out println

--- backend/burning-okr/burning-okr-logic/src/main/java/org/burningokr/service/activity/ActivityService.java
+++ backend/burning-okr/burning-okr-logic/src/main/java/org/burningokr/service/activity/ActivityService.java
@@ -27,14 +27,11 @@ public class ActivityService {
   public <T extends Trackable<?>> void createActivity(T t, Action action) {
     User authenticatedUser = authorizationUserContextService.getAuthenticatedUser();
     Activity activity = new Activity();
-    System.out.println(activity.getUserId());
     activity.setUserId(authenticatedUser.getId() + " (" + authenticatedUser.getMail() + ")");
-    System.out.println(activity.getUserId());
     activity.setObject(
       t.getClass().getSimpleName() + " - " + t.getName() + " (id:" + t.getId() + ")");
     activity.setAction(action);
     activity.setDate(LocalDateTime.now());
-    System.out.println(activity);
     activityRepository.save(activity);
   }
 }
