4c6298973d4115671b172d44a0f6c5900a780be7
EcCo18 <clemens.kurzeya@icloud.com>
2023-08-07 16:54:45 +0200

    refactor(error-handling): added validation and error handling for UUID

--- backend/burning-okr/burning-okr-app/src/main/java/org/burningokr/controller/userhandling/UserController.java
+++ backend/burning-okr/burning-okr-app/src/main/java/org/burningokr/controller/userhandling/UserController.java
@@ -1,24 +1,31 @@
 package org.burningokr.controller.userhandling;
 
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ConstraintViolationException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.burningokr.annotation.RestApiController;
 import org.burningokr.dto.users.UserDto;
 import org.burningokr.mapper.users.UserMapper;
 import org.burningokr.model.users.User;
 import org.burningokr.service.security.authenticationUserContext.AuthenticationUserContextService;
 import org.burningokr.service.userhandling.UserService;
+import org.hibernate.validator.constraints.UUID;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Collection;
-import java.util.UUID;
 
 @RestApiController()
 @RequiredArgsConstructor
+@Validated
+@Slf4j
 public class UserController {
 
   private final UserMapper userMapper;
@@ -64,9 +71,16 @@ public class UserController {
 
   @GetMapping("/users/{userId}")
   public ResponseEntity<UserDto> getUserById(
-          @PathVariable UUID userId
+      @PathVariable @UUID String userId
   ) {
-    var user = userService.findById(userId);
+    var user = userService.findById(java.util.UUID.fromString(userId));
     return ResponseEntity.ok(userMapper.mapEntityToDto(user.orElseThrow(EntityNotFoundException::new)));
+  }
+
+  @ExceptionHandler(ConstraintViolationException.class)
+  public ResponseEntity<String> handleConstraintViolation(Exception ex) {
+    log.warn(("received invalid path variable or request param, error message: %s, returning error message and" +
+        " resuming with normal operation").formatted(ex.getMessage()));
+    return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
   }
 }
