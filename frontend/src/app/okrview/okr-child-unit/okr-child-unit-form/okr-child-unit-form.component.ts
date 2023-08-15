5778ece964bdd8f4712a10882aa7878aefdd9c61
Fabio <fabioleidheuser@gmail.com>
2023-08-15 13:17:20 +0200

    feat(okr-child-unit-form): added downsampling for images that get uploaded

--- frontend/src/app/okrview/okr-child-unit/okr-child-unit-form/okr-child-unit-form.component.ts
+++ frontend/src/app/okrview/okr-child-unit/okr-child-unit-form/okr-child-unit-form.component.ts
@@ -67,14 +67,45 @@ export class OkrChildUnitFormComponent {
 
   onChange(event) {
     const file: File = event.target.files[0];
-    const reader: FileReader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => {
 
-      const base64String: string = reader.result.toString();
-      const tempArray: string[] = base64String.split(',');
-      this.imageBase64 = tempArray[1];
-    };
+    if (file) {
+      console.log(file.size);
+      const reader: FileReader = new FileReader();
+      reader.onload = e => {
+        // Create an <img> element to load and process the image
+        const imgElement: HTMLImageElement = document.createElement('img');
+        imgElement.src = e.target.result as string;
+        imgElement.onload = () => {
+          //When the image is loaded, downsample it by a factor of 4 (0.25)
+          this.downsampleImage(imgElement, 0.25);
+        };
+      };
+      // Read the file as a data URL
+      reader.readAsDataURL(file);
+    }
+  }
+
+  downsampleImage(originalImage: HTMLImageElement, scaleFactor: number): void {
+    // Create a canvas element and get its 2D context
+    const canvas: HTMLCanvasElement = document.createElement('canvas');
+    const context: CanvasRenderingContext2D = canvas.getContext('2d');
+    // Set the canvas dimensions based on the desired scale factor
+    canvas.width = originalImage.width * scaleFactor;
+    canvas.height = originalImage.height * scaleFactor;
+    // Draw the original image onto the canvas, scaling it down
+    context.drawImage(originalImage, 0, 0, canvas.width, canvas.height);
+    // Convert the canvas content to a downsized data URL (JPEG format here)
+    // Now you can use the downsized data URL as needed
+    // For example, you can display it in an <img> element
+
+    //const downsampledDataURL: string = canvas.toDataURL('image/jpeg');
+    // const imgElement: HTMLImageElement = document.createElement('img');
+    // imgElement.src = downsampledDataURL;
+    // document.body.appendChild(imgElement);
+
+    const base64String: string = canvas.toDataURL('image/jpeg');
+    const tempArray: string[] = base64String.split(',');
+    this.imageBase64 = tempArray[1];
   }
 
   deleteAvatar() {
@@ -170,6 +201,5 @@ export class OkrChildUnitFormComponent {
 
     }
   }
-
 
 }
