a5cab0939db390bc39652021a80c8cf19e569920
cchoulkis <cchouklis@brockhaus-ag.de>
2023-08-21 12:14:24 +0200

    test(Base64ImageValidator): unit testing for encoding images

--- backend/burning-okr/burning-okr-app/src/main/java/org/burningokr/annotation/validation/Base64ImageValidator.java
+++ backend/burning-okr/burning-okr-app/src/main/java/org/burningokr/annotation/validation/Base64ImageValidator.java
@@ -7,11 +7,11 @@ import java.util.Base64;
 import java.util.regex.Pattern;
 
 public class Base64ImageValidator implements
-        ConstraintValidator<Base64Image, String> {
+    ConstraintValidator<Base64Image, String> {
 
-    public boolean nullable;
-    public float maxSizeB;
-    private final Pattern base64FormatPattern
+  public boolean nullable;
+  public float maxSizeB;
+  private final Pattern base64FormatPattern
       = Pattern.compile("^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}={2})$");
 
   @Override
@@ -22,7 +22,7 @@ public class Base64ImageValidator implements
   }
 
   @Override
-    public boolean isValid(String value, ConstraintValidatorContext context) {
+  public boolean isValid(String value, ConstraintValidatorContext context) {
     if (this.nullable && value == null)
       return true;
     else if (!this.nullable && value == null)
@@ -32,24 +32,26 @@ public class Base64ImageValidator implements
     }
   }
 
-    public boolean isValidBase64String(String base64String) {
-        return this.base64FormatPattern.matcher(base64String).matches();
-    }
+  public boolean isValidBase64String(String base64String) {
+    return this.base64FormatPattern.matcher(base64String).matches();
+  }
 
-    public boolean encodedImgHasValidSize(String base64String) {
-        float approxImgSizeInByte = this.approximateImageSize(base64String);
-        boolean approximateIsReasonable = approxImgSizeInByte <= 1.2 * this.maxSizeB;
-        if(approximateIsReasonable) {
-          byte[] imageData = Base64.getDecoder().decode(base64String);
-          return imageData.length <= this.maxSizeB;
-        }
-        else {
-          return false;
-        }
+  public boolean encodedImgHasValidSize(String base64String) {
+    float approxImgSizeInByte = this.approximateImageSize(base64String);
+    boolean approximateIsReasonable = approxImgSizeInByte <= 1.2 * this.maxSizeB;
+    if (approximateIsReasonable) {
+      return getImageSizeInByte(base64String) <= this.maxSizeB;
+    } else {
+      return false;
     }
+  }
 
-    private float approximateImageSize(String base64String) {
-        return ((base64String.length() * (3F/4)) - 2);
-    }
+  public int getImageSizeInByte(String base64String) {
+    return Base64.getDecoder().decode(base64String).length;
+  }
+
+  public float approximateImageSize(String base64String) {
+    return ((base64String.length() * (3F / 4)) - 2);
+  }
 
 }
