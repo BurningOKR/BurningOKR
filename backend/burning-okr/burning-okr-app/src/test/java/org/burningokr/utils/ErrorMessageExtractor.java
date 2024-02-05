8ebdcda72ad813a794425c541de28fcee74cc71f
Lionel Pereira Azevedo <Lionel.PereiraAzevedo@brockhaus-ag.de>
2024-02-05 12:58:27 +0100

    refactor(ErrorMessageExtractor): Minor refactor

--- backend/burning-okr/burning-okr-app/src/test/java/org/burningokr/utils/ErrorMessageExtractor.java
+++ backend/burning-okr/burning-okr-app/src/test/java/org/burningokr/utils/ErrorMessageExtractor.java
@@ -3,15 +3,13 @@ package org.burningokr.utils;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MvcResult;
 
-import java.sql.SQLOutput;
 import java.util.Objects;
 
 public class ErrorMessageExtractor {
 
   public static String extractMessageFromHandler(MvcResult result) {
-    if (HttpStatus.BAD_REQUEST.value() != result.getResponse().getStatus()) return "";
-    String msg = Objects.requireNonNull(result.getResolvedException()).getMessage().trim().split("default message")[2];
-    System.out.println(msg.trim().length());
-    return msg.trim().substring(1, msg.length() - 3);
+    if (HttpStatus.BAD_REQUEST.value() != result.getResponse().getStatus()) return "<extraction failed: response status was not 400>";
+    String msg = Objects.requireNonNull(result.getResolvedException()).getMessage().trim().split("default message")[2].trim();
+    return msg.substring(1, msg.length() - 3);
   }
 }
