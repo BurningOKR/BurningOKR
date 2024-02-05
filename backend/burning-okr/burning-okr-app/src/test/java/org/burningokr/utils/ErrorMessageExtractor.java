24f81bfa69c3ddf4bf274317dabdca36c733fd92
Lionel Pereira Azevedo <Lionel.PereiraAzevedo@brockhaus-ag.de>
2024-02-05 13:18:03 +0100

    fix(ErrorMessageExtractor): Revert refactor

--- backend/burning-okr/burning-okr-app/src/test/java/org/burningokr/utils/ErrorMessageExtractor.java
+++ backend/burning-okr/burning-okr-app/src/test/java/org/burningokr/utils/ErrorMessageExtractor.java
@@ -9,7 +9,7 @@ public class ErrorMessageExtractor {
 
   public static String extractMessageFromHandler(MvcResult result) {
     if (HttpStatus.BAD_REQUEST.value() != result.getResponse().getStatus()) return "<extraction failed: response status was not 400>";
-    String msg = Objects.requireNonNull(result.getResolvedException()).getMessage().trim().split("default message")[2].trim();
-    return msg.substring(1, msg.length() - 3);
+    String msg = Objects.requireNonNull(result.getResolvedException()).getMessage().trim().split("default message")[2];
+    return msg.trim().substring(1, msg.length() - 3);
   }
 }
